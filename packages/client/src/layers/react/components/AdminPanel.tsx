688f14ad08ebe01f90ff3ee9c4dfc56a34054b1a
Curtis Chong <curtischong5@gmail.com>
2023-06-07 22:46:32 -0400

    admin panel is no longer clickable

--- packages/client/src/layers/react/components/AdminPanel.tsx
+++ packages/client/src/layers/react/components/AdminPanel.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { registerUIComponent } from "../engine";
 import { concat, map, of } from "rxjs";
 import styled from "styled-components";
+import { CloseableContainer } from "./common";
 
 export function registerAdminPanel() {
   registerUIComponent(
     "AdminPanel",
     {
-      rowStart: 3,
-      rowEnd: 13,
+      rowStart: 2,
+      rowEnd: 11,
       colStart: 1,
       colEnd: 4,
     },
@@ -20,16 +21,21 @@ export function registerAdminPanel() {
       const {
         network: { world },
       } = layers;
-      return show ? <div></div> : null;
+      return show ? (
+        // pointerEvents: all is needed so when we click on the admin panel, we don't gain focus on the noa canvas
+        <div
+          className="relative z-50 w-full h-full bg-slate-900 p-10 text-white"
+          style={{ pointerEvents: "all" }}
+        >
+          <p className="text-2xl">Admin Panel</p>
+          <button
+            className="p-5 bg-slate-700 cursor-pointer"
+            onClick={() => alert("hi")}
+          >
+            Download Voxels
+          </button>
+        </div>
+      ) : null;
     }
   );
 }
-
-const AdminPanelContainer = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: red;
-`;
--- packages/client/src/layers/react/components/index.ts
+++ packages/client/src/layers/react/components/index.ts
@@ -13,7 +13,7 @@ export function registerUIComponents() {
   registerLoadingState();
   registerActionQueue();
   registerCrosshairs();
-  registerBlockExplorer();
+  // registerBlockExplorer();
   registerInventoryHud();
   registerSidebar();
   registerToast();

e59a8b575fa2a742684978798ad9607cf53cc25d
Curtis Chong <curtischong5@gmail.com>
2023-06-10 15:43:42 -0400

    debugging mud2 tests

--- packages/contracts/src/test/systems/RegisterCreation.t.sol
+++ packages/contracts/src/test/systems/RegisterCreation.t.sol
@@ -14,9 +14,9 @@ import { Utilities } from "@latticexyz/std-contracts/src/test/Utilities.sol";
 contract RegisterCreationTest is MudV2Test {
   IWorld private world;
   IStore private store;
-  address payable internal alice;
   Utilities internal immutable utils = new Utilities();
 
+  address payable internal alice;
   bytes32 voxel1;
   bytes32 voxel2;
 
@@ -26,23 +26,29 @@ contract RegisterCreationTest is MudV2Test {
     store = IStore(worldAddress);
 
     alice = utils.getNextUserAddress();
-
-    // Give two voxels to alice
-    voxel1 = getUniqueEntity();
-    VoxelType.set(voxel1, CyanWoolID);
-    OwnedBy.set(voxel1, addressToEntityKey(alice));
-
-    voxel2 = getUniqueEntity();
-    VoxelType.set(voxel2, CyanWoolID);
-    OwnedBy.set(voxel2, addressToEntityKey(alice));
   }
 
   function testRegisterCreation() public {
     vm.startPrank(alice);
+
+
+    // Give two voxels to alice
+    voxel1 = bytes32(uint256(1)); // For some reason, I can't use: voxel1 = getUniqueEntity();
+//    voxel1 = getUniqueEntity();
+//    require(false, "failed here2");
+    VoxelType.set(voxel1, CyanWoolID);
+    OwnedBy.set(voxel1, addressToEntityKey(alice));
+
+//    voxel2 = getUniqueEntity();
+    voxel2 = bytes32(uint256(2));
+    VoxelType.set(voxel2, CyanWoolID);
+    OwnedBy.set(voxel2, addressToEntityKey(alice));
+
     VoxelCoord memory coord1 = VoxelCoord(1, 2, 1);
     VoxelCoord memory coord2 = VoxelCoord(2, 1, 2);
     world.build(voxel1, coord1);
     world.build(voxel2, coord2);
+//    require(false, "failed here");
 
     world.registerCreation("test creation name", coord1, coord2);
     vm.stopPrank();

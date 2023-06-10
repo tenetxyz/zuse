985556e80e3a3b9fee2ddeff5583b1a7f8ca684b
Curtis Chong <curtischong5@gmail.com>
2023-06-10 13:24:26 -0400

    porting over registerSystem to mud2

--- packages/contracts/src/systems/MineSystem.sol
+++ packages/contracts/src/systems/MineSystem.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import { getKeysWithValue } from "@latticexyz/world/src/modules/keyswithvalue/getKeysWithValue.sol";
 import { getUniqueEntity } from "@latticexyz/world/src/modules/uniqueentity/getUniqueEntity.sol";
 import { System } from "@latticexyz/world/src/System.sol";
 import { VoxelCoord } from "../types.sol";
+<<<<<<< HEAD
 import { OwnedBy, Position, PositionTableId, VoxelType } from "../codegen/Tables.sol";
 import { AirID, WaterID } from "../prototypes/Voxels.sol";
 import { addressToEntityKey } from "../utils.sol";
+=======
+import { OwnedBy, Position, PositionTableId, Item } from "../codegen/Tables.sol";
+import { AirID, WaterID } from "../prototypes/Blocks.sol";
+import { addressToEntityKey, getEntitiesAtCoord } from "../utils.sol";
+>>>>>>> 717a828 (porting over registerSystem to mud2)
 import { IWorld } from "../codegen/world/IWorld.sol";
 import { Occurrence } from "../codegen/Tables.sol";
 import { console } from "forge-std/console.sol";
@@ -21,8 +26,8 @@ contract MineSystem is System {
 
     // TODO: check claim in chunk
 
-    // Check ECS voxels at coord
-    bytes32[] memory entitiesAtPosition = getKeysWithValue(PositionTableId, Position.encode(coord.x, coord.y, coord.z));
+    // Check ECS blocks at coord
+    bytes32[] memory entitiesAtPosition = getEntitiesAtCoord(coord);
 
     bytes32 entity;
 

1e96831cdeb2c35b5143a6487598afdbc06ee9a8
Curtis Chong <curtischong5@gmail.com>
2023-06-12 11:58:26 -0400

    backup before swap to ephemerial table

--- packages/contracts/src/systems/GiftVoxelSystem.sol
+++ packages/contracts/src/systems/GiftVoxelSystem.sol
@@ -9,27 +9,35 @@ import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 contract GiftVoxelSystem is System {
     using EnumerableSet for EnumerableSet.Bytes32Set;
+    EnumerableSet.Bytes32Set private uniqueVoxelTypesIOwn;
 
     function giftVoxel(bytes32 voxelType ) public returns (bytes32) {
-        // first make sure the user has enough inventory room to receive the gift
-        QueryFragment[] memory fragments = new QueryFragment[](2);
-
-        fragments[0] = QueryFragment(QueryType.HasValue, OwnedByTableId, abi.encode(_msgSender())); // Specify OwnedBy first since it's a more restrictive filter (for performance reasons)
-        fragments[1] = QueryFragment(QueryType.Has, VoxelTypeTableId, new bytes(0));
-        bytes32[][] memory voxelsIOwn = query(fragments);
-        EnumerableSet.AddressSet uniqueVoxelTypesIOwn;
-
-        // TODO: when MUD adds a join operation, we can find all the unique voxel types in one query
-        for(int i = 0; i < voxelsIOwn; i < voxelsIOwn.length){
-            bytes32 voxelType = VoxelType.get(voxelsIOwn[i]);
-            uniqueVoxelTypesIOwn.add(voxelType);
-        }
-        require(uniqueVoxelTypesIOwn.length < 36, "You cannot own more than 36 unique voxel types");
-
+        require(numUniqueVoxelsIOwn() < 36, "You cannot own more than 36 unique voxel types");
 
         bytes32 entity = getUniqueEntity();
         VoxelType.set(entity, voxelType);
         OwnedBy.set(entity, addressToEntityKey(_msgSender()));
         return entity;
+    }
+
+    function numUniqueVoxelsIOwn() private view returns (uint) {
+        // first make sure the user has enough inventory room to receive the gift
+        QueryFragment[] memory fragments = new QueryFragment[](2);
+
+        fragments[0] = QueryFragment(QueryType.HasValue, OwnedByTableId, abi.encode(_msgSender())); // Specify OwnedBy first since it's a more restrictive filter (for performance reasons)
+        fragments[1] = QueryFragment(QueryType.Has, VoxelTypeTableId, new bytes(0));
+        bytes32[][] memory voxelsIOwn = query(fragments); // a key-value tuple of bytes32
+
+        require(uniqueVoxelTypesIOwn.length() == 0, "the uniqueVoxelTypesIOwn set should be empty");
+        // TODO: when MUD adds a join operation, we can find all the unique voxel types in one query
+        for(uint256 i = 0; i < voxelsIOwn.length; i++){
+            uniqueVoxelTypesIOwn.add(VoxelType.get(voxelsIOwn[1][i]));
+        }
+        uint numUniqueVoxels = uniqueVoxelTypesIOwn.length();
+        // clear the uniqueVoxelTypesIOwn set
+        for(uint256 i = 0; i < uniqueVoxelTypesIOwn.length(); i++){
+            uniqueVoxelTypesIOwn.remove(uniqueVoxelTypesIOwn[i]);
+        }
+        return numUniqueVoxels;
     }
 }

00ea14520281ad54f29cd56b6e17e490cb0011a4
Curtis Chong <curtischong5@gmail.com>
2023-06-11 21:55:05 -0400

    fixing gifted voxels not appearing properly (#35)

--- packages/contracts/src/systems/GiftVoxelSystem.sol
+++ packages/contracts/src/systems/GiftVoxelSystem.sol
@@ -9,7 +9,7 @@ contract GiftVoxelSystem is System {
     function giftVoxel(bytes32 voxelType ) public returns (bytes32) {
         bytes32 entity = getUniqueEntity();
         VoxelType.set(entity, voxelType);
-        OwnedBy.set(entity, addressToEntityKey(msg.sender));
+        OwnedBy.set(entity, addressToEntityKey(_msgSender()));
         return entity;
     }
 }

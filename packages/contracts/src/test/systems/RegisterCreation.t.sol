67ac0f56f95f32912e2e8a175c8195131830fb60
Curtis Chong <curtischong5@gmail.com>
2023-06-11 21:42:59 -0400

    fixing gifted voxels not appearing properly

--- packages/contracts/src/test/systems/RegisterCreation.t.sol
+++ packages/contracts/src/test/systems/RegisterCreation.t.sol
@@ -36,15 +36,15 @@ contract RegisterCreationTest is MudV2Test {
     // NOTE: I don't think you can call Component.set(store, value);, you can only call Component.get(store, key);
     // This is why I am gifting the voxels to Alice.
     // For some reason, you also can't use: voxel1 = getUniqueEntity();
-    bytes32 voxel1 = world.giftVoxel(CyanWoolID);
-    bytes32 voxel2 = world.giftVoxel(CyanWoolID);
+    bytes32 voxel1 = world.tenet_GiftVoxelSystem_giftVoxel(CyanWoolID);
+    bytes32 voxel2 = world.tenet_GiftVoxelSystem_giftVoxel(CyanWoolID);
 
     VoxelCoord memory coord1 = VoxelCoord(1, 2, 1);
     VoxelCoord memory coord2 = VoxelCoord(2, 1, 2);
-    world.build(voxel1, coord1);
-    world.build(voxel2, coord2);
+    world.tenet_BuildSystem_build(voxel1, coord1);
+    world.tenet_BuildSystem_build(voxel2, coord2);
 
-    world.registerCreation("test creation name", coord1, coord2);
+    world.tenet_RegisterCreation_registerCreation("test creation name", coord1, coord2);
     vm.stopPrank();
   }
 }

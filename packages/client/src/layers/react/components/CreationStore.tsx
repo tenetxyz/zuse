a32f209a7fe24713204cb9bdef85cb504293b117
Curtis Chong <curtischong5@gmail.com>
2023-06-19 21:48:14 -0400

    listen to new creation voxels

--- packages/client/src/layers/react/components/CreationStore.tsx
+++ packages/client/src/layers/react/components/CreationStore.tsx
@@ -1,17 +1,7 @@
 import React, { ChangeEvent, KeyboardEvent, useEffect } from "react";
 import { Layers } from "../../../types";
-import {
-  defineQuery,
-  Entity,
-  Has,
-  HasValue,
-  setComponent,
-} from "@latticexyz/recs";
-import { NotificationIcon } from "../../noa/components/persistentNotification";
-import { IVoxelSelection } from "../../noa/components/VoxelSelection";
-import { calculateMinMax } from "../../../utils/voxels";
+import { defineQuery, Has, Type } from "@latticexyz/recs";
 import { useComponentValue, useObservableValue } from "@latticexyz/react";
-import { to64CharAddress } from "../../../utils/entity";
 import { ComponentUpdate } from "@latticexyz/recs/src/types";
 import { UpdateType } from "@latticexyz/recs/src/constants";
 
@@ -41,23 +31,21 @@ const CreationStore: React.FC<Props> = ({ layers, filters, setFilters }) => {
 
   const [searchValue, setSearchValue] = React.useState<string>("");
 
-  const creationsUpdate: (ComponentUpdate & { type: UpdateType }) | undefined =
-    useObservableValue(
-      defineQuery(
-        // I think if we inline this query, we aren't going to re-subscribe on re-renders
-        [Has(VoxelTypes), Has(RelativePositions), Has(VoxelMetadata)],
-        {
-          runOnInit: true,
-        }
-      ).update$
-    );
+  const creationsUpdate = useComponentValue(VoxelTypes, undefined);
 
   useEffect(() => {
-    console.log(searchValue);
+    return;
   }, [searchValue]);
 
+  useEffect(() => {
+    const creationIds = VoxelTypes.entities();
+    for (const creationId of creationIds) {
+      console.log(creationId);
+    }
+  }, [creationsUpdate]);
+
   return (
-    <div className="max-w-md mx-auto p-4 text-slate-700">
+    <div className="max-w-md mx-auto p-4 text-white">
       <input
         className="bg-slate-700 p-1 ml-2 focus:outline-slate-700 border-1 border-solid mb-1 "
         value={searchValue}

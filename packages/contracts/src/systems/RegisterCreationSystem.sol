d6b2831d06c33e56a137cd666f9d43350d6664d4
Curtis Chong <curtischong5@gmail.com>
2023-06-20 11:20:51 -0400

    added logic to print the duplicated coord

--- packages/contracts/src/systems/RegisterCreationSystem.sol
+++ packages/contracts/src/systems/RegisterCreationSystem.sol
@@ -5,7 +5,7 @@ import { getUniqueEntity } from "@latticexyz/world/src/modules/uniqueentity/getU
 import { System } from "@latticexyz/world/src/System.sol";
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 import { IWorld } from "../codegen/world/IWorld.sol";
-import { addressToEntityKey, getEntitiesAtCoord } from "../utils.sol";
+import { addressToEntityKey, getEntitiesAtCoord, voxelCoordToString } from "../utils.sol";
 import { OwnedBy, Position, Name, VoxelType, Description, VoxelTypes, VoxelMetadata, RelativePositions } from "../codegen/Tables.sol";
 import {PositionData} from "../codegen/tables/Position.sol";
 import { VoxelCoord } from "../types.sol";
@@ -40,8 +40,12 @@ contract RegisterCreationSystem is System {
             voxelCoords.length <= MAX_BLOCKS_IN_CREATION,
             string(abi.encodePacked("Your creation cannot exceed ", Strings.toString(MAX_BLOCKS_IN_CREATION), " blocks"))
         );
-        // TODO: specify which coords are duplicated
-        require(!hasDuplicateVoxelCoords(voxelCoords), "Two voxels in your creation has the same coordinates");
+
+        (bool hasDuplicate, VoxelCoord memory duplicate1, VoxelCoord memory duplicate2) = hasDuplicateVoxelCoords(voxelCoords);
+        require(
+            !hasDuplicate,
+            string(abi.encodePacked("Two voxels in your creation has the same coordinates: ", voxelCoordToString(duplicate1)))
+        );
 
         // TODO: should we also limit the dimensions of the creation?
     }
@@ -98,15 +102,16 @@ contract RegisterCreationSystem is System {
 
     function hasDuplicateVoxelCoords(
         VoxelCoord[] memory coords
-    ) private view returns (bool) {
+    ) private view returns (bool, VoxelCoord memory, VoxelCoord memory) {
         for (uint i = 0; i < coords.length; i++) {
             for (uint j = i+1; j < coords.length; j++) {
                 if(coords[i].x == coords[j].x && coords[i].y == coords[j].y && coords[i].z == coords[j].z) {
-                    return true;
+                    return (true, coords[i], coords[j]);
                 }
             }
         }
-        return false;
+        VoxelCoord memory emptyCoord = VoxelCoord(0, 0, 0);
+        return (false, emptyCoord, emptyCoord);
     }
 
     // hashing the message sender means that two different players can register the same creation

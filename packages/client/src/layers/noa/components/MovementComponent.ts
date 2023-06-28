eb370d672c4e6c194af752b1d771deaed407d3a6
Curtis Chong <curtischong5@gmail.com>
2023-06-28 18:27:39 -0400

    back to original jump

--- packages/client/src/layers/noa/components/MovementComponent.ts
+++ packages/client/src/layers/noa/components/MovementComponent.ts
@@ -1,9 +1,15 @@
 import vec3 from "gl-vec3";
+import { RigidBody } from "noa-engine/dist/src/components/physics";
+
+enum BodyState {
+  onGround,
+  inAir,
+}
 
 export interface IMovementState {
   heading: number; // radians
   running: boolean;
-  jumping: boolean;
+  onJump: boolean;
 
   // options
   maxSpeed: number;
@@ -25,15 +31,17 @@ export interface IMovementState {
 
   // internal state
   _jumpCount: number;
-  _currjumptime: number;
   _isJumping: boolean;
+  _currjumptime: number;
+
+  resting?: [number, number, number];
 }
 
 export function MovementState(): IMovementState {
   return {
     heading: 0,
     running: false,
-    jumping: false,
+    onJump: false,
 
     // options
     maxSpeed: 10,
@@ -60,16 +68,8 @@ export function MovementState(): IMovementState {
   };
 }
 
-interface IPhysicsBody {
-  atRestY(): number;
-  applyForce(force: number[]): void;
-  applyImpulse(impulse: number[]): void;
-  velocity: number[];
-  friction: number;
-}
-
 interface IEntity {
-  getPhysics(id: string): IPhysicsBody | undefined;
+  getPhysics(id: string): RigidBody | undefined;
 }
 
 interface IEngine {
@@ -99,7 +99,7 @@ export default function (noa: any): IMovement {
       for (let i = 0; i < states.length; i++) {
         const state = states[i];
         const phys = ents.getPhysics(state.__id);
-        if (phys) applyMovementPhysics(dt, state, phys);
+        if (phys) applyMovementPhysics(dt, state, phys.body);
       }
     },
   };
@@ -109,195 +109,26 @@ const tempvec = vec3.create();
 const tempvec2 = vec3.create();
 const zeroVec = vec3.create();
 
-// function applyMovementPhysics(dt: number, state: IMovementState, body: IPhysicsBody) {
-//   // move implementation originally written as external module
-//   // see https://github.com/fenomas/voxel-fps-controller
-//   // for original code
-//   if (body.hasOwnProperty("body")) {
-//     body = body.body;
-//   }
-//   let onGround = body.velocity[1] === 0;
-//   if (onGround) {
-//     state._isJumping = false;
-//     state._jumpCount = 0;
-//   }
-
-//   let canjump = onGround || state._jumpCount < state.airJumps;
-
-//   // if (state.isFlying) {
-//   //   body.velocity[1] = 0; // so the user doesn't fall
-//   //   if (state.jumping) {
-//   //     body.applyForce([0, state.moveForce, 0]); // move upward
-//   //   } else if (state.isCrouching) {
-//   //     body.applyForce([0, -state.moveForce, 0]); // move downward
-//   //   }
-//   // } else {
-//   // process jump input
-//   if (state.jumping) {
-//     if (state._isJumping) {
-//       // continue previous jump
-//       if (state._currjumptime > 0) {
-//         let jumpForce: number = state.jumpForce;
-//         if (state._currjumptime < dt) jumpForce *= state._currjumptime / dt;
-//         body.applyForce([0, jumpForce, 0]);
-//         state._currjumptime -= dt;
-//       }
-//     } else if (canjump) {
-//       // start new jump
-//       state._isJumping = true;
-//       if (!onGround) state._jumpCount++;
-//       state._currjumptime = state.jumpTime;
-//       body.applyImpulse([0, state.jumpImpulse, 0]);
-//       // clear downward velocity on airjump
-//       if (!onGround && body.velocity[1] < 0) body.velocity[1] = 0;
-//     }
-//   } else {
-//     state._isJumping = false;
-//   }
-//   // }
-
-//   // exportMovementForcesToBody(state, body, dt, onGround);
-
-//   // apply movement forces if entity is moving, otherwise just friction
-//   let m: any = tempvec;
-//   let push: any = tempvec2;
-//   if (state.running) {
-//     let speed: number = state.maxSpeed;
-//     // todo: add crouch/sprint modifiers if needed
-//     // if (state.sprint) speed *= state.sprintMoveMult
-//     // if (state.crouch) speed *= state.crouchMoveMult
-//     vec3.set(m, 0, 0, speed);
-
-//     // rotate move vector to entity's heading
-//     vec3.rotateY(m, m, zeroVec, state.heading);
-
-//     // push vector to achieve desired speed & dir
-//     // following code to adjust 2D velocity to desired amount is patterned on Quake:
-//     // https://github.com/id-Software/Quake-III-Arena/blob/master/code/game/bg_pmove.c#L275
-//     vec3.sub(push, m, body.velocity);
-//     push[1] = 0;
-//     let pushLen: number = vec3.len(push);
-//     vec3.normalize(push, push);
-
-//     if (pushLen > 0) {
-//       // pushing force vector
-//       let canPush: number = state.moveForce;
-//       if (!onGround) canPush *= state.airMoveMult;
-
-//       // apply final force
-//       let pushAmt: number = state.responsiveness * pushLen;
-//       if (canPush > pushAmt) canPush = pushAmt;
-
-//       vec3.scale(push, push, canPush);
-//       body.applyForce(push);
-//     }
-
-//     // different friction when not moving
-//     // idea from Sonic: http://info.sonicretro.org/SPG:Running
-//     body.friction = state.runningFriction;
-//   } else {
-//     body.friction = state.standingFriction;
-//   }
-// }
-
-// const exportMovementForcesToBody = (state: IMovementState, body: IPhysicsBody, dt: number, onGround: boolean) => {
-//   let canjump = onGround || state._jumpCount < state.airJumps;
-
-//   if (state.isFlying) {
-//     body.velocity[1] = 0; // so the user doesn't fall
-//     if (state.jumping) {
-//       body.applyForce([0, state.moveForce, 0]); // move upward
-//     } else if (state.isCrouching) {
-//       body.applyForce([0, -state.moveForce, 0]); // move downward
-//     }
-//   } else {
-//     // process jump input
-//     if (state.jumping) {
-//       if (state._isJumping) {
-//         // continue previous jump
-//         if (state._currjumptime > 0) {
-//           let jumpForce: number = state.jumpForce;
-//           if (state._currjumptime < dt) jumpForce *= state._currjumptime / dt;
-//           body.applyForce([0, jumpForce, 0]);
-//           state._currjumptime -= dt;
-//         }
-//       } else if (canjump) {
-//         // start new jump
-//         state._isJumping = true;
-//         if (!onGround) state._jumpCount++;
-//         state._currjumptime = state.jumpTime;
-//         body.applyImpulse([0, state.jumpImpulse, 0]);
-//         // clear downward velocity on airjump
-//         if (!onGround && body.velocity[1] < 0) body.velocity[1] = 0;
-//       }
-//     } else {
-//       state._isJumping = false;
-//     }
-//   }
-// };
-// const exportMovementForcesToBody = (state: IMovementState, body: IPhysicsBody, dt: number, onGround: boolean) => {
-//   let canjump = onGround || (state._jumpCount < state.airJumps && state.canFly);
-
-//   if (state.isFlying) {
-//     body.velocity[1] = 0;
-//     body.applyForce([0, state.jumping ? state.moveForce : -state.moveForce * Number(state.isCrouching), 0]);
-//   } else {
-//     if (state.jumping && canjump) {
-//       state._isJumping = true;
-//       state._currjumptime = onGround ? state.jumpTime : state._currjumptime - dt;
-//       body.velocity[1] = onGround ? 0 : body.velocity[1];
-//       state._jumpCount += Number(!onGround);
-//       body[onGround ? "applyImpulse" : "applyForce"]([0, onGround ? state.jumpImpulse : state.jumpForce, 0]);
-//     } else {
-//       state._isJumping = false;
-//     }
-//   }
-// };
-
-function applyMovementPhysics(dt, state, body) {
+function applyMovementPhysics(dt: number, state: IMovementState, body: RigidBody) {
   // move implementation originally written as external module
-  //   see https://github.com/fenomas/voxel-fps-controller
-  //   for original code
-  if (body.hasOwnProperty("body")) {
-    body = body.body;
-  }
+  // see https://github.com/fenomas/voxel-fps-controller
+  // for original code
 
-  // jumping
-  var onGround = body.atRestY() < 0;
-  var canjump = onGround || state._jumpCount < state.airJumps;
-  if (onGround) {
-    state._isJumping = false;
+  // if vecloicy is 0 and isresting is ttrue, then we know it's on ground
+  // if velocity is nonzero it's not on ground
+  let isOnGround = body.atRestY() < 0;
+  // console.log(isOnGround);
+  if (isOnGround) {
     state._jumpCount = 0;
   }
 
-  // process jump input
-  if (state.jumping) {
-    if (state._isJumping) {
-      // continue previous jump
-      if (state._currjumptime > 0) {
-        var jf = state.jumpForce;
-        if (state._currjumptime < dt) jf *= state._currjumptime / dt;
-        body.applyForce([0, jf, 0]);
-        state._currjumptime -= dt;
-      }
-    } else if (canjump) {
-      // start new jump
-      state._isJumping = true;
-      if (!onGround) state._jumpCount++;
-      state._currjumptime = state.jumpTime;
-      body.applyImpulse([0, state.jumpImpulse, 0]);
-      // clear downward velocity on airjump
-      if (!onGround && body.velocity[1] < 0) body.velocity[1] = 0;
-    }
-  } else {
-    state._isJumping = false;
-  }
+  exportMovementForcesToBody(state, body, dt, isOnGround ? BodyState.onGround : BodyState.inAir);
 
   // apply movement forces if entity is moving, otherwise just friction
-  var m = tempvec;
-  var push = tempvec2;
+  let m: any = tempvec;
+  let push: any = tempvec2;
   if (state.running) {
-    var speed = state.maxSpeed;
+    let speed: number = state.maxSpeed;
     // todo: add crouch/sprint modifiers if needed
     // if (state.sprint) speed *= state.sprintMoveMult
     // if (state.crouch) speed *= state.crouchMoveMult
@@ -309,18 +140,18 @@ function applyMovementPhysics(dt, state, body) {
     // push vector to achieve desired speed & dir
     // following code to adjust 2D velocity to desired amount is patterned on Quake:
     // https://github.com/id-Software/Quake-III-Arena/blob/master/code/game/bg_pmove.c#L275
-    vec3.subtract(push, m, body.velocity);
+    vec3.sub(push, m, body.velocity);
     push[1] = 0;
-    var pushLen = vec3.length(push);
+    let pushLen: number = vec3.len(push);
     vec3.normalize(push, push);
 
     if (pushLen > 0) {
       // pushing force vector
-      var canPush = state.moveForce;
-      if (!onGround) canPush *= state.airMoveMult;
+      let canPush: number = state.moveForce;
+      if (!isOnGround) canPush *= state.airMoveMult;
 
       // apply final force
-      var pushAmt = state.responsiveness * pushLen;
+      let pushAmt: number = state.responsiveness * pushLen;
       if (canPush > pushAmt) canPush = pushAmt;
 
       vec3.scale(push, push, canPush);
@@ -334,3 +165,44 @@ function applyMovementPhysics(dt, state, body) {
     body.friction = state.standingFriction;
   }
 }
+
+const exportMovementForcesToBody = (state: IMovementState, body: RigidBody, dt: number, bodyState: BodyState) => {
+  let canjump = bodyState === BodyState.onGround || state._jumpCount < state.airJumps;
+
+  if (state.onJump) {
+    if (state._isJumping) {
+      // continue previous jump
+      if (state._currjumptime > 0) {
+        var jf = state.jumpForce;
+        if (state._currjumptime < dt) jf *= state._currjumptime / dt;
+        body.applyForce([0, jf, 0]);
+        state._currjumptime -= dt;
+      }
+    } else if (canjump) {
+      // start new jump
+      state._isJumping = true;
+      if (bodyState === BodyState.inAir) state._jumpCount++;
+      state._currjumptime = state.jumpTime;
+      body.applyImpulse([0, state.jumpImpulse, 0]);
+      // clear downward velocity on airjump
+      if (bodyState === BodyState.inAir && body.velocity[1] < 0) body.velocity[1] = 0;
+    }
+  } else {
+    state._isJumping = false;
+  }
+
+  // if (state.onJump) {
+  //   if (state.canFly) {
+  //     state.isFlying = true;
+  //     body.applyForce([0, state.jumpForce, 0]);
+  //     body.velocity[1] = 0;
+  //   } else {
+  //     // the user is trying to do an air jump
+  //     if (state._jumpCount < state.airJumps) {
+  //       state._jumpCount++;
+  //       state._currjumptime = state.jumpTime;
+  //       body.applyImpulse([0, state.jumpImpulse, 0]);
+  //     }
+  //   }
+  // }
+};

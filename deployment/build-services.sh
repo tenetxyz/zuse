4576786e287b2963c902543ebddf0d5fd375578b
Curtis Chong <curtischong5@gmail.com>
2023-07-01 14:21:05 -0400

    build script works

--- deployment/build-services.sh
+++ deployment/build-services.sh
@@ -14,6 +14,12 @@
 read -p "Enter the server URL: " server_url
 
 cd ../../mud/packages/services
-make ecs-snapshot
-make faucet
-scp -P 2222 bin/faucet "ec2-user@$server_url"
+# make ecs-snapshot
+# make ecs-snapshot for linux
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -a -o ./bin/ecs-snapshot ./cmd/ecs-snapshot
+scp -i ~/.ssh/Tenet.pem bin/ecs-snapshot "ec2-user@$server_url:/home/ec2-user/ecs-snapshot"
+
+# make faucet
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -a -o ./bin/faucet ./cmd/faucet
+scp -i ~/.ssh/Tenet.pem bin/faucet "ec2-user@$server_url:/home/ec2-user/faucet"
+

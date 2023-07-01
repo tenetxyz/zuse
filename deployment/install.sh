c107f5a82cb72be036c2138f7dddd1a879297425
Curtis Chong <curtischong5@gmail.com>
2023-07-01 17:34:14 -0400

    install packages in our repo

--- deployment/install.sh
+++ deployment/install.sh
@@ -11,7 +11,18 @@ nvm install --lts
 wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
 source /home/ec2-user/.bashrc
 
-yum install gcc-c++ # needed for node-gyp
+sudo yum install gcc-c++ -y # needed for node-gyp by mud
+
+# install go
+sudo yum install golang -y
+
+# install protoc
+sudo yum install -y protobuf-compiler
+
+# install foundry
+curl -L https://foundry.paradigm.xyz | bash
+source /home/ec2-user/.bashrc
+foundryup
 
 # install our mud fork
 cd ~
@@ -19,3 +30,15 @@ git clone https://github.com/tenetxyz/mud
 cd mud
 pnpm install
 pnpm build
+
+# install yarn
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
+sudo yum install yarn -y
+
+
+# install packages for our project
+cd ~/voxel-aw
+yarn set version stable
+echo "nodeLinker: node-modules" >> .yarnrc.yml
+yarn install

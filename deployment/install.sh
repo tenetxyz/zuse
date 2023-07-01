2965cfda7c020d44f556db913f84d48098cb1ecd
Curtis Chong <curtischong5@gmail.com>
2023-07-01 14:44:28 -0400

    install node

--- deployment/install.sh
+++ deployment/install.sh
@@ -2,6 +2,11 @@
 
 # downloads and installs our entire repo on the prod server.
 
+# install nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh # activate nvm
+nvm install --lts
+
 # install pnpm
 wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
 source /home/ec2-user/.bashrc

9d9f0aaec658a222acc43e609ee4068fa42a93d8
Curtis Chong <curtischong5@gmail.com>
2023-07-02 18:13:17 -0400

    fixed text offset

--- packages/client/src/layers/noa/systems/renderFloatingText.ts
+++ packages/client/src/layers/noa/systems/renderFloatingText.ts
@@ -37,7 +37,7 @@ const fontWidth = 70;
 const backgroundPadding = 30;
 
 // Create the dynamic texture
-export const renderFloatingTextAboveCoord = (coord1: VoxelCoord, noa: Engine, text: string) => {
+export const renderFloatingTextAboveCoord = (coord: VoxelCoord, noa: Engine, text: string) => {
   const { longestLine, numLines } = determineTextSize(text);
   const lines = text.split("\n");
 
@@ -57,22 +57,30 @@ export const renderFloatingTextAboveCoord = (coord1: VoxelCoord, noa: Engine, te
   const backgroundHeight = numLines * fontHeight;
 
   const backgroundWidth = longestLineWidth + backgroundPadding * 2;
+
+  const offsetX = textureLength / 2 - longestLineWidth / 2;
   textureContext.fillStyle = "#414141";
-  textureContext.fillRect(0, 0, backgroundWidth, backgroundHeight + backgroundPadding * 2);
+  textureContext.fillRect(offsetX, 0, backgroundWidth, backgroundHeight + backgroundPadding * 2);
 
   // Draw text
   textureContext.fillStyle = "white";
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i];
     const textWidth = textureContext.measureText(line).width;
-    textureContext.fillText(line, backgroundWidth / 2 - textWidth / 2, fontHeight * (i + 1) + backgroundPadding / 2);
+
+    // we need to do i+1 for the y-axis, cause for some reason, the fonts are drawn from the bottom up
+    textureContext.fillText(
+      line,
+      offsetX + backgroundWidth / 2 - textWidth / 2,
+      fontHeight * (i + 1) + backgroundPadding / 2
+    );
   }
 
   // Update the dynamic texture
   dynamicTexture.update();
 
   // Create a plane and apply the dynamic texture using MeshBuilder
-  const plane = MeshBuilder.CreatePlane("plane", {}, scene);
+  const plane = MeshBuilder.CreatePlane("plane", { width: 1, height: 1 }, scene);
   const material = new StandardMaterial("Mat", scene);
   material.diffuseTexture = dynamicTexture;
   plane.material = material;
@@ -80,12 +88,12 @@ export const renderFloatingTextAboveCoord = (coord1: VoxelCoord, noa: Engine, te
   // Rotate plane to face camera
   plane.billboardMode = Mesh.BILLBOARDMODE_ALL;
   plane.position.set(
-    // coord1.x + 1 - backgroundWidth / (2 * textureLength),
-    coord1.x + 0.5,
-    coord1.y + 1.5,
-    // coord1.z + 1 - backgroundWidth / (2 * textureLength)
-    coord1.z + 0.5
+    coord.x + 0.5,
+    // coord.x + backgroundWidth / (2 * textureLength) + 0.5,
+    coord.y + 1,
+    // coord.z + backgroundWidth / (2 * textureLength) + 0.5
+    coord.z + 0.5
   );
-  const isStatic = false; // false so it will turn to the player
+  const isStatic = false; // this is false so it will turn to the player
   noa.rendering.addMeshToScene(plane, isStatic);
 };

a58a58e52047ea7f4b7229dad22aff57d1531a2a
Curtis Chong <curtischong5@gmail.com>
2023-07-03 17:51:34 -0400

    loading almost there

--- packages/client/src/layers/noa/systems/renderEnt.ts
+++ packages/client/src/layers/noa/systems/renderEnt.ts
@@ -1,7 +1,10 @@
 import { SceneLoader } from "@babylonjs/core";
+import '@babylonjs/loaders/glTF';
 import { NoaLayer } from ".."
 import { VoxelCoord } from "@latticexyz/utils";
+// import FileLoader from "./gltfFileLoader"
 
+// sad this didn't work: https://forum.babylonjs.com/t/babylonjs-viewer-load-error-unable-to-import-meshes-from/25974/3
 export const renderEnt = (noaLayer: NoaLayer, voxelCoord: VoxelCoord) => {
     const {
         noa
@@ -9,23 +12,52 @@ export const renderEnt = (noaLayer: NoaLayer, voxelCoord: VoxelCoord) => {
     const newEntity = noa.entities.add()
     const scene = noa.rendering.getScene();
 
-    // SceneLoader.ImportMeshAsync("", './assets/models/', "table.gltf", scene)
+    console.log("is plugin available");
+    console.log(SceneLoader.IsPluginForExtensionAvailable('.gltf'));
     // SceneLoader.ImportMesh("", 'https://bafkreihdudhfdos7nfhtmi2eijqmnfpiig3qfjxr6yfxppk5hdulse5e6a.ipfs.nftstorage.link', "", scene, (res) => {
     // debugger
     // }, (err) => {
     //   debugger
     // });
-    SceneLoader.ImportMeshAsync("", "https://bafkreihdudhfdos7nfhtmi2eijqmnfpiig3qfjxr6yfxppk5hdulse5e6a.ipfs.nftstorage.link", "", scene)
+    // maybe it fails cause the url is not a .gltf file?
+    // SceneLoader.ImportMeshAsync(null, "https://bafkreihdudhfdos7nfhtmi2eijqmnfpiig3qfjxr6yfxppk5hdulse5e6a.ipfs.nftstorage.link/", "", scene)
+    // SceneLoader.ImportMeshAsync("", '../../../../public/assets/models/', "table.gltf", scene)
+    // This page helped mehttps://forum.babylonjs.com/t/loaders-doesnt-work/29223
+    // SceneLoader.ImportMeshAsync(["mesh"], 'https://bafkreihdudhfdos7nfhtmi2eijqmnfpiig3qfjxr6yfxppk5hdulse5e6a.ipfs.nftstorage.link', "", scene, null, '.gltf')
+    // SceneLoader.ImportMeshAsync(["mesh"], '../../../../public/assets/models/', "mouth.gltf", scene, null, '.gltf')
+    SceneLoader.ImportMeshAsync(["mesh"], '/assets/models/', "table.glb", scene, (onSuccess ) => {
+        console.log("success")
+        console.log(onSuccess)
+        //  for(const mesh of onSuccess.json.meshes){
+        //   mesh.position.set(voxelCoord.x, voxelCoord.y, voxelCoord.z);
+        //   noa.rendering.addMeshToScene(mesh, false);
+        //  }
+    }, '.gltf')
       .then((result) => {
         // Do something with the loaded meshes if needed
          // Access the loaded mesh
-         debugger
-         const mesh = result.meshes[0];
-
-         mesh.position.set(voxelCoord.x, voxelCoord.y, voxelCoord.z);
-         noa.rendering.addMeshToScene(mesh, false);
+         console.log("then result")
+         console.log(result)
       })
       .catch((error) => {
         console.error('Error loading glTF model:', error);
       });
+    // SceneLoader.ImportMesh("", '/assets/models/', "table.glb", scene, (success) => {
+    //   // onsuccess
+    //   console.log("success");
+    //   console.log(success)
+    // }, (progress:any) => {
+    //   // on progress
+    //   // debugger
+    //      for(const mesh of progress.json.meshes){
+    //       mesh.position.set(voxelCoord.x, voxelCoord.y, voxelCoord.z);
+    //       noa.rendering.addMeshToScene(mesh, false);
+    //      }
+    //      console.log("progress");
+    //      console.log(progress);
+    // },
+    // (err) => {
+    //   debugger
+    //   console.warn(err)
+    // }, '.gltf');
 }

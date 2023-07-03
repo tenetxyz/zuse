003368ae54aa22e2335381c9cfd284a8c4b00748
Curtis Chong <curtischong5@gmail.com>
2023-07-03 19:42:42 -0400

    mesh rendering works

--- packages/client/src/layers/noa/systems/renderEnt.ts
+++ packages/client/src/layers/noa/systems/renderEnt.ts
@@ -1,4 +1,4 @@
-import { SceneLoader } from "@babylonjs/core";
+import { MeshBuilder, SceneLoader, Vector3 } from "@babylonjs/core";
 import '@babylonjs/loaders/glTF';
 import { NoaLayer } from ".."
 import { VoxelCoord } from "@latticexyz/utils";
@@ -25,23 +25,40 @@ export const renderEnt = (noaLayer: NoaLayer, voxelCoord: VoxelCoord) => {
     // This page helped mehttps://forum.babylonjs.com/t/loaders-doesnt-work/29223
     // SceneLoader.ImportMeshAsync(["mesh"], 'https://bafkreihdudhfdos7nfhtmi2eijqmnfpiig3qfjxr6yfxppk5hdulse5e6a.ipfs.nftstorage.link', "", scene, null, '.gltf')
     // SceneLoader.ImportMeshAsync(["mesh"], '../../../../public/assets/models/', "mouth.gltf", scene, null, '.gltf')
-    SceneLoader.ImportMeshAsync(["mesh"], '/assets/models/', "table.glb", scene, (onSuccess ) => {
-        console.log("success")
-        console.log(onSuccess)
-        //  for(const mesh of onSuccess.json.meshes){
-        //   mesh.position.set(voxelCoord.x, voxelCoord.y, voxelCoord.z);
-        //   noa.rendering.addMeshToScene(mesh, false);
-        //  }
-    }, '.gltf')
-      .then((result) => {
-        // Do something with the loaded meshes if needed
-         // Access the loaded mesh
-         console.log("then result")
-         console.log(result)
-      })
-      .catch((error) => {
-        console.error('Error loading glTF model:', error);
-      });
+    // SceneLoader.ImportMeshAsync("", '/assets/models/', "table.glb", scene, (onSuccess ) => {
+    // SceneLoader.ImportMesh("", '/assets/models/', "table.gltf", scene, (onSuccess ) => {
+    //     // console.log("success", onSuccess)
+    //     console.log(onSuccess)
+    //     //  for(const mesh of onSuccess.json.meshes){
+    //     //   mesh.position = new Vector3(voxelCoord.x, voxelCoord.y, voxelCoord.z);
+    //     //   noa.rendering.addMeshToScene(mesh, false);
+    //     //  }
+    // }, '.gltf')
+
+    // SceneLoader.ImportMesh("",  "https://models.babylonjs.com/Marble/marble/","marble.gltf", scene, function (newMeshes) {
+    SceneLoader.ImportMesh("", '/assets/models/', "table.gltf", scene, (newMeshes ) => {
+        // Set the target of the camera to the first imported mesh
+        // camera.target = newMeshes[0];
+        console.log("newMeshes");
+        console.log(newMeshes);
+        alert(newMeshes.length);
+        newMeshes[0].normalizeToUnitCube();
+        var i;
+        var len;
+        //for(i=0 , len=newMeshes.length; i<len ; i++){
+        var m = newMeshes[1];     
+  });
+
+
+      // .then((result) => {
+      //   // Do something with the loaded meshes if needed
+      //    // Access the loaded mesh
+      //    console.log("then result", result )
+      //   //  console.log(result)
+      // })
+      // .catch((error) => {
+      //   console.error('Error loading glTF model:', error);
+      // });
     // SceneLoader.ImportMesh("", '/assets/models/', "table.glb", scene, (success) => {
     //   // onsuccess
     //   console.log("success");

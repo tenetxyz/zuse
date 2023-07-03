04b91915771b32da5c1adf74409f17ffb86f79ca
Curtis Chong <curtischong5@gmail.com>
2023-07-03 15:59:29 -0400

    table file

--- packages/client/src/layers/noa/systems/renderEnt.ts
+++ packages/client/src/layers/noa/systems/renderEnt.ts
@@ -9,10 +9,17 @@ export const renderEnt = (noaLayer: NoaLayer, voxelCoord: VoxelCoord) => {
     const newEntity = noa.entities.add()
     const scene = noa.rendering.getScene();
 
-    SceneLoader.ImportMeshAsync(null, 'assets/models/', 'mouth.gltf', scene)
+    // SceneLoader.ImportMeshAsync("", './assets/models/', "table.gltf", scene)
+    // SceneLoader.ImportMesh("", 'https://bafkreihdudhfdos7nfhtmi2eijqmnfpiig3qfjxr6yfxppk5hdulse5e6a.ipfs.nftstorage.link', "", scene, (res) => {
+    // debugger
+    // }, (err) => {
+    //   debugger
+    // });
+    SceneLoader.ImportMeshAsync("", "https://bafkreihdudhfdos7nfhtmi2eijqmnfpiig3qfjxr6yfxppk5hdulse5e6a.ipfs.nftstorage.link", "", scene)
       .then((result) => {
         // Do something with the loaded meshes if needed
          // Access the loaded mesh
+         debugger
          const mesh = result.meshes[0];
 
          mesh.position.set(voxelCoord.x, voxelCoord.y, voxelCoord.z);

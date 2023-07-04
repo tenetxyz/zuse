739d32248b9bad8cbf753ebb13c2584ae43d8f64
Curtis Chong <curtischong5@gmail.com>
2023-07-03 20:38:24 -0400

    more mesh work

--- packages/client/src/layers/noa/systems/renderEnt.ts
+++ packages/client/src/layers/noa/systems/renderEnt.ts
@@ -1,80 +1,41 @@
-import { MeshBuilder, SceneLoader, Vector3 } from "@babylonjs/core";
-import '@babylonjs/loaders/glTF';
-import { NoaLayer } from ".."
+import { AbstractMesh, MeshBuilder, SceneLoader, Vector3 } from "@babylonjs/core";
+import "@babylonjs/loaders/glTF"; // load the glTF loader plugin
+import { NoaLayer } from "..";
 import { VoxelCoord } from "@latticexyz/utils";
-// import FileLoader from "./gltfFileLoader"
 
-// sad this didn't work: https://forum.babylonjs.com/t/babylonjs-viewer-load-error-unable-to-import-meshes-from/25974/3
 export const renderEnt = (noaLayer: NoaLayer, voxelCoord: VoxelCoord) => {
-    const {
-        noa
-    } = noaLayer;
-    const newEntity = noa.entities.add()
-    const scene = noa.rendering.getScene();
+  const { noa } = noaLayer;
+  const newEntity = noa.entities.add();
+  const scene = noa.rendering.getScene();
 
-    console.log("is plugin available");
-    console.log(SceneLoader.IsPluginForExtensionAvailable('.gltf'));
-    // SceneLoader.ImportMesh("", 'https://bafkreihdudhfdos7nfhtmi2eijqmnfpiig3qfjxr6yfxppk5hdulse5e6a.ipfs.nftstorage.link', "", scene, (res) => {
-    // debugger
-    // }, (err) => {
-    //   debugger
-    // });
-    // maybe it fails cause the url is not a .gltf file?
-    // SceneLoader.ImportMeshAsync(null, "https://bafkreihdudhfdos7nfhtmi2eijqmnfpiig3qfjxr6yfxppk5hdulse5e6a.ipfs.nftstorage.link/", "", scene)
-    // SceneLoader.ImportMeshAsync("", '../../../../public/assets/models/', "table.gltf", scene)
-    // This page helped mehttps://forum.babylonjs.com/t/loaders-doesnt-work/29223
-    // SceneLoader.ImportMeshAsync(["mesh"], 'https://bafkreihdudhfdos7nfhtmi2eijqmnfpiig3qfjxr6yfxppk5hdulse5e6a.ipfs.nftstorage.link', "", scene, null, '.gltf')
-    // SceneLoader.ImportMeshAsync(["mesh"], '../../../../public/assets/models/', "mouth.gltf", scene, null, '.gltf')
-    // SceneLoader.ImportMeshAsync("", '/assets/models/', "table.glb", scene, (onSuccess ) => {
-    // SceneLoader.ImportMesh("", '/assets/models/', "table.gltf", scene, (onSuccess ) => {
-    //     // console.log("success", onSuccess)
-    //     console.log(onSuccess)
-    //     //  for(const mesh of onSuccess.json.meshes){
-    //     //   mesh.position = new Vector3(voxelCoord.x, voxelCoord.y, voxelCoord.z);
-    //     //   noa.rendering.addMeshToScene(mesh, false);
-    //     //  }
-    // }, '.gltf')
+  // Note: it's imprtant to check that we have imported the glTF loader plugin before trying to use it
+  // If we are going to support other formats, please do a check like this when developing!
+  // console.log("is plugin available");
+  // console.log(SceneLoader.IsPluginForExtensionAvailable('.gltf'));
 
-    // SceneLoader.ImportMesh("",  "https://models.babylonjs.com/Marble/marble/","marble.gltf", scene, function (newMeshes) {
-    SceneLoader.ImportMesh("", '/assets/models/', "table.gltf", scene, (newMeshes ) => {
-        // Set the target of the camera to the first imported mesh
-        // camera.target = newMeshes[0];
-        console.log("newMeshes");
-        console.log(newMeshes);
-        alert(newMeshes.length);
-        newMeshes[0].normalizeToUnitCube();
-        var i;
-        var len;
-        //for(i=0 , len=newMeshes.length; i<len ; i++){
-        var m = newMeshes[1];     
+  interface Models {
+    [name: string]: AbstractMesh;
+  }
+
+  // This thread says to use setAbsolutePosition?
+  // https://forum.babylonjs.com/t/set-global-position-of-a-mesh/2476/2
+  const models: Models = {};
+  console.log("first");
+  console.log(scene.meshes);
+
+  // SceneLoader.ImportMesh("", "/assets/models/", "table.gltf", scene, (newMeshes) => {
+  SceneLoader.ImportMesh("", "https://models.babylonjs.com/Marble/marble/", "marble.gltf", scene, (newMeshes) => {
+    // example of rendering a mesh: https://playground.babylonjs.com/#0108NG#232
+    console.log("newMeshes", newMeshes);
+    // noa.rendering.addMeshToScene(newMeshes[0], false);
+    for (const mesh of newMeshes) {
+      // mesh.setEnabled(false); //this will hide the meshes when loaded
+      models[mesh.name] = mesh; //add them to our models-object, we will call them later via their name
+      mesh.setAbsolutePosition(new Vector3(voxelCoord.x, voxelCoord.y, voxelCoord.z));
+      // mesh.position = new Vector3(voxelCoord.x, voxelCoord.y, voxelCoord.z);
+      // noa.rendering.addMeshToScene(mesh, false);
+    }
+    console.log("second");
+    console.log(scene.meshes);
   });
-
-
-      // .then((result) => {
-      //   // Do something with the loaded meshes if needed
-      //    // Access the loaded mesh
-      //    console.log("then result", result )
-      //   //  console.log(result)
-      // })
-      // .catch((error) => {
-      //   console.error('Error loading glTF model:', error);
-      // });
-    // SceneLoader.ImportMesh("", '/assets/models/', "table.glb", scene, (success) => {
-    //   // onsuccess
-    //   console.log("success");
-    //   console.log(success)
-    // }, (progress:any) => {
-    //   // on progress
-    //   // debugger
-    //      for(const mesh of progress.json.meshes){
-    //       mesh.position.set(voxelCoord.x, voxelCoord.y, voxelCoord.z);
-    //       noa.rendering.addMeshToScene(mesh, false);
-    //      }
-    //      console.log("progress");
-    //      console.log(progress);
-    // },
-    // (err) => {
-    //   debugger
-    //   console.warn(err)
-    // }, '.gltf');
-}
+};

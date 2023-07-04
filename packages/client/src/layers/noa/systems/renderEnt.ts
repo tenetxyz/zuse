43b7f95011158bbd51e39f7acf0e44a0a027db40
Curtis Chong <curtischong5@gmail.com>
2023-07-04 10:24:15 -0400

    couldn't get it to work

--- packages/client/src/layers/noa/systems/renderEnt.ts
+++ packages/client/src/layers/noa/systems/renderEnt.ts
@@ -1,4 +1,4 @@
-import { AbstractMesh, Mesh, MeshBuilder, SceneLoader, SimplificationType, Vector3 } from "@babylonjs/core";
+import { AbstractMesh, ArcRotateCamera, Mesh, SceneLoader, Vector3 } from "@babylonjs/core";
 import "@babylonjs/loaders/glTF"; // load the glTF loader plugin
 import { NoaLayer } from "..";
 import { VoxelCoord } from "@latticexyz/utils";
@@ -29,45 +29,33 @@ export const renderEnt = (noaLayer: NoaLayer, voxelCoord: VoxelCoord) => {
   //   const camera = scene.cameras[0];
   //   console.log(camera.isInFrustum(mesh));
   // })();
-  // SceneLoader.ImportMesh("", "/assets/models/", "table.gltf", scene, (newMeshes) => {
-  //   for (let m of newMeshes) {
-  //     // const mesh = newMeshes[0];
-  //     // const mesh = newMeshes[0].clone("new mesh", scene.rootNodes()[0]);
-  //     const mesh = m as Mesh;
-  //     mesh.computeWorldMatrix(true);
-  //     mesh.normalizeToUnitCube();
-  //     mesh.optimizeIndices;
-  //     // mesh.scaling = new Vector3(25, 25, 25);
-  //     mesh.position.set(voxelCoord.x, voxelCoord.y + 1, voxelCoord.z);
-  //     const camera = scene.cameras[0];
-  //     console.log(camera.isInFrustum(mesh));
+  SceneLoader.ImportMesh(
+    "",
+    "https://models.babylonjs.com/Marble/marble/",
+    "marble.gltf",
+    scene,
+    (newMeshes) => {
+      console.log("on success");
+      // example of rendering a mesh (marble): https://playground.babylonjs.com/#0108NG#232
+      // another example (skull): https://www.babylonjs-playground.com/#1BUQD5#2
 
-  //     // mesh.isVisible = false;
-  //     // for (let index = 0; index < 2; index++) {
-  //     //   var newInstance = mesh.createInstance("i" + index);
-  //     //   newInstance.position.x = voxelCoord.x;
-  //     //   newInstance.position.z = voxelCoord.z;
-  //     //   newInstance.position.y = voxelCoord.y + 1;
-  //     // }
-  //   }
-  // example of rendering a mesh (marble): https://playground.babylonjs.com/#0108NG#232
-  // another example (skull): https://www.babylonjs-playground.com/#1BUQD5#2
-  // console.log("newMeshes", newMeshes);
-  // // noa.rendering.addMeshToScene(newMeshes[0], false);
-  // for (const mesh of newMeshes) {
-  //   // mesh.setEnabled(false); //this will hide the meshes when loaded
-  //   models[mesh.name] = mesh; //add them to our models-object, we will call them later via their name
-  //   mesh.normalizeToUnitCube();
-  //   // mesh.setAbsolutePosition(new Vector3(voxelCoord.x, voxelCoord.y, voxelCoord.z));
-  //   // mesh.position = new Vector3(voxelCoord.x, voxelCoord.y, voxelCoord.z);
-  //   mesh.position.set(voxelCoord.x, voxelCoord.y, voxelCoord.z);
-  //   noa.rendering.addMeshToScene(mesh, false);
-  //   if (mesh instanceof Mesh) {
-  //     console.log("is mesh!");
-  //   }
-  //   break;
-  // }
-  // console.log("second");
-  // console.log(scene.meshes);
-  // });
+      // on success
+      const mesh = newMeshes.find((mesh) => mesh.id === "__root__")! as Mesh; //newMeshes[0] as Mesh; // the root mesh
+      // mesh.computeWorldMatrix(true);
+      mesh.normalizeToUnitCube();
+      mesh.scaling = new Vector3(25, 25, 25);
+      mesh.position.set(voxelCoord.x, voxelCoord.y + 2, voxelCoord.z);
+      const camera = scene.cameras[0];
+      camera.update();
+      console.log(camera.isInFrustum(mesh));
+      console.log(mesh);
+      // noa.rendering.addMeshToScene(mesh, false);
+    },
+    (onProgress) => {
+      console.log("progress", onProgress);
+    },
+    (onError) => {
+      console.log("onError", onError);
+    }
+  );
 };

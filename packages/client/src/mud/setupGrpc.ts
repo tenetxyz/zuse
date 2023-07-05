5257536c9a0ae4868df3c9c8c90a248fbd237d38
Curtis Chong <curtischong5@gmail.com>
2023-07-05 09:12:12 -0400

    trying to make grpc work

--- packages/client/src/mud/setupGrpc.ts
+++ packages/client/src/mud/setupGrpc.ts
@@ -1,7 +1,6 @@
-import { Player, Attack } from "../codegen/proto/player_pb";
+import "../codegen/proto/player_pb.js";
 import { grpc } from "@improbable-eng/grpc-web";
 // import { createChannel, createClient } from "nice-grpc-web";
-
 import { PlayerServiceClient } from "../codegen/proto/PlayerServiceClientPb";
 
 const url = "http://0.0.0.0:50051";
@@ -9,14 +8,10 @@ const url = "http://0.0.0.0:50051";
 const client = new PlayerServiceClient(url, {}, { transport: grpc.WebsocketTransport() });
 
 export function attackPlayer(attackerId: string, victimId: string, damage: number) {
-  return new Promise<Player>((resolve, reject) => {
-    const req: Attack = {
-      attackerId: "",
-      victimId: "",
-      damage: 0,
-    };
-
-    client.attackPlayer(req, {}, (err, response) => {
+  return new Promise<Shard.Player>((resolve, reject) => {
+    // Create a new Attack object
+    const attack = new Shard.Attack().setAttackerId(attackerId).setVictimId(victimId).setDamage(damage);
+    client.attackPlayer(attack, {}, (err, response) => {
       if (err) {
         reject(err);
       } else {

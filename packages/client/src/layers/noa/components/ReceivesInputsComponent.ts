96918f32652b67702f65020db2955661f5bb6ae1
Curtis Chong <curtischong5@gmail.com>
2023-07-05 22:38:36 -0400

    you take a screenshot and won't fall down

--- packages/client/src/layers/noa/components/ReceivesInputsComponent.ts
+++ packages/client/src/layers/noa/components/ReceivesInputsComponent.ts
@@ -1,5 +1,6 @@
 // This file was copied over from noa: https://github.com/fenomas/noa/blob/master/src/components/receivesInputs.js
 // The reason why was we needed to modify it to support flying.
+import { Engine } from "noa-engine";
 import { IMovementState } from "./MovementComponent";
 
 export const RECEIVES_INPUTS_COMPONENT_NAME = "receivesInputs";
@@ -40,7 +41,7 @@ export default function (noa: any): System {
       for (let i = 0; i < states.length; i++) {
         const state = states[i];
         const moveState = ents.getMovement(state.__id);
-        setMovementState(moveState, inputState, camHeading);
+        setMovementState(noa, moveState, inputState, camHeading);
       }
     },
   };
@@ -52,13 +53,13 @@ export default function (noa: any): System {
  * @param camHeading number
  */
 
-function setMovementState(state: IMovementState, inputs: InputState, camHeading: number): void {
+function setMovementState(noa: Engine, state: IMovementState, inputs: InputState, camHeading: number): void {
   // this means: if the player is currently jumping and the previous frame it wasn't jumping
   // then the user pressed the jump button on this frame
-  state.isJumpPressed = inputs.jump && !state.isJumpHeld;
+  state.isJumpPressed = inputs.jump && !state.isJumpHeld && noa.container.hasPointerLock;
 
-  state.isJumpHeld = inputs.jump;
-  state.isCrouching = inputs.crouch;
+  state.isJumpHeld = inputs.jump && noa.container.hasPointerLock;
+  state.isCrouching = inputs.crouch && noa.container.hasPointerLock; // only crouch if the mouse is locked. This is so mac users can screenshot
 
   const fb = inputs.forward ? (inputs.backward ? 0 : 1) : inputs.backward ? -1 : 0;
   const rl = inputs.right ? (inputs.left ? 0 : 1) : inputs.left ? -1 : 0;

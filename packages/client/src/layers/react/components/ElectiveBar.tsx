236710d78a39fca189eb9a5dec09c10c523550cc
Curtis Chong <curtischong5@gmail.com>
2023-07-07 00:32:24 -0400

    the code can identify the spawnID by looking at the targeted voxel

--- packages/client/src/layers/react/components/ElectiveBar.tsx
+++ packages/client/src/layers/react/components/ElectiveBar.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
-import { Layers } from "../../../types";
-import { getComponentValue, setComponent } from "@latticexyz/recs";
+import { ComponentRecord, Layers } from "../../../types";
+import { Entity, getComponentValue, setComponent } from "@latticexyz/recs";
 import { useComponentValue } from "@latticexyz/react";
 
 interface Props {
@@ -20,22 +20,32 @@ export const ElectiveBar = ({ layers }: Props) => {
     },
   } = layers;
 
+  // Note: this is only a subset of the actual targetedBlock interface
+  interface TargetedBlock {
+    position: number[];
+  }
+
+  const getTargetedSpawnId = (targetedBlock: TargetedBlock): String | undefined => {
+    if (!targetedBlock) {
+      return undefined;
+    }
+    const position = targetedBlock.position;
+    // if this block is a spawn, then get the spawnId
+    const entityAtPosition = getEntityAtPosition({ x: position[0], y: position[1], z: position[2] });
+    if (!entityAtPosition) {
+      return undefined;
+    }
+    return getComponentValue(OfSpawn, entityAtPosition)?.value;
+  };
+
   useEffect(() => {
-    noa.on("targetBlockChanged", (targetedBlock: { position: number[] }) => {
-      if (!targetedBlock) {
-        return;
+    noa.on("targetBlockChanged", (targetedBlock: TargetedBlock) => {
+      const spawnId = getTargetedSpawnId(targetedBlock);
+      // TODO: get the Spawn corerdpongint o this id
+      // also change the component to be the value of htis spawn, not thespawnID
+      if (spawnId) {
+        setComponent(SpawnInFocus, SingletonEntity, { value: spawnId });
       }
-      const position = targetedBlock.position;
-      // if this block is a spawn, then get the spawnId
-      const entityAtPosition = getEntityAtPosition({ x: position[0], y: position[1], z: position[3] });
-      if (!entityAtPosition) {
-        return;
-      }
-      const spawnId = getComponentValue(OfSpawn, entityAtPosition);
-      if (!spawnId) {
-        return;
-      }
-      setComponent(SpawnInFocus, SingletonEntity, { value: entityAtPosition.toString() });
     });
   }, []);
   const spawnInFocus = useComponentValue(SpawnInFocus, SingletonEntity);

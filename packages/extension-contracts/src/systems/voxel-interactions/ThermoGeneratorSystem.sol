6ca6a302e4d63bfc30867f4b9549164869e96b98
Dhrumil Shah <dhrumil.ketul@gmail.com>
2023-07-08 16:39:12 -0400

    wip thermo

--- packages/extension-contracts/src/systems/voxel-interactions/ThermoGeneratorSystem.sol
+++ packages/extension-contracts/src/systems/voxel-interactions/ThermoGeneratorSystem.sol
@@ -20,11 +20,20 @@ contract ThermoGeneratorSystem is VoxelInteraction {
     bytes32 neighbourEntityId,
     BlockDirection neighbourBlockDirection
   ) internal override returns (bool changedEntity) {
-    // if the neighbour has temperature, we should become active
-    if (entityHasTemperature(neighbourEntityId, callerNamespace)) {
-      return true;
-    }
-    return false;
+      GeneratorData memory generatorData = Generator.get(callerNamespace, interactEntity);
+      
+      if (generatorData.sources.length == 2) {
+          // Call entityHasTemperature(source[i], callerNamespace) on both and return true if one is false
+          if(entityHasTemperature(generatorData.sources[0], callerNamespace) == false || entityHasTemperature(generatorData.sources[1], callerNamespace) == false) {
+              return true;
+          }
+          }      
+      else if (entityHasTemperature(neighbourEntityId, callerNamespace)) {
+          return true;
+      }
+      else {
+          return false;
+      }
   }
 
   function entityShouldInteract(bytes32 entityId, bytes16 callerNamespace) internal view override returns (bool) {
@@ -46,7 +55,7 @@ contract ThermoGeneratorSystem is VoxelInteraction {
     GeneratorData memory generatorData = Generator.get(callerNamespace, interactEntity);
     changedEntity = false;
     
-    TemperatureEntity[] memory tempDataEntities;
+    TemperatureEntity[] memory tempDataEntities = new TemperatureEntity[](2);
     uint256 count = 0;
 
     for (uint256 i = 0; i < neighbourEntityIds.length && count < 2; i++) {
@@ -62,10 +71,14 @@ contract ThermoGeneratorSystem is VoxelInteraction {
         }
     }
 
-    if (tempDataEntities.length == 2) {
+    if (count == 2) {
      changedEntity = handleTempDataEntities(callerNamespace, interactEntity, generatorData, tempDataEntities);
     } else {
-      changedEntity = true;
+      if (generatorData.genRate != 0 && generatorData.sources.length != 0) {
+            generatorData.genRate = 0;
+            generatorData.sources = new bytes32[](0);
+            Generator.set(callerNamespace, interactEntity, generatorData);
+      }
     }
 
     return changedEntity;
@@ -90,13 +103,11 @@ contract ThermoGeneratorSystem is VoxelInteraction {
       TemperatureAtTime.set(callerNamespace, tempDataEntities[0].entity, Temp1AtTime);
       TemperatureAtTime.set(callerNamespace, tempDataEntities[1].entity, Temp2AtTime);
 
-      changedEntity = true;
+      changedEntity = false;
     } else {
       
       TemperatureAtTimeData memory Temp1AtTimeData = TemperatureAtTime.get(callerNamespace, tempDataEntities[0].entity);
       TemperatureAtTimeData memory Temp2AtTimeData = TemperatureAtTime.get(callerNamespace, tempDataEntities[1].entity);
-
-      require(Temp1AtTimeData.lastUpdateBlock == Temp2AtTimeData.lastUpdateBlock);
 
       uint256 absoluteDifferenceAtTime;
       if(Temp1AtTimeData.temperature >= Temp2AtTimeData.temperature) {
@@ -118,17 +129,18 @@ contract ThermoGeneratorSystem is VoxelInteraction {
       bytes32[] memory sources = new bytes32[](2);
       sources[0] = tempDataEntities[0].entity;
       sources[1] = tempDataEntities[1].entity;
-      generatorData.sources = sources;
 
-      generatorData.genRate = (absoluteDifferenceAtTime + absoluteDifferenceNow) / 2;
-     
-      Generator.set(callerNamespace, interactEntity, generatorData);
+      uint256 newGenRate = (absoluteDifferenceAtTime + absoluteDifferenceNow) / 2;
 
-      changedEntity = true;
+      if (generatorData.genRate != newGenRate) {
+        generatorData.genRate = newGenRate;
+        generatorData.sources = sources;
+        Generator.set(callerNamespace, interactEntity, generatorData);
+        changedEntity = true;
+      }
     }
 
     return changedEntity;
-
   }
 
   function eventHandler(

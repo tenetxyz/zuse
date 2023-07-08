9aabb9d788e15954f02d9e491e2aded7cd7adb71
Dhrumil Shah <dhrumil.ketul@gmail.com>
2023-07-08 16:39:12 -0400

    wip thermo

---
+++ packages/extension-contracts/src/systems/voxel-interactions/ThermoGeneratorSystem.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import { SingleVoxelInteraction } from "../../prototypes/SingleVoxelInteraction.sol";
+import { IWorld } from "../../../src/codegen/world/IWorld.sol";
+import { Signal, SignalData, Powered, PoweredData, PoweredTableId, SignalSource, SignalSourceTableId } from "../../codegen/Tables.sol";
+import { BlockDirection } from "../../codegen/Types.sol";
+import { getCallerNamespace } from "@tenetxyz/contracts/src/SharedUtils.sol";
+import { registerExtension, entityIsPowered, entityIsSignal, entityIsSignalSource } from "../../Utils.sol";
+
+contract ThermoGeneratorSystem is SingleVoxelInteraction {
+  function registerInteraction() public override {
+    address world = _world();
+    registerExtension(world, "ThermoGeneratorSystem", IWorld(world).extension_ThermoGeneratorSystem_eventHandler.selector);
+  }
+
+  function onNewNeighbour(
+    bytes16 callerNamespace,
+    bytes32 interactEntity,
+    bytes32 neighbourEntityId,
+    BlockDirection neighbourBlockDirection
+  ) internal override returns (bool changedEntity) {
+    changedEntity = runSingleInteraction(callerNamespace, interactEntity, neighbourEntityId, neighbourBlockDirection);
+    return changedEntity;
+  }
+
+  function entityShouldInteract(bytes32 entityId, bytes16 callerNamespace) internal view override returns (bool) {
+    return entityIsPowered(entityId, callerNamespace);
+  }
+
+
+
+
+  function runSingleInteraction(
+    bytes16 callerNamespace,
+    bytes32 poweredEntity,
+    bytes32 compareEntity,
+    BlockDirection compareBlockDirection
+  ) internal override returns (bool changedEntity) {
+    PoweredData memory poweredData = Powered.get(callerNamespace, poweredEntity);
+    changedEntity = false;
+
+    bool compareIsSignalSource = entityIsSignalSource(compareEntity, callerNamespace);
+    bool compareIsActiveSignal = entityIsSignal(compareEntity, callerNamespace);
+    if (compareIsActiveSignal) {
+      SignalData memory compareSignalData = Signal.get(callerNamespace, compareEntity);
+      compareIsActiveSignal =
+        compareSignalData.isActive &&
+        (compareSignalData.direction == compareBlockDirection || compareBlockDirection == BlockDirection.Down);
+    }
+
+    if (poweredData.isActive) {
+      // if we're active and the source direction is the same as the compare block direction
+      // and if the compare entity is not active, we should become inactive
+      if (poweredData.direction == compareBlockDirection) {
+        if (!compareIsSignalSource && !compareIsActiveSignal) {
+          poweredData.isActive = false;
+          poweredData.direction = BlockDirection.None;
+          Powered.set(callerNamespace, poweredEntity, poweredData);
+          changedEntity = true;
+        }
+      }
+    } else {
+      // if we're not active, and the compare entity is active, we should become active
+      // compare entity could be a signal source, or it could be an active signal that's in our direction or below us
+      if (compareIsSignalSource || compareIsActiveSignal) {
+        poweredData.isActive = true;
+        poweredData.direction = compareBlockDirection;
+        Powered.set(callerNamespace, poweredEntity, poweredData);
+        changedEntity = true;
+      }
+    }
+
+    return changedEntity;
+  }
+
+  function eventHandler(
+    bytes32 centerEntityId,
+    bytes32[] memory neighbourEntityIds
+  ) public override returns (bytes32, bytes32[] memory) {
+    return super.eventHandler(centerEntityId, neighbourEntityIds);
+  }
+}

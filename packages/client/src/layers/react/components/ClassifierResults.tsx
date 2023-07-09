c973b80564a3fe07706f4f44c3547027d2253eba
Curtis Chong <curtischong5@gmail.com>
2023-07-08 22:13:54 -0400

    made the code to display successful classifies more efficient

--- packages/client/src/layers/react/components/ClassifierResults.tsx
+++ packages/client/src/layers/react/components/ClassifierResults.tsx
@@ -1,6 +1,6 @@
 import { cacheStore$ } from "@latticexyz/network/dev";
 import { Classifier } from "./ClassifierStore";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useObservableValue } from "@latticexyz/react";
 import { to256BitString, unpackTuple } from "@latticexyz/utils";
 import { jsonStringifyWithBigInt } from "../../../utils/encodeOrDecode";
@@ -26,38 +26,53 @@ export const ClassifierResults = ({ layers, classifier }: Props) => {
     },
   } = layers;
   //   useObservableValue(cacheStore$);
+  const storeEvent = useRef<any>(undefined);
   const [results, setResults] = useState<ClassifierResult[]>([]);
+
   useEffect(() => {
-    const classifierResultTableKey = `TableId<${classifier.namespace}:${classifier.classificationResultTableName}>`;
-    const subscription = cacheStore$.subscribe((storeEvent) => {
-      if (!storeEvent) {
-        return;
-      }
-      const componentIndex = storeEvent.components.indexOf(classifierResultTableKey);
-      // TODO: this is so inefficient since the client needs to loop through all entities in the world just to find the ones that have the classifier
-      const cacheStoreKeys = Array.from(storeEvent.state.keys()).filter((key) => {
-        const [component] = unpackTuple(key);
-        return component === componentIndex;
-      });
-
-      // TODO: should we remove all numeric keys in the json object? We should test more. If they are just duplicates of the keys (that are strings), then we should remove them
-      const records = cacheStoreKeys
-        .map((cacheStoreKey) => [cacheStoreKey, jsonStringifyWithBigInt(storeEvent.state.get(cacheStoreKey) ?? "")])
-        .filter(([_, record]) => record !== "")
-        .map(([cacheStoreKey, record]) => {
-          const [_componentIndex, entityIndex] = unpackTuple(cacheStoreKey);
-          const creationId = storeEvent.entities[entityIndex];
-          const creation = getComponentValue(Creation, stringToEntity(to256BitString(creationId.toString())));
-          return {
-            creation,
-            record,
-          };
-        });
-
-      setResults(records as ClassifierResult[]);
+    const subscription = cacheStore$.subscribe((event) => {
+      storeEvent.current = event;
     });
     return () => subscription.unsubscribe();
-  }, [cacheStore$, classifier]);
+  }, [cacheStore$]);
+
+  useEffect(() => {
+    const classifierResultTableKey = `TableId<${classifier.namespace}:${classifier.classificationResultTableName}>`;
+    if (!storeEvent.current) {
+      return;
+    }
+    const componentIndex = storeEvent.current.componentToIndex.get(classifierResultTableKey);
+    if (!componentIndex) {
+      // console.warn(`cannot find component index for classifier result table=${classifierResultTableKey}`);
+      // this case may happen if NOBODY has submitted to the classifier yet (cause by default, the table only seen by the client if it has records in it)
+      setResults([]);
+      return;
+    }
+    // TODO: this is so inefficient since the client needs to loop through all entities in the world just to find the ones that have the classifier
+    const cacheStoreKeys = Array.from(storeEvent.current.state.keys()).filter((key) => {
+      const [component] = unpackTuple(key);
+      return component === componentIndex;
+    });
+
+    // TODO: should we remove all numeric keys in the json object? We should test more. If they are just duplicates of the keys (that are strings), then we should remove them
+    const records = cacheStoreKeys
+      .map((cacheStoreKey) => [
+        cacheStoreKey,
+        jsonStringifyWithBigInt(storeEvent.current.state.get(cacheStoreKey) ?? ""),
+      ])
+      .filter(([_, record]) => record !== "")
+      .map(([cacheStoreKey, record]) => {
+        const [_componentIndex, entityIndex] = unpackTuple(cacheStoreKey);
+        const creationId = storeEvent.current.entities[entityIndex];
+        const creation = getComponentValue(Creation, stringToEntity(to256BitString(creationId.toString())));
+        return {
+          creation,
+          record,
+        };
+      });
+
+    setResults(records as ClassifierResult[]);
+  }, [classifier]);
 
   return (
     <div>

5c0dede3c8f06330f1974680cffd0ee747078135
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-07-09 15:55:05 -0400

    fix overflow

--- packages/client/src/layers/react/components/CreationStore.tsx
+++ packages/client/src/layers/react/components/CreationStore.tsx
@@ -100,15 +100,29 @@ const CreationStore: React.FC<Props> = ({ layers, filters, setFilters, setShowAl
               }}
             />
           </div>
-          <div className="flex flex-col gap-5 w-full mt-5 justify-center items-center">
+          <div className="flex flex-col gap-5 mt-5 mb-4 w-full h-full justify-start items-center overflow-scroll">
             {creationsToDisplay.map((creation, idx) => {
               return (
-                <div
-                  key={"creation-" + idx}
-                  className="w-full cursor-pointer p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
-                >
+                <div key={"creation-" + idx} className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow">
                   <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{creation.name}</h5>
                   <p className="font-normal text-gray-700 leading-4">{creation.description}</p>
+                  <div className="flex mt-5 gap-2">
+                    <button
+                      type="button"
+                      onClick={() => {
+                        spawnCreation(creation);
+                      }}
+                      className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+                    >
+                      Spawn
+                    </button>
+                    <button
+                      type="button"
+                      className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                    >
+                      View Details
+                    </button>
+                  </div>
                 </div>
               );
             })}
@@ -132,9 +146,13 @@ const CreationStore: React.FC<Props> = ({ layers, filters, setFilters, setShowAl
     }
 
     return (
-      <div>
-        <hr />
-        <div className="flex w-full mt-5">
+      <div
+        className="flex w-full"
+        style={{
+          height: "140px",
+        }}
+      >
+        <div className="flex w-full h-fit">
           <button
             type="button"
             onClick={() => setRegisterNewCreation(true)}
@@ -148,44 +166,43 @@ const CreationStore: React.FC<Props> = ({ layers, filters, setFilters, setShowAl
   };
 
   return (
-    <div className="flex flex-col h-full p-4 justify-between">
-      <div>
-        <nav className="flex" aria-label="Breadcrumb">
-          <ol className="inline-flex items-center space-x-1 md:space-x-3">
-            <li>
-              <div className="flex items-center">
-                <a
-                  onClick={creationsNavClicked}
-                  className="cursor-pointer text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2"
-                >
-                  Creations
-                </a>
-              </div>
-            </li>
-            <li aria-current="page">
-              <div className="flex items-center">
-                <svg
-                  className="w-3 h-3 text-gray-400 mx-1"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 6 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m1 9 4-4-4-4"
-                  />
-                </svg>
-                <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2">{getCurrentViewName()}</span>
-              </div>
-            </li>
-          </ol>
-        </nav>
-        {renderMiddleContent()}
-      </div>
+    <div className="flex flex-col h-full p-4">
+      <nav className="flex" aria-label="Breadcrumb">
+        <ol className="inline-flex items-center space-x-1 md:space-x-3">
+          <li>
+            <div className="flex items-center">
+              <a
+                onClick={creationsNavClicked}
+                className="cursor-pointer text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2"
+              >
+                Creations
+              </a>
+            </div>
+          </li>
+          <li aria-current="page">
+            <div className="flex items-center">
+              <svg
+                className="w-3 h-3 text-gray-400 mx-1"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+              <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2">{getCurrentViewName()}</span>
+            </div>
+          </li>
+        </ol>
+      </nav>
+      {renderMiddleContent()}
+      <hr className="h-0.5 bg-gray-300 mt-4 mb-4 border-0" />
       {renderFooter()}
     </div>
   );

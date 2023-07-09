eae6bbeef15d66bc8cd07f5f3ee1d114151deea1
Curtis Chong <curtischong5@gmail.com>
2023-07-09 13:44:45 -0400

    added types to functions

--- packages/client/src/layers/noa/systems/renderFloatingText.ts
+++ packages/client/src/layers/noa/systems/renderFloatingText.ts
@@ -1,15 +1,15 @@
-import { DynamicTexture, Mesh, MeshBuilder, StandardMaterial } from "@babylonjs/core";
+import { DynamicTexture, ICanvasRenderingContext, Mesh, MeshBuilder, StandardMaterial } from "@babylonjs/core";
 import { VoxelCoord } from "@latticexyz/utils";
 import { Engine } from "noa-engine";
 
-const textureLength = 1024 * 4; // this means: that one block is subdivided into 1024 subpixels
+const textureLength = 256 * 4; // this means: that one block is subdivided into 1024 subpixels
 
 // These constants are for us to predict the size of the background
 // It's important for us to use a monospace font so we can simply multiply the width of a character by the number of characters
-const fontSize = 100 * 3;
+const fontSize = 25 * 3;
 const charHeight = fontSize;
 
-const backgroundPadding = 30 * 4;
+const backgroundPadding = 7 * 4;
 
 // Create the dynamic texture
 export const renderFloatingTextAboveCoord = (coord: VoxelCoord, noa: Engine, text: string) => {
@@ -29,6 +29,8 @@ export const renderFloatingTextAboveCoord = (coord: VoxelCoord, noa: Engine, tex
   const backgroundWidth = longestLineWidth + backgroundPadding * scaleFactor * 2;
   const backgroundHeight = totalLineHeight + backgroundPadding * scaleFactor * 2;
 
+  // Since the pivot point of the plane is in the middle of the textureLength,
+  // we need to offset the text so that the middle of the text is on the pivot point (only the x-axis is offset rn)
   const offsetX = textureLengthScaled / 2 - longestLineWidth / 2;
 
   drawBackground(textureContext, offsetX, backgroundWidth, backgroundHeight);
@@ -39,7 +41,7 @@ export const renderFloatingTextAboveCoord = (coord: VoxelCoord, noa: Engine, tex
   addTextToScene(scene, dynamicTexture, coord, noa);
 };
 
-const createDynamicTexture = (scene, textureLengthScaled) => {
+const createDynamicTexture = (scene: any, textureLengthScaled: number) => {
   const dynamicTexture = new DynamicTexture(
     "dynamic texture",
     { width: textureLengthScaled, height: textureLengthScaled },
@@ -50,12 +52,24 @@ const createDynamicTexture = (scene, textureLengthScaled) => {
   return dynamicTexture;
 };
 
-const drawBackground = (textureContext, offsetX, backgroundWidth, backgroundHeight) => {
+const drawBackground = (
+  textureContext: ICanvasRenderingContext,
+  offsetX: number,
+  backgroundWidth: number,
+  backgroundHeight: number
+) => {
   textureContext.fillStyle = "#414141";
   textureContext.fillRect(offsetX, 0, backgroundWidth, backgroundHeight);
 };
 
-const drawText = (textureContext, lines, offsetX, backgroundWidth, fontSizeScaled, scaleFactor) => {
+const drawText = (
+  textureContext: ICanvasRenderingContext,
+  lines: string[],
+  offsetX: number,
+  backgroundWidth: number,
+  fontSizeScaled: number,
+  scaleFactor: number
+) => {
   textureContext.fillStyle = "white";
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i];
@@ -63,27 +77,27 @@ const drawText = (textureContext, lines, offsetX, backgroundWidth, fontSizeScale
     textureContext.fillText(
       line,
       offsetX + backgroundWidth / 2 - textWidth / 2,
+      // we need to do i+1 for the y-axis, cause for some reason, the fonts are drawn from the bottom up
       charHeight * scaleFactor * (i + 1) + (backgroundPadding * scaleFactor) / 2
     );
   }
 };
 
-const addTextToScene = (scene, dynamicTexture, coord, noa) => {
+const addTextToScene = (scene: any, dynamicTexture: DynamicTexture, coord: VoxelCoord, noa: any) => {
   const plane = MeshBuilder.CreatePlane("plane", { width: 1, height: 1 }, scene);
   const material = new StandardMaterial("Mat", scene);
   material.diffuseTexture = dynamicTexture;
   plane.material = material;
 
-  plane.billboardMode = Mesh.BILLBOARDMODE_ALL;
+  plane.billboardMode = Mesh.BILLBOARDMODE_ALL; // Rotate plane to face camera
 
   plane.position.set(coord.x + 0.5, coord.y + 1, coord.z + 0.5);
-  const isStatic = false;
+  const isStatic = false; // this is false so it the text will turn to the player
   noa.rendering.addMeshToScene(plane, isStatic);
 };
 
 export const determineTextSize = (text: string) => {
   const lines = text.split("\n");
-  const numLines = lines.length;
   const longestLine = lines.reduce((longestLine, line) => {
     if (line.length > longestLine.length) {
       return line;

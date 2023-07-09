0a3f7e29c35eca0c6b69ae0b217dc9bb4ce85698
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-07-09 16:47:57 -0400

    classifiers list

--- packages/client/src/layers/react/components/ClassifierStore.tsx
+++ packages/client/src/layers/react/components/ClassifierStore.tsx
@@ -113,6 +113,10 @@ const ClassifierStore: React.FC<Props> = ({
     );
   };
 
+  const getCurrentViewName = () => {
+    return "";
+  };
+
   const renderInterfaceVoxelImages = (interfaceVoxels: Entity[]) => {
     return (
       <div className="flex flex-row space-x-2">
@@ -140,7 +144,12 @@ const ClassifierStore: React.FC<Props> = ({
   };
 
   return (
-    <div className="flex flex-col p-4">
+    <div
+      className="flex flex-col h-full p-4"
+      style={{
+        height: "calc(100% - 3rem)",
+      }}
+    >
       <div className="flex w-full">
         <SearchBar
           value={filters.classifierQuery}
@@ -149,13 +158,65 @@ const ClassifierStore: React.FC<Props> = ({
           }}
         />
       </div>
-      <div className="flex w-full mt-5 justify-center items-center">
+      <div className="flex w-full mt-5 flex-col items-center">
         <div
           onClick={() => setShowAllCreations(true)}
           className="w-full cursor-pointer block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
         >
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">All Creations</h5>
         </div>
+      </div>
+      <nav className="flex mt-5" aria-label="Breadcrumb">
+        <ol className="inline-flex items-center space-x-1 md:space-x-3">
+          <li>
+            <div className="flex items-center">
+              <a
+                // onClick={creationsNavClicked}
+                className="cursor-pointer text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2"
+              >
+                Classifiers
+              </a>
+            </div>
+          </li>
+          <li aria-current="page">
+            <div className="flex items-center">
+              <svg
+                className="w-3 h-3 text-gray-400 mx-1"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+              <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2">{getCurrentViewName()}</span>
+            </div>
+          </li>
+        </ol>
+      </nav>
+      <div className="flex w-full h-full mt-5 flex-col gap-5 items-center overflow-scroll">
+        {classifiersToDisplay.map((classifier, idx) => {
+          return (
+            <div key={"classifier-" + idx} className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{classifier.name}</h5>
+              <p className="font-normal text-gray-700 leading-4">{classifier.description}</p>
+              <div className="flex mt-5 gap-2">
+                <button
+                  type="button"
+                  className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                >
+                  View Details
+                </button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );

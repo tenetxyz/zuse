e5702f074f8966eddfdbacba0d45fd37bd9d8913
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-07-09 09:47:07 -0400

    add search bar

--- packages/client/src/layers/react/components/TenetSidebar.tsx
+++ packages/client/src/layers/react/components/TenetSidebar.tsx
@@ -131,7 +131,35 @@ export function registerTenetSidebar() {
               </ul>
             </div>
             <div>
-              <p>Hi</p>
+              <div className="flex w-full p-4">
+                <label className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+                <div className="relative w-full">
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg
+                      className="w-4 h-4 text-gray-500"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                      />
+                    </svg>
+                  </div>
+                  <input
+                    type="search"
+                    id="search"
+                    className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Search"
+                    required
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>

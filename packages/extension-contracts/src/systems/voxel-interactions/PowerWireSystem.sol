ccf126fb60f44e9aedf44b4bf7cf84b0001bb641
Dhrumil Shah <dhrumil.ketul@gmail.com>
2023-07-10 16:15:07 -0400

    new table

---
+++ packages/extension-contracts/src/systems/voxel-interactions/PowerWireSystem.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import { SingleVoxelInteraction } from "../../prototypes/SingleVoxelInteraction.sol";
+import { IWorld } from "../../../src/codegen/world/IWorld.sol";
+import { tabel, data } from "../../codegen/Tables.sol";
+import { BlockDirection } from "../../codegen/Types.sol";
+import { registerExtension, getOppositeDirection,  } from "../../Utils.sol";
+
+contract PowerWireSystem is SingleVoxelInteraction {
+  function registerInteraction() public override {
+    address world = _world();
+    registerExtension(world, "PowerWireSystem", IWorld(world).extension_SignalSystem_eventHandler.selector);
+  }
+
+  function entityShouldInteract(bytes32 entityId, bytes16 callerNamespace) internal view override returns (bool) {
+    return entityIsSignal(entityId, callerNamespace);
+  }
+
+  function runSingleInteraction(
+    bytes16 callerNamespace,
+    bytes32 signalEntity,
+    bytes32 compareEntity,
+    BlockDirection compareBlockDirection
+  ) internal override returns (bool changedEntity) {
+    SignalData memory signalData = Signal.get(callerNamespace, signalEntity);
+    changedEntity = false;
+
+    bool compareIsSignalSource = entityIsSignalSource(compareEntity, callerNamespace);
+    bool compareIsActiveSignal = entityIsSignal(compareEntity, callerNamespace);
+    if (compareIsActiveSignal) {
+      SignalData memory compareSignalData = Signal.get(callerNamespace, compareEntity);
+      compareIsActiveSignal =
+        compareSignalData.isActive &&
+        compareSignalData.direction != getOppositeDirection(compareBlockDirection);
+    }
+    bool compareIsActiveInvertedSignal = entityIsInvertedSignal(compareEntity, callerNamespace);
+    if (compareIsActiveInvertedSignal) {
+      InvertedSignalData memory compareInvertedSignalData = InvertedSignal.get(callerNamespace, compareEntity);
+      compareIsActiveInvertedSignal = compareInvertedSignalData.isActive;
+    }
+
+    if (signalData.isActive) {
+      // if we're active and the source direction is the same as the compare block direction
+      // and if the compare entity is not active, we should become inactive
+      if (signalData.direction == compareBlockDirection) {
+        if (!compareIsSignalSource && !compareIsActiveSignal && !compareIsActiveInvertedSignal) {
+          signalData.isActive = false;
+          signalData.direction = BlockDirection.None;
+          Signal.set(callerNamespace, signalEntity, signalData);
+          changedEntity = true;
+        }
+      }
+    } else {
+      // if we're not active, and the compare entity is active, we should become active
+      // compare entity could be a signal source, or it could be an active signal
+      if (compareIsSignalSource || compareIsActiveSignal || compareIsActiveInvertedSignal) {
+        signalData.isActive = true;
+        signalData.direction = compareBlockDirection;
+        Signal.set(callerNamespace, signalEntity, signalData);
+        changedEntity = true;
+      }
+    }
+
+    return changedEntity;
+  }
+
+  function eventHandler(
+    bytes32 centerEntityId,
+    bytes32[] memory neighbourEntityIds
+  ) public override returns (bytes32, bytes32[] memory) {
+    return super.eventHandler(centerEntityId, neighbourEntityIds);
+  }
+}

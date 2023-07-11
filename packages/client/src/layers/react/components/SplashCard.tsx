77564a3264648a71d15dba52a5c95bc48403e489
Curtis Chong <curtischong5@gmail.com>
2023-07-11 13:56:16 -0400

    added splash card

---
+++ packages/client/src/layers/react/components/SplashCard.tsx
@@ -0,0 +1,9 @@
+export const SplashCard = () => {
+  return (
+    <div className="flex flex-row">
+      {/* <p>Everlon v0.0.1</p>
+      <i className="fa-brands fa-discord"></i>
+      <i className="fa-brands fa-twitter"></i> */}
+    </div>
+  );
+};

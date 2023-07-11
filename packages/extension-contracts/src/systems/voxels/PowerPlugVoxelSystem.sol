4af0c9e5a63725ec542c08ae75bff197a4bfa479
Dhrumil Shah <dhrumil.ketul@gmail.com>
2023-07-11 18:50:44 -0400

    texture

--- packages/extension-contracts/src/systems/voxels/PowerPlugVoxelSystem.sol
+++ packages/extension-contracts/src/systems/voxels/PowerPlugVoxelSystem.sol
@@ -14,9 +14,9 @@ import { NoaBlockType } from "@tenet-contracts/src/codegen/Types.sol";
 
 bytes32 constant PowerPlugID = bytes32(keccak256("powerplug"));
 
-string constant PowerPlugTexture = "bafkreidohfeb5yddppqv6swfjs6s3g7qe44u75ogwaqkky4nolgh7bbafu";
+string constant PowerPlugTexture = "bafkreidq36bqpc6fno5vtoafgn7zhyrin2v5wkjfybhqfnrgec4pmlf5we";
 
-string constant PowerPlugUVWrap = "bafkreigx5gstl4b2fcz62dwex55mstoo7egdcsrmsox6trmiieplcuyalm";
+string constant PowerPlugUVWrap = "bafkreifdtu65gok35bevprpupxucirs2tan2k77444sl67stdhdgzwffra";
 
 contract PowerPlugVoxelSystem is VoxelType {
   function registerVoxel() public override {

002c4af57fac54dd27e5f9705dbf048d42f818d9
Curtis Chong <curtischong5@gmail.com>
2023-07-13 00:20:59 -0400

    update deployment scripts

--- deployment2/README.md
+++ deployment2/README.md
@@ -1,6 +1,8 @@
 - build the client locally
 - make deploy
-- ssh into the server, paste setup.sh onto it, and run it
+- ssh into the server and type sh run.sh (then setup the server)
+- deploy your contracts to the rpc using make run deploy
+- then go to this url: http://ec2-52-14-250-127.us-east-2.compute.amazonaws.com:8000/?worldAddress=0x5FbDB2315678afecb367f032d93F642f64180aa3
 
 ### Server-scripts
 
--- deployment2/server-scripts/run.sh
+++ deployment2/server-scripts/run.sh
@@ -5,40 +5,48 @@ setup() {
     sh setup.sh
 }
 
-run() {
-    sh run-node-and-client.sh
+run_node() {
+    anvil --block-time 1 --block-base-fee-per-gas 0 \
+    --host 0.0.0.0 --chain-id 905 --gas-limit 1000000000
+}
+
+run_client() {
+    cd ~/dist
+    python3 -m http.server
 }
 
 run_faucet() {
-    ./faucet
+    sh run-faucet.sh
 }
 
 run_snapshot() {
-    ./ecs-snapshot
+    sh run-snapshot.sh
 }
 
 run_relay() {
-    ./relay
+    sh run-relay.sh
 }
 
 # main script
 while true; do
     echo "Please choose an option from the following:"
     echo "1: Run setup"
-    echo "2: Run node and client"
-    echo "3: Run faucet"
-    echo "4: Run snapshot"
-    echo "5: Run relay"
+    echo "2: Run node"
+    echo "3: Run client"
+    echo "4: Run faucet"
+    echo "5: Run snapshot"
+    echo "6: Run relay"
     echo "q: Quit"
     read input
 
     case "$input" in
         1) setup; break ;;
-        2) run; break ;;
-        3) run_faucet; break ;;
-        4) run_snapshot; break ;;
-        5) run_relay; break ;;
+        2) run_node; break ;;
+        3) run_client; break ;;
+        4) run_faucet; break ;;
+        5) run_snapshot; break ;;
+        6) run_relay; break ;;
         q) echo "Quitting..."; break ;;
-        *) echo "Invalid option. Please enter a number from 1-5, or 'q' to quit." ;;
+        *) echo "Invalid option. Please enter a number from 1-6, or 'q' to quit." ;;
     esac
 done

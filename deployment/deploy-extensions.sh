76021c0e41c4489b4b8a4051710961cf7e80350f
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-07-13 19:44:17 -0400

    add firebase and fix deploy

--- deployment/deploy-extensions.sh
+++ deployment/deploy-extensions.sh
@@ -2,4 +2,4 @@
 
 source server_url.rc
 cd ../packages/extension-contracts
-yarn run deploy --rpc="http://$SERVER_HOST:8545" --installDefaultModules false --worldAddress 0x5FbDB2315678afecb367f032d93F642f64180aa3
+yarn run deploy --rpc="http://$SERVER_HOST:8545"

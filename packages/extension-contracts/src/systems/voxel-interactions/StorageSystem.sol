550c220fab7529b04985bc1d6e7937c2dfa8e054
Dhrumil Shah <dhrumil.ketul@gmail.com>
2023-07-15 20:30:23 -0400

    better

--- packages/extension-contracts/src/systems/voxel-interactions/StorageSystem.sol
+++ packages/extension-contracts/src/systems/voxel-interactions/StorageSystem.sol
@@ -25,8 +25,51 @@ contract StorageSystem is SingleVoxelInteraction {
     BlockDirection compareBlockDirection
   ) internal override returns (bool changedEntity) {
 
+    StorageData memory storageData = StorageData.get(callerNamespace, signalEntity);
+    changedEntity = false;
 
+    bool isSourceWire = entityIsPowerWire(compareEntity, callerNamespace) &&
+          PowerWire.get(callerNamespace, compareEntity).transferRate > 0 &&
+          PowerWire.get(callerNamespace, compareEntity).source != bytes32(0);
+    
+    bool isDestinationWire = entityIsPowerWire(compareEntity, callerNamespace) &&
+          PowerWire.get(callerNamespace, compareEntity).transferRate = 0 &&
+          PowerWire.get(callerNamespace, compareEntity).destination != bytes32(0);
 
+    if (isSourceWire && storageData.lastUpdateBlock != block.number) {
+      PowerWireData memory sourceWireData = PowerWire.get(callerNamespace, compareEntity);
+      uint256 newEnergyToStore = (sourceWireData.transferRate + storageData.lastInRate) / 2 * (block.number - storageData.lastUpdateBlock);
+      uint256 proposedEnergyStored = newEnergyToStore + storageData.energyStored;
+      
+      storageData.energyStored = (proposedEnergyStored > storageData.maxStorage) ? storageData.maxStorage : proposedEnergyStored;
+      storageData.lastInRate = sourceWireData.transferRate;
+      storageData.lastUpdateBlock = block.number;
+      Storage.set(callerNamespace, signalEntity, storageData);
+      
+      sourceWireData.destination = signalEntity;
+      PowerWire.set(callerNamespace, compareEntity, sourceWireData);
+      
+      changedEntity = true;
+    }
+
+    if (isDestinationWire && storageData.lastUpdateBlock != block.number) {
+      PowerWireData memory destWireData = PowerWire.get(callerNamespace, compareEntity);
+      uint256 validTransferRate = 2 * (storageData.energyStored / ( block.number - storageData.lastUpdateBlock ) ) - storageData.lastOutRate;
+      if (validTransferRate > destWireData.maxTransferRate) { validTransferRate = destWireData.maxTransferRate; }
+      uint256 energyToLeave = (storageData.lastOutRate + validTransferRate) / 2 * ( block.number - storageData.lastUpdateBlock );
+
+      storageData.energyStored = storageData.energyStored - energyToLeave;
+      storageData.lastOutRate = validTransferRate;
+      storageData.lastUpdateBlock = block.number;
+      Storage.set(callerNamespace, signalEntity, storageData);
+
+      destWireData.source = signalEntity;
+      destWireData.transferRate = validTransferRate;
+      PowerWire.set(callerNamespace, compareEntity, destWireData);
+
+      changedEntity = true;
+    }
+    
     }
     return changedEntity;
   }

df647dd58467f1569ccc1ec0ee7b5ae89984aee3
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-07-18 09:57:24 -0400

    Connect redstone to generator (#159)
    * connect signal to generator; add source direction to generator
    
    * fix
    
    * fix thermogen
    
    * fix

--- packages/extension-contracts/src/systems/voxel-interactions/SignalSystem.sol
+++ packages/extension-contracts/src/systems/voxel-interactions/SignalSystem.sol
@@ -3,10 +3,10 @@ pragma solidity >=0.8.0;
 
 import { SingleVoxelInteraction } from "@tenet-contracts/src/prototypes/SingleVoxelInteraction.sol";
 import { IWorld } from "../../../src/codegen/world/IWorld.sol";
-import { Signal, SignalData, InvertedSignalData, InvertedSignal, SignalSource } from "@tenet-extension-contracts/src/codegen/Tables.sol";
+import { Signal, SignalData, InvertedSignalData, InvertedSignal, SignalSource, Generator } from "@tenet-extension-contracts/src/codegen/Tables.sol";
 import { BlockDirection } from "@tenet-contracts/src/Types.sol";
 import { getOppositeDirection } from "@tenet-contracts/src/Utils.sol";
-import { registerExtension, entityIsSignal, entityIsSignalSource, entityIsInvertedSignal } from "../../Utils.sol";
+import { registerExtension, entityIsSignal, entityIsSignalSource, entityIsInvertedSignal, entityIsGenerator } from "../../Utils.sol";
 
 contract SignalSystem is SingleVoxelInteraction {
   function registerInteraction() public override {
@@ -28,6 +28,8 @@ contract SignalSystem is SingleVoxelInteraction {
     changedEntity = false;
 
     bool compareIsSignalSource = entityIsSignalSource(compareEntity, callerNamespace);
+    bool compareIsActiveGenerator = entityIsGenerator(compareEntity, callerNamespace) &&
+      Generator.get(callerNamespace, compareEntity).genRate > 0;
     bool compareIsActiveSignal = entityIsSignal(compareEntity, callerNamespace);
     if (compareIsActiveSignal) {
       SignalData memory compareSignalData = Signal.get(callerNamespace, compareEntity);
@@ -45,7 +47,12 @@ contract SignalSystem is SingleVoxelInteraction {
       // if we're active and the source direction is the same as the compare block direction
       // and if the compare entity is not active, we should become inactive
       if (signalData.direction == compareBlockDirection) {
-        if (!compareIsSignalSource && !compareIsActiveSignal && !compareIsActiveInvertedSignal) {
+        if (
+          !compareIsActiveGenerator &&
+          !compareIsSignalSource &&
+          !compareIsActiveSignal &&
+          !compareIsActiveInvertedSignal
+        ) {
           signalData.isActive = false;
           signalData.direction = BlockDirection.None;
           Signal.set(callerNamespace, signalEntity, signalData);
@@ -55,7 +62,7 @@ contract SignalSystem is SingleVoxelInteraction {
     } else {
       // if we're not active, and the compare entity is active, we should become active
       // compare entity could be a signal source, or it could be an active signal
-      if (compareIsSignalSource || compareIsActiveSignal || compareIsActiveInvertedSignal) {
+      if (compareIsActiveGenerator || compareIsSignalSource || compareIsActiveSignal || compareIsActiveInvertedSignal) {
         signalData.isActive = true;
         signalData.direction = compareBlockDirection;
         Signal.set(callerNamespace, signalEntity, signalData);
--- packages/extension-contracts/src/systems/voxel-interactions/ThermoGeneratorSystem.sol
+++ packages/extension-contracts/src/systems/voxel-interactions/ThermoGeneratorSystem.sol
@@ -8,6 +8,12 @@ import { BlockDirection } from "../../codegen/Types.sol";
 import { getCallerNamespace } from "@tenet-contracts/src/Utils.sol";
 import { registerExtension, entityIsGenerator, entityHasTemperature, entityIsPowerWire } from "../../Utils.sol";
 
+struct TemperatureEntity {
+  bytes32 entity;
+  BlockDirection entityBlockDirection;
+  TemperatureData data;
+}
+
 contract ThermoGeneratorSystem is VoxelInteraction {
   function registerInteraction() public override {
     address world = _world();
@@ -20,17 +26,23 @@ contract ThermoGeneratorSystem is VoxelInteraction {
     bytes32 neighbourEntityId,
     BlockDirection neighbourBlockDirection
   ) internal override returns (bool changedEntity) {
+    GeneratorData memory generatorData = Generator.get(callerNamespace, interactEntity);
+    bool isSourceDirection = false;
+    if (generatorData.sources.length == 2) {
+      BlockDirection[] memory sourceDirections = abi.decode(generatorData.sourceDirections, (BlockDirection[]));
+      isSourceDirection =
+        sourceDirections[0] == neighbourBlockDirection ||
+        sourceDirections[1] == neighbourBlockDirection;
+    }
+
     return
-      entityHasTemperature(neighbourEntityId, callerNamespace) || entityIsPowerWire(neighbourEntityId, callerNamespace);
+      entityHasTemperature(neighbourEntityId, callerNamespace) ||
+      entityIsPowerWire(neighbourEntityId, callerNamespace) ||
+      isSourceDirection;
   }
 
   function entityShouldInteract(bytes32 entityId, bytes16 callerNamespace) internal view override returns (bool) {
     return entityIsGenerator(entityId, callerNamespace);
-  }
-
-  struct TemperatureEntity {
-    bytes32 entity;
-    TemperatureData data;
   }
 
   function runInteraction(
@@ -46,15 +58,22 @@ contract ThermoGeneratorSystem is VoxelInteraction {
     uint256 count = 0;
     bytes32 source1 = bytes32(0);
     bytes32 source2 = bytes32(0);
-    if (generatorData.sources.length == 2) {
+    if (
+      generatorData.sources.length == 2 &&
+      entityHasTemperature(generatorData.sources[0], callerNamespace) &&
+      entityHasTemperature(generatorData.sources[1], callerNamespace)
+    ) {
       // already have sources
       TemperatureEntity memory source1Entity;
       source1Entity.entity = generatorData.sources[0];
       source1Entity.data = Temperature.get(callerNamespace, generatorData.sources[0]);
+      BlockDirection[] memory sourceDirections = abi.decode(generatorData.sourceDirections, (BlockDirection[]));
+      source1Entity.entityBlockDirection = sourceDirections[0];
       tempDataEntities[0] = source1Entity;
 
       TemperatureEntity memory source2Entity;
       source2Entity.entity = generatorData.sources[1];
+      source2Entity.entityBlockDirection = sourceDirections[1];
       source2Entity.data = Temperature.get(callerNamespace, generatorData.sources[1]);
       tempDataEntities[1] = source2Entity;
       count = 2;
@@ -65,6 +84,7 @@ contract ThermoGeneratorSystem is VoxelInteraction {
           TemperatureEntity memory tempEntity;
           tempEntity.entity = neighbourEntityIds[i];
           tempEntity.data = Temperature.get(callerNamespace, neighbourEntityIds[i]);
+          tempEntity.entityBlockDirection = neighbourEntityDirections[i];
 
           // Add it to the memory array
           tempDataEntities[count] = tempEntity;
@@ -76,9 +96,10 @@ contract ThermoGeneratorSystem is VoxelInteraction {
     if (count == 2) {
       changedEntity = handleTempDataEntities(callerNamespace, interactEntity, generatorData, tempDataEntities);
     } else {
-      if (generatorData.genRate != 0 && generatorData.sources.length != 0) {
+      if (generatorData.sources.length != 0) {
         generatorData.genRate = 0;
         generatorData.sources = new bytes32[](0);
+        generatorData.sourceDirections = abi.encode(new BlockDirection[](0));
         Generator.set(callerNamespace, interactEntity, generatorData);
       }
     }
@@ -133,11 +154,16 @@ contract ThermoGeneratorSystem is VoxelInteraction {
       sources[0] = tempDataEntities[0].entity;
       sources[1] = tempDataEntities[1].entity;
 
+      BlockDirection[] memory sourceDirections = new BlockDirection[](2);
+      sourceDirections[0] = tempDataEntities[0].entityBlockDirection;
+      sourceDirections[1] = tempDataEntities[1].entityBlockDirection;
+
       uint256 newGenRate = (absoluteDifferenceAtTime + absoluteDifferenceNow) / 2;
 
       if (generatorData.genRate != newGenRate) {
         generatorData.genRate = newGenRate;
         generatorData.sources = sources;
+        generatorData.sourceDirections = abi.encode(sourceDirections);
         Generator.set(callerNamespace, interactEntity, generatorData);
         changedEntity = true;
       }

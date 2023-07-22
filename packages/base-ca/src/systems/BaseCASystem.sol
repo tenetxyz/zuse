2b41d4f2e781785155ac1cd74d5ea4a8e8043817
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-07-22 12:48:32 -0400

    baseca gen

--- packages/base-ca/src/systems/BaseCASystem.sol
+++ packages/base-ca/src/systems/BaseCASystem.sol
@@ -3,46 +3,51 @@ pragma solidity >=0.8.0;
 
 import { System } from "@latticexyz/world/src/System.sol";
 import { hasKey } from "@latticexyz/world/src/modules/keysintable/hasKey.sol";
-import { Type, TypeData, Position, PositionData } from "@tenet-contracts/src/codegen/Tables.sol";
-import { VoxelVariantsKey } from "@tenet-contracts/src/Types.sol";
-import { BlockDirection } from "@tenet-contracts/src/Types.sol";
+import { VoxelType, Position, PositionData } from "@base-ca/src/codegen/Tables.sol";
+
+// struct VoxelCoord {
+//   int32 x;
+//   int32 y;
+//   int32 z;
+// }
 
 contract BaseCASystem is System {
-  function enterWorld(TypeData memory voxelType, VoxelCoord memory coord, bytes32 entity) public {
+  function enterWorld(bytes32 voxelTypeId, PositionData memory coord, bytes32 entity) public {
     address callerAddress = msg.sender;
 
-    Position.set(callerAddress, entity, coord);
-    VoxelVariantsKey memory voxelVariantData = updateVoxelVariant(entity);
-    Type.set(callerAddress, entity, voxelType);
+    Position.set(callerAddress, entity, PositionData({ x: coord.x, y: coord.y, z: coord.z }));
+    // VoxelVariantsKey memory voxelVariantData = updateVoxelVariant(entity);
+    bytes32 voxelVariantId;
+    VoxelType.set(callerAddress, entity, voxelTypeId, voxelVariantId);
   }
 
-  function updateVoxelVariant(bytes32 entity) public returns () {}
+  // function updateVoxelVariant(bytes32 entity) public returns () {}
 
-  function exitWorld(bytes32 entity) public {
-    address callerAddress = msg.sender;
-    // call exitWorld() in other contracts
-    BasePosition.deleteRecord(callerAddress, entity);
-    BaseType.deleteRecord(callerAddress, entity);
-  }
+  // function exitWorld(bytes32 entity) public {
+  //   address callerAddress = msg.sender;
+  //   // call exitWorld() in other contracts
+  //   BasePosition.deleteRecord(callerAddress, entity);
+  //   BaseType.deleteRecord(callerAddress, entity);
+  // }
 
-  // called by world
-  function runInteraction(
-    bytes32 interactEntity,
-    bytes32[] memory neighbourEntityIds,
-    bytes32[] memory childEntityIds,
-    bytes32[] memory parentEntityIds
-  ) public {
-    // loop over all neighbours and run interaction logic
-    // the interaction's used will can be in different namespaces
-    // just hard coded, or registered
-    runInteractionSystems(entity);
+  // // called by world
+  // function runInteraction(
+  //   bytes32 interactEntity,
+  //   bytes32[] memory neighbourEntityIds,
+  //   bytes32[] memory childEntityIds,
+  //   bytes32[] memory parentEntityIds
+  // ) public {
+  //   // loop over all neighbours and run interaction logic
+  //   // the interaction's used will can be in different namespaces
+  //   // just hard coded, or registered
+  //   runInteractionSystems(entity);
 
-    // can change type at position
-    // define valid movements
-  }
+  //   // can change type at position
+  //   // define valid movements
+  // }
 
-  function moveEntities(bytes32[] entity) public {
-    // use callerNamespace
-    // define valid movements
-  }
+  // function moveEntities(bytes32[] entity) public {
+  //   // use callerNamespace
+  //   // define valid movements
+  // }
 }

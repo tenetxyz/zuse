8a4aaf84de88585ce331bc5a56ff5e7553b998a4
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-07-23 15:07:26 -0400

    fix

--- packages/composed-ca/script/PostDeploy.s.sol
+++ packages/composed-ca/script/PostDeploy.s.sol
@@ -6,7 +6,7 @@ import { Script } from "forge-std/Script.sol";
 import { console } from "forge-std/console.sol";
 import { VoxelVariantsRegistryData } from "@tenet-registry/src/codegen/tables/VoxelVariantsRegistry.sol";
 import { NoaBlockType } from "@tenet-registry/src/codegen/Types.sol";
-// import { AirVoxelID, AirVoxelVariantID, DirtVoxelID, DirtVoxelVariantID, DirtTexture, DirtUVWrap, GrassVoxelID, GrassVoxelVariantID } from "@base-ca/src/Constants.sol";
+import { RoadVoxelID, RoadVoxelVariantID, RoadVoxelTexture, RoadVoxelUVWrap } from "@composed-ca/src/Constants.sol";
 
 address constant REGISTRY_WORLD = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
 string constant REGISTER_VOXEL_TYPE_SIG = "registerVoxelType(string,bytes32,bytes32,address)";
@@ -22,27 +22,19 @@ contract PostDeploy is Script {
 
     IWorld(worldAddress).defineVoxelTypeDefs();
 
-    // // Register Air
-    // VoxelVariantsRegistryData memory airVariant;
-    // airVariant.blockType = NoaBlockType.BLOCK;
-    // REGISTRY_WORLD.call(abi.encodeWithSignature(REGISTER_VOXEL_VARIANT_SIG, AirVoxelVariantID, airVariant));
-    // REGISTRY_WORLD.call(
-    //   abi.encodeWithSignature(REGISTER_VOXEL_TYPE_SIG, "Air", AirVoxelID, AirVoxelVariantID, worldAddress)
-    // );
-
-    // // Register Dirt
-    // VoxelVariantsRegistryData memory dirtVariant;
-    // dirtVariant.blockType = NoaBlockType.BLOCK;
-    // dirtVariant.opaque = true;
-    // dirtVariant.solid = true;
-    // string[] memory dirtMaterials = new string[](1);
-    // dirtMaterials[0] = DirtTexture;
-    // dirtVariant.materials = abi.encode(dirtMaterials);
-    // dirtVariant.uvWrap = DirtUVWrap;
-    // REGISTRY_WORLD.call(abi.encodeWithSignature(REGISTER_VOXEL_VARIANT_SIG, DirtVoxelVariantID, dirtVariant));
-    // REGISTRY_WORLD.call(
-    //   abi.encodeWithSignature(REGISTER_VOXEL_TYPE_SIG, "Dirt", DirtVoxelID, DirtVoxelVariantID, worldAddress)
-    // );
+    // Register Road
+    VoxelVariantsRegistryData memory roadVariant;
+    roadVariant.blockType = NoaBlockType.BLOCK;
+    roadVariant.opaque = true;
+    roadVariant.solid = true;
+    string[] memory roadMaterials = new string[](1);
+    roadMaterials[0] = RoadVoxelTexture;
+    roadVariant.materials = abi.encode(roadMaterials);
+    roadVariant.uvWrap = RoadVoxelUVWrap;
+    REGISTRY_WORLD.call(abi.encodeWithSignature(REGISTER_VOXEL_VARIANT_SIG, RoadVoxelVariantID, roadVariant));
+    REGISTRY_WORLD.call(
+      abi.encodeWithSignature(REGISTER_VOXEL_TYPE_SIG, "Road", RoadVoxelID, RoadVoxelVariantID, worldAddress)
+    );
 
     vm.stopBroadcast();
   }

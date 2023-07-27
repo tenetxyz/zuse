ad7c5e280b7010151ea0cf9ce8070fbd0c605529
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-07-27 19:29:37 -0400

    add looping logic

--- packages/contracts/src/systems/CASystem.sol
+++ packages/contracts/src/systems/CASystem.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.0;
 import { IStore } from "@latticexyz/store/src/IStore.sol";
 import { System } from "@latticexyz/world/src/System.sol";
 import { VoxelCoord } from "../Types.sol";
+import { hasEntity } from "@tenet-utils/src/Utils.sol";
 import { safeCall } from "@tenet-utils/src/CallUtils.sol";
 import { CAVoxelType, CAVoxelTypeData } from "@tenet-base-ca/src/codegen/tables/CAVoxelType.sol";
 import { NUM_VOXEL_NEIGHBOURS, MAX_VOXEL_NEIGHBOUR_UPDATE_DEPTH } from "../Constants.sol";
@@ -115,27 +116,68 @@ contract CASystem is System {
   }
 
   function runCA(address caAddress, uint32 scale, bytes32 entity) public {
-    // Run interaction logic
-    bytes32[] memory neighbourEntityIds = calculateNeighbourEntities(scale, entity);
-    bytes32[] memory childEntityIds = calculateChildEntities(scale, entity);
-    bytes32 parentEntity = calculateParentEntity(scale, entity);
-    bytes memory returnData = runInteraction(caAddress, entity, neighbourEntityIds, childEntityIds, parentEntity);
-    bytes32[] memory changedEntities = abi.decode(returnData, (bytes32[]));
+    bytes32[] memory centerEntitiesToCheckStack = new bytes32[](MAX_VOXEL_NEIGHBOUR_UPDATE_DEPTH);
+    uint256 centerEntitiesToCheckStackIdx = 0;
+    uint256 useStackIdx = 0;
+
+    // start with the center entity
+    centerEntitiesToCheckStack[centerEntitiesToCheckStackIdx] = entity;
+    useStackIdx = centerEntitiesToCheckStackIdx;
+
+    // Keep looping until there is no neighbour to process or we reached max depth
+    while (useStackIdx < MAX_VOXEL_NEIGHBOUR_UPDATE_DEPTH) {
+      bytes32 useCenterEntityId = centerEntitiesToCheckStack[useStackIdx];
+      bytes32[] memory useNeighbourEntities = calculateNeighbourEntities(scale, useCenterEntityId);
+      bytes32[] memory childEntityIds = calculateChildEntities(scale, useCenterEntityId);
+      bytes32 parentEntity = calculateParentEntity(scale, useCenterEntityId);
+      if (!hasEntity(useNeighbourEntities)) {
+        // if no neighbours, then we don't run any voxel interactions because there would be none
+        break;
+      }
+
+      // Run interaction logic
+      bytes memory returnData = runInteraction(
+        caAddress,
+        useCenterEntityId,
+        useNeighbourEntities,
+        childEntityIds,
+        parentEntity
+      );
+      bytes32[] memory changedEntities = abi.decode(returnData, (bytes32[]));
+
+      // If there are changed entities, we want to run voxel interactions again but with this new neighbour as the center
+      for (uint256 i; i < changedEntities.length; i++) {
+        if (uint256(changedEntities[i]) != 0) {
+          centerEntitiesToCheckStackIdx++;
+          require(
+            centerEntitiesToCheckStackIdx < MAX_VOXEL_NEIGHBOUR_UPDATE_DEPTH,
+            "VoxelInteractionSystem: Reached max depth"
+          );
+          centerEntitiesToCheckStack[centerEntitiesToCheckStackIdx] = changedEntities[i];
+        }
+      }
+
+      // at this point, we've consumed the top of the stack,
+      // so we can pop it, in this case, we just increment the stack index
+      if (centerEntitiesToCheckStackIdx > useStackIdx) {
+        useStackIdx++;
+      } else {
+        // this means we didnt any any updates, so we can break out of the loop
+        break;
+      }
+    }
+
     // Update VoxelType and Position at this level to match the CA
-    for (uint256 i = 0; i < changedEntities.length; i++) {
-      bytes32 changedEntity = changedEntities[i];
+    // Go through all the center entities that had an event run, and run its variant selector
+    for (uint256 i = 0; i <= centerEntitiesToCheckStackIdx; i++) {
+      bytes32 changedEntity = centerEntitiesToCheckStack[i];
       if (changedEntity == 0) {
         continue;
       }
 
       CAVoxelTypeData memory changedEntityVoxelType = CAVoxelType.get(IStore(caAddress), _world(), changedEntity);
       // Update VoxelType
-      VoxelType.set(
-        scale,
-        changedEntities[i],
-        changedEntityVoxelType.voxelTypeId,
-        changedEntityVoxelType.voxelVariantId
-      );
+      VoxelType.set(scale, changedEntity, changedEntityVoxelType.voxelTypeId, changedEntityVoxelType.voxelVariantId);
       // TODO: Do we need this?
       // Position should not change of the entity
       // Position.set(scale, changedEntities[i], coord.x, coord.y, coord.z);

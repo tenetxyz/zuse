f6e8679335d857a2c6a91aedbc28a2d22a06c13e
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-07-26 21:40:12 -0400

    fix loading

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -4,10 +4,16 @@ import { App } from "./App";
 import { setup } from "./mud/setup";
 import { MUDProvider } from "./MUDContext";
 import "@fontsource/fira-sans";
+import { BootScreen } from "./layers/react/engine";
 
 const rootElement = document.getElementById("react-root");
 if (!rootElement) throw new Error("React root not found");
 const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <BootScreen initialOpacity={1}>
+    <span className="text-white">Connecting</span>
+  </BootScreen>
+);
 
 // TODO: figure out if we actually want this to be async or if we should render something else in the meantime
 setup().then((result) => {

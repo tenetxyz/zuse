c7655fe0c4db1bb84fb4824e5f98042a6e910ad4
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-07-27 17:11:41 -0400

    fix tunneling spot coverage

--- packages/base-ca/src/Utils.sol
+++ packages/base-ca/src/Utils.sol
@@ -3,11 +3,30 @@ pragma solidity >=0.8.0;
 
 import { BlockDirection } from "@tenet-utils/src/Types.sol";
 import { hasKey } from "@latticexyz/world/src/modules/keysintable/hasKey.sol";
+import { getKeysWithValue } from "@latticexyz/world/src/modules/keyswithvalue/getKeysWithValue.sol";
 import { CAPosition, CAPositionData, CAPositionTableId } from "@base-ca/src/codegen/Tables.sol";
 
 function getEntityPositionStrict(address callerAddress, bytes32 entity) view returns (CAPositionData memory) {
   require(hasKey(CAPositionTableId, CAPosition.encodeKeyTuple(callerAddress, entity)), "Entity must have a position"); // even if its air, it must have a position
   return CAPosition.get(callerAddress, entity);
+}
+
+function getEntityAtCoord(address callerAddress, CAPositionData memory coord) view returns (bytes32) {
+  bytes32[][] memory allEntitiesAtCoord = getKeysWithValue(
+    CAPositionTableId,
+    CAPosition.encode(coord.x, coord.y, coord.z)
+  );
+  bytes32 entity;
+  for (uint256 i = 0; i < allEntitiesAtCoord.length; i++) {
+    if (allEntitiesAtCoord[i][0] == bytes32(uint256(uint160(callerAddress)))) {
+      if (uint256(entity) != 0) {
+        revert("Found more than one entity at the same position");
+      }
+      entity = allEntitiesAtCoord[i][1];
+    }
+  }
+
+  return entity;
 }
 
 function calculateBlockDirection(
--- packages/base-ca/src/systems/ElectronSystem.sol
+++ packages/base-ca/src/systems/ElectronSystem.sol
@@ -6,6 +6,7 @@ import { VoxelInteraction } from "@base-ca/src/prototypes/VoxelInteraction.sol";
 import { BlockDirection } from "@tenet-utils/src/Types.sol";
 import { CAVoxelType, CAPosition, CAPositionData, CAPositionTableId } from "@base-ca/src/codegen/Tables.sol";
 import { AirVoxelID, DirtVoxelID, GrassVoxelID, BedrockVoxelID } from "@base-ca/src/Constants.sol";
+import { getEntityAtCoord } from "@base-ca/src/Utils.sol";
 import { safeCall } from "@tenet-utils/src/CallUtils.sol";
 
 contract ElectronSystem is VoxelInteraction {
@@ -24,16 +25,31 @@ contract ElectronSystem is VoxelInteraction {
     bytes32[] memory neighbourEntityIds,
     BlockDirection[] memory neighbourEntityDirections
   ) internal override returns (bool changedEntity) {
+    CAPositionData memory baseCoord = CAPosition.get(callerAddress, interactEntity);
     // Check if block south of us is an electron, if so revert
     for (uint8 i = 0; i < neighbourEntityIds.length; i++) {
       bytes32 neighbourEntityId = neighbourEntityIds[i];
       if (neighbourEntityId == 0) {
         continue;
       }
+      bytes32 neighbourEntityType = CAVoxelType.getVoxelTypeId(callerAddress, neighbourEntityId);
       if (neighbourEntityDirections[i] == BlockDirection.South) {
-        bytes32 neighbourEntityType = CAVoxelType.getVoxelTypeId(callerAddress, neighbourEntityId);
         if (neighbourEntityType == BedrockVoxelID) {
           revert("ElectronSystem: Cannot place electron when it's tunneling spot is already occupied");
+        }
+      } else if (neighbourEntityDirections[i] == BlockDirection.North) {
+        if (neighbourEntityType == BedrockVoxelID) {
+          revert("ElectronSystem: Cannot place electron when it's tunneling spot is already occupied");
+        }
+        CAPositionData memory neighbourCoord = CAPosition.get(callerAddress, neighbourEntityId);
+        // Check one above
+        CAPositionData memory aboveCoord = CAPositionData(neighbourCoord.x, neighbourCoord.y, neighbourCoord.z + 1);
+        bytes32 aboveEntity = getEntityAtCoord(callerAddress, aboveCoord);
+        if (aboveEntity != 0) {
+          bytes32 aboveEntityType = CAVoxelType.getVoxelTypeId(callerAddress, aboveEntity);
+          if (aboveEntityType == BedrockVoxelID) {
+            revert("ElectronSystem: Cannot place electron when it's tunneling spot is already occupied");
+          }
         }
       }
     }

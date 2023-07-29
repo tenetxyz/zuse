24615d7f7216ce601995bcde280b65c998712607
Curtis Chong <curtischong5@gmail.com>
2023-07-28 20:17:32 -0400

    level1 blocks is void-like (#193)
    * level1 blocks is void-like
    
    * dirt only appears on level2

--- packages/client/src/layers/noa/constants.ts
+++ packages/client/src/layers/noa/constants.ts
@@ -21,9 +21,10 @@ export const Textures = {
   GrassSide: getNftStorageLink("bafkreien7wqwfkckd56rehamo2riwwy5jvecm5he6dmbw2lucvh3n4w6ue"),
   Dirt: getNftStorageLink("bafkreihy3pblhqaqquwttcykwlyey3umpou57rkvtncpdrjo7mlgna53g4"),
   Bedrock: getNftStorageLink("bafkreidfo756faklwx7o4q2753rxjqx6egzpmqh2zhylxaehqalvws555a"),
-  Tile2: getNftStorageLink("bafkreieyverjnmklxj6u4lsudyjfry3ezano7xjvvx454d6xurxkw3w7cq"),
-  Tile3: getNftStorageLink("bafkreia5o4wtc2cygbmywkcglfnzmzmzfpn6g2mdljqd3bjp7k5tkgspoi"),
-  Tile4: getNftStorageLink("bafkreifw7lb4m42jw4wtkjy3zgwfr44uqkwg7uranqazei5knpkzbkexqa"),
+  Tile1: getNftStorageLink("bafkreidtehpqibhtrtxrmqwq7j6sg77ed5ie74gvnwc43a7w3ni6x5rumq"),
+  Tile3: getNftStorageLink("bafkreieyverjnmklxj6u4lsudyjfry3ezano7xjvvx454d6xurxkw3w7cq"),
+  Tile4: getNftStorageLink("bafkreia5o4wtc2cygbmywkcglfnzmzmzfpn6g2mdljqd3bjp7k5tkgspoi"),
+  Tile5: getNftStorageLink("bafkreifw7lb4m42jw4wtkjy3zgwfr44uqkwg7uranqazei5knpkzbkexqa"),
 };
 
 export const UVWraps = {
@@ -31,7 +32,8 @@ export const UVWraps = {
   Grass: getNftStorageLink("bafkreiaur4pmmnh3dts6rjtfl5f2z6ykazyuu4e2cbno6drslfelkga3yy"),
   Dirt: getNftStorageLink("bafkreifsrs64rckwnfkwcyqkzpdo3tpa2at7jhe6bw7jhevkxa7estkdnm"),
   Bedrock: getNftStorageLink("bafkreihdit6glam7sreijo7itbs7uwc2ltfeuvcfaublxf6rjo24hf6t4y"),
-  Tile2: getNftStorageLink("bafkreifu4usebe7nqjpezcrzgjqsiabvuxsffrjszxzzsvuwawtbrhw6ia"),
-  Tile3: getNftStorageLink("bafkreieypgjex23vxc5qnnbju2pjrabp6fqezaiki2icukssvpyz2ryrny"),
-  Tile4: getNftStorageLink("bafkreia52odexmenv7pcj7sm54nuu3ifylaijdckdkv7k3yxph4b6khnii"),
+  Tile1: getNftStorageLink("bafkreibakdlkrzelxxf6mo4m6qbjzhucna6aph7cf5azqntprtmyqsntki"),
+  Tile3: getNftStorageLink("bafkreifu4usebe7nqjpezcrzgjqsiabvuxsffrjszxzzsvuwawtbrhw6ia"),
+  Tile4: getNftStorageLink("bafkreieypgjex23vxc5qnnbju2pjrabp6fqezaiki2icukssvpyz2ryrny"),
+  Tile5: getNftStorageLink("bafkreia52odexmenv7pcj7sm54nuu3ifylaijdckdkv7k3yxph4b6khnii"),
 };

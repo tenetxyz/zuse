c290f45bbeb7478e718196e1521fe2822331e3ae
Curtis Chong <curtischong5@gmail.com>
2023-08-01 00:41:58 +0100

    add dummy classifier for demo (#217)

--- packages/client/src/layers/react/components/useClassifierSearch.ts
+++ packages/client/src/layers/react/components/useClassifierSearch.ts
@@ -28,90 +28,102 @@ export const useClassifierSearch = ({ layers, filters }: Props) => {
     },
   } = layers;
 
-  const allClassifiers = React.useRef<Classifier[]>([]);
-  const filteredClassifiers = React.useRef<Classifier[]>([]); // Filtered based on the specified filters. The user's search box query does NOT affect this.
-  const [classifiersToDisplay, setClassifiersToDisplay] = React.useState<Classifier[]>([]);
-  const fuse = React.useRef<Fuse<Classifier>>();
+  // const allClassifiers = React.useRef<Classifier[]>([]);
+  // const filteredClassifiers = React.useRef<Classifier[]>([]); // Filtered based on the specified filters. The user's search box query does NOT affect this.
+  // const [classifiersToDisplay, setClassifiersToDisplay] = React.useState<Classifier[]>([]);
+  // const fuse = React.useRef<Fuse<Classifier>>();
 
-  useComponentUpdate(Classifier, () => {
-    allClassifiers.current = [];
-    const classifierTable = Classifier.values;
-    classifierTable.name.forEach((name: string, classifierId) => {
-      const description = classifierTable.description.get(classifierId);
-      const creator = classifierTable.creator.get(classifierId);
-      if (!creator) {
-        console.warn("No creator found for classifier", classifierId);
-        return;
-      }
+  // useComponentUpdate(Classifier, () => {
+  //   allClassifiers.current = [];
+  //   const classifierTable = Classifier.values;
+  //   classifierTable.name.forEach((name: string, classifierId) => {
+  //     const description = classifierTable.description.get(classifierId);
+  //     const creator = classifierTable.creator.get(classifierId);
+  //     if (!creator) {
+  //       console.warn("No creator found for classifier", classifierId);
+  //       return;
+  //     }
 
-      const classifySelector = classifierTable.classifySelector.get(classifierId);
-      if (!classifySelector) {
-        console.warn("No classify selector found for classifier", classifierId);
-        return;
-      }
-      const classifierNamespace =
-        getComponentValue(FunctionSelectors, classifySelector.padEnd(66, "0") as Entity)?.namespace ??
-        "unknown namespace";
+  //     const classifySelector = classifierTable.classifySelector.get(classifierId);
+  //     if (!classifySelector) {
+  //       console.warn("No classify selector found for classifier", classifierId);
+  //       return;
+  //     }
+  //     const classifierNamespace =
+  //       getComponentValue(FunctionSelectors, classifySelector.padEnd(66, "0") as Entity)?.namespace ??
+  //       "unknown namespace";
 
-      const rawSelectorInterface = classifierTable.selectorInterface.get(classifierId);
-      const selectorInterface =
-        (rawSelectorInterface &&
-          (abiDecode(
-            "(uint256 index,bytes32 entity,string name,string desc)[]",
-            rawSelectorInterface
-          ) as InterfaceVoxel[])) ||
-        [];
-      selectorInterface.forEach((voxel) => {
-        voxel.index = parseInt(voxel.index.toString()); // We want a number here, not a BigInt
-      });
+  //     const rawSelectorInterface = classifierTable.selectorInterface.get(classifierId);
+  //     const selectorInterface =
+  //       (rawSelectorInterface &&
+  //         (abiDecode(
+  //           "(uint256 index,bytes32 entity,string name,string desc)[]",
+  //           rawSelectorInterface
+  //         ) as InterfaceVoxel[])) ||
+  //       [];
+  //     selectorInterface.forEach((voxel) => {
+  //       voxel.index = parseInt(voxel.index.toString()); // We want a number here, not a BigInt
+  //     });
 
-      allClassifiers.current.push({
-        creator: creator,
-        name: name,
-        description: description,
-        classifierId: getEntityString(classifierId),
-        classificationResultTableName: classifierTable.classificationResultTableName.get(classifierId),
-        namespace: removeTrailingNulls(hexToAscii(classifierNamespace.substring(2))), // We need to remove trailing nulls since I think the namespace is padded to 16 chars in mud
-        selectorInterface: selectorInterface,
-      } as Classifier);
-    });
+  //     allClassifiers.current.push({
+  //       creator: creator,
+  //       name: name,
+  //       description: description,
+  //       classifierId: getEntityString(classifierId),
+  //       classificationResultTableName: classifierTable.classificationResultTableName.get(classifierId),
+  //       namespace: removeTrailingNulls(hexToAscii(classifierNamespace.substring(2))), // We need to remove trailing nulls since I think the namespace is padded to 16 chars in mud
+  //       selectorInterface: selectorInterface,
+  //     } as Classifier);
+  //   });
 
-    // After we have parsed all the classifiers, apply the classifier
-    // filters to narrow down the classifiers that will be displayed.
-    applyClassifierFilters();
-  });
+  //   // After we have parsed all the classifiers, apply the classifier
+  //   // filters to narrow down the classifiers that will be displayed.
+  //   applyClassifierFilters();
+  // });
 
-  const applyClassifierFilters = () => {
-    // TODO: add classifier filters
-    // maybe people will filter classifiers based on their name, description, creator, or voxelTypes
-    filteredClassifiers.current = allClassifiers.current;
+  // const applyClassifierFilters = () => {
+  //   // TODO: add classifier filters
+  //   // maybe people will filter classifiers based on their name, description, creator, or voxelTypes
+  //   filteredClassifiers.current = allClassifiers.current;
 
-    // only the filtered classifiers can be queried
-    const options = {
-      includeScore: false,
-      // TODO: the creator is just an address. we need to replace it with a readable name
-      keys: ["name", "description", "creator"],
-    };
-    fuse.current = new Fuse(filteredClassifiers.current, options);
+  //   // only the filtered classifiers can be queried
+  //   const options = {
+  //     includeScore: false,
+  //     // TODO: the creator is just an address. we need to replace it with a readable name
+  //     keys: ["name", "description", "creator"],
+  //   };
+  //   fuse.current = new Fuse(filteredClassifiers.current, options);
 
-    queryForClassifiersToDisplay();
-  };
-  // recalculate which classifiers are in the display pool when the filters change
-  useEffect(applyClassifierFilters, [filters]);
+  //   queryForClassifiersToDisplay();
+  // };
+  // // recalculate which classifiers are in the display pool when the filters change
+  // useEffect(applyClassifierFilters, [filters]);
 
-  const queryForClassifiersToDisplay = () => {
-    if (!fuse.current) {
-      return;
-    }
-    if (filters.classifierQuery === "") {
-      setClassifiersToDisplay(filteredClassifiers.current);
-      return;
-    }
+  // const queryForClassifiersToDisplay = () => {
+  //   if (!fuse.current) {
+  //     return;
+  //   }
+  //   if (filters.classifierQuery === "") {
+  //     setClassifiersToDisplay(filteredClassifiers.current);
+  //     return;
+  //   }
 
-    const queryResult = fuse.current.search(filters.classifierQuery).map((r) => r.item);
-    setClassifiersToDisplay(queryResult);
-  };
-  React.useEffect(queryForClassifiersToDisplay, [filters.classifierQuery]);
+  //   const queryResult = fuse.current.search(filters.classifierQuery).map((r) => r.item);
+  //   setClassifiersToDisplay(queryResult);
+  // };
+  // React.useEffect(queryForClassifiersToDisplay, [filters.classifierQuery]);
+  const classifiersToDisplay: Classifier[] = [
+    {
+      name: "AND Gate",
+      description: "Classifies if this creation is an AND Gate",
+      classifierId: "0x12213123" as Entity,
+      creator: "0x1231242" as Entity,
+      functionSelector: "0x123123123",
+      classificationResultTableName: "classifiaction result table name",
+      selectorInterface: [],
+      namespace: "tenet",
+    },
+  ];
 
   return {
     classifiersToDisplay,

74103a41422f7c42a4fc3788cb4cfa8350af43a8
Curtis Chong <curtischong5@gmail.com>
2023-08-03 12:05:06 +0100

    hotreload script now rebuilds everything on first run

--- packages/contracts/hotreload-sol-files.sh
+++ packages/contracts/hotreload-sol-files.sh
@@ -2,10 +2,11 @@
 
 # NOTE: you need to run brew install fswatch to use this script
 
+yarn tablegen
+yarn worldgen
 clear && printf '\e[3J'
 echo "Listening for changes in *.sol files in the current directory tree"
-fswatch -o -r --event Updated --event Created --event Removed --include "\.sol$" --include "\.ts$" --exclude ".*" --exclude "src/codegen/*" . | while read num; do
+fswatch -o -r --event Updated --event Created --event Removed --include "\.sol$" --exclude "src/codegen/*" . | while read num; do
     clear && printf '\e[3J'
-    yarn mud tablegen
     forge build
 done

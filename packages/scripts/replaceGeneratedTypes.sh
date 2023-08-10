cc41300cfa658f07540359b717bf19dcb3f6a393
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-08-10 10:52:11 -0400

    fix types

--- packages/scripts/replaceGeneratedTypes.sh
+++ packages/scripts/replaceGeneratedTypes.sh
@@ -18,14 +18,20 @@ find "src/codegen/tables" -type f | while read -r input_file; do
   target='import { BlockDirection } from "./../Types.sol"'
   replacement='import { BlockDirection } from "@tenet-utils/src/Types.sol"'
   awk -v target="$target" -v replacement="$replacement" '{ gsub(target, replacement); print }' $input_file > temp && mv temp $input_file
-done
 
-# for table in the table/ directory, replace the generated types
-find "src/codegen/tables" -type f | while read -r input_file; do
-  echo $input_file;
   target='import { NoaBlockType } from "./../Types.sol"'
   replacement='import { NoaBlockType } from "@tenet-registry/src/codegen/Types.sol"'
   awk -v target="$target" -v replacement="$replacement" '{ gsub(target, replacement); print }' $input_file > temp && mv temp $input_file
+
+  start_pattern="struct VoxelTypeData {"
+  end_pattern="}"
+  replacement='import { VoxelTypeData } from "@tenet-utils/src/Types.sol";'
+
+  awk -v start="$start_pattern" -v end="$end_pattern" -v rep="$replacement" '
+    !p && $0 ~ start { p=1; print rep; next }
+    p && $0 ~ end { p=0; next }
+    !p
+  ' "$input_file" > "$temp_file" && mv "$temp_file" "$input_file"
 done
 
 echo "[TENET] Replaced generated types in Types.sol"

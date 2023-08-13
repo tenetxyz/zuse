e6e6bb1a2f1fbaf212b5b5e7b50de15b42805c83
Curtis Chong <curtischong5@gmail.com>
2023-08-13 15:13:36 -0400

    creations now dont have air when registered

--- packages/client/src/layers/react/components/RegisterCreation.tsx
+++ packages/client/src/layers/react/components/RegisterCreation.tsx
@@ -48,7 +48,7 @@ const RegisterCreation: React.FC<Props> = ({ layers, formData, setFormData, rese
       noa,
     },
     network: {
-      contractComponents: { OfSpawn, Position },
+      contractComponents: { OfSpawn, Position, VoxelType },
       parsedComponents: { ParsedVoxelTypeRegistry, ParsedCreationRegistry, ParsedSpawn },
       api: { getEntityAtPosition, registerCreation },
     },
@@ -151,6 +151,13 @@ const RegisterCreation: React.FC<Props> = ({ layers, formData, setFormData, rese
         }
       }
     }
+    // filter out air voxels
+    const nonAirVoxels = voxels.filter((entity) => {
+      const voxelTypeId = getComponentValueStrict(VoxelType, entity);
+      const voxelTypeDesc = ParsedVoxelTypeRegistry.getRecordStrict(voxelTypeId.voxelTypeId as Entity);
+      // This is kinda sus since no voxel can have "Air" in its name.
+      return !voxelTypeDesc.name.includes("Air");
+    });
     return voxels;
   };
 

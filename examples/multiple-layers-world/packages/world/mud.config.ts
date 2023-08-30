a55ceb71e7c703c0380efa51357ed14c2898dca7
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-08-30 17:02:49 -0600

    update real world

--- examples/multiple-layers-world/packages/world/mud.config.ts
+++ examples/multiple-layers-world/packages/world/mud.config.ts
@@ -1,23 +1,8 @@
-import { mudConfig } from "@latticexyz/world/register";
+import { tenetMudConfig } from "@tenetxyz/base-world";
 import { resolveTableId } from "@latticexyz/config";
 
-/**
- * Importing this enables "snap sync mode".
- * It allows clients to sync the latest state of the world using view functions.
- */
-import "@latticexyz/world/snapsync";
-
-export default mudConfig({
-  snapSync: true,
+export default tenetMudConfig({
   tables: {
-    WorldConfig: {
-      keySchema: {
-        voxelTypeId: "bytes32",
-      },
-      schema: {
-        caAddress: "address",
-      },
-    },
     Player: {
       keySchema: {
         player: "address",
@@ -29,59 +14,6 @@ export default mudConfig({
         lastUpdateCoord: "bytes", // VoxelCoord
       },
     },
-    VoxelType: {
-      keySchema: {
-        scale: "uint32",
-        entity: "bytes32",
-      },
-      schema: {
-        voxelTypeId: "bytes32", // TODO: rename to voxelBaseTypeId
-        voxelVariantId: "bytes32",
-      },
-    },
-    Position: {
-      keySchema: {
-        scale: "uint32",
-        entity: "bytes32",
-      },
-      schema: {
-        // VoxelCoord is removed in MUD2, so we need to manually specify x,y,z
-        x: "int32",
-        y: "int32",
-        z: "int32",
-      },
-    },
-    VoxelActivated: {
-      keySchema: {
-        player: "address",
-      },
-      schema: {
-        scale: "uint32",
-        entity: "bytes32",
-        message: "string",
-      },
-      ephemeral: true,
-    },
-    // tables for spawning
-    OfSpawn: {
-      // maps a voxel spawned in the world -> the entityId representing its spawn
-      keySchema: {
-        scale: "uint32",
-        entity: "bytes32",
-      },
-      schema: {
-        spawnId: "bytes32",
-      },
-    },
-    Spawn: {
-      schema: {
-        creationId: "bytes32", // the creation that it's a spawn of
-        isModified: "bool", // modified spawns can't be submitted to classifiers
-        lowerSouthWestCorner: "bytes", // VoxelCoord
-        voxels: "bytes", // the voxel entities that have been spawned
-      },
-    },
-    // Other World specific tables
     OwnedBy: {
       keySchema: {
         scale: "uint32",
@@ -126,44 +58,9 @@ export default mudConfig({
   },
   modules: [
     {
-      name: "UniqueEntityModule",
-      root: true,
-      args: [],
-    },
-    {
-      name: "KeysInTableModule",
-      root: true,
-      args: [resolveTableId("Position")],
-    },
-    {
-      name: "KeysInTableModule",
-      root: true,
-      args: [resolveTableId("WorldConfig")],
-    },
-    {
-      name: "KeysWithValueModule",
-      root: true,
-      args: [resolveTableId("Position")],
-    },
-    {
       name: "KeysWithValueModule",
       root: true,
       args: [resolveTableId("OwnedBy")],
-    },
-    {
-      name: "KeysWithValueModule",
-      root: true,
-      args: [resolveTableId("VoxelType")],
-    },
-    {
-      name: "KeysInTableModule",
-      root: true,
-      args: [resolveTableId("VoxelType")],
-    },
-    {
-      name: "KeysInTableModule",
-      root: true,
-      args: [resolveTableId("Spawn")],
     },
     {
       name: "KeysInTableModule",
--- examples/multiple-layers-world/packages/world/src/Constants.sol
+++ examples/multiple-layers-world/packages/world/src/Constants.sol
@@ -5,13 +5,3 @@ address constant REGISTRY_ADDRESS = 0x057ef64E23666F000b34aE31332854aCBd1c8544;
 address constant BASE_CA_ADDRESS = 0x8464135c8F25Da09e49BC8782676a84730C318bC;
 address constant LEVEL_2_CA_ADDRESS = 0x663F3ad617193148711d28f5334eE4Ed07016602;
 address constant LEVEL_3_CA_ADDRESS = 0x0116686E2291dbd5e317F47faDBFb43B599786Ef;
-
-string constant BUILD_SIG = "build(bytes32,(int32,int32,int32))";
-string constant GIFT_VOXEL_SIG = "giftVoxel(bytes16,bytes32)";
-string constant RM_ALL_OWNED_VOXELS_SIG = "removeAllOwnedVoxels()";
-
-uint256 constant NUM_VOXEL_NEIGHBOURS = 10;
-uint256 constant MAX_VOXEL_NEIGHBOUR_UPDATE_DEPTH = 100;
-
-int32 constant CHUNK_MAX_Y = 255;
-int32 constant CHUNK_MIN_Y = -63;

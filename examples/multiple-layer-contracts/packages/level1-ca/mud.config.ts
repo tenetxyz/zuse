9c558cf1ce26d83f6aa6a9270b5bac529de1c210
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-08-30 15:02:44 -0600

    fix ports

---
+++ examples/multiple-layer-contracts/packages/level1-ca/mud.config.ts
@@ -0,0 +1,79 @@
+import { mudConfig } from "@latticexyz/world/register";
+import { resolveTableId } from "@latticexyz/config";
+
+export default mudConfig({
+  namespace: "ca",
+  tables: {
+    ElectronTunnelSpot: {
+      keySchema: {
+        callerAddress: "address",
+        entity: "bytes32",
+      },
+      schema: {
+        atTop: "bool",
+        sibling: "bytes32",
+      },
+    },
+  },
+  systems: {
+    CASystem: {
+      name: "CASystem",
+      openAccess: true,
+      registerAsRoot: true,
+    },
+    CAHelperSystem: {
+      name: "CAHelperSystem",
+      openAccess: false,
+      registerAsRoot: true,
+      accessList: ["CASystem"],
+    },
+    CACallerSystem: {
+      name: "CACallerSystem",
+      openAccess: false,
+      registerAsRoot: true,
+      accessList: ["AirVoxelSystem", "ElectronVoxelSystem", "ElectronSystem"],
+    },
+  },
+  modules: [
+    {
+      name: "UniqueEntityModule",
+      root: true,
+      args: [],
+    },
+    {
+      name: "KeysWithValueModule",
+      root: true,
+      args: [resolveTableId("CAPosition")],
+    },
+    {
+      name: "KeysInTableModule",
+      root: true,
+      args: [resolveTableId("CAPosition")],
+    },
+    {
+      name: "KeysInTableModule",
+      root: true,
+      args: [resolveTableId("CAVoxelType")],
+    },
+    {
+      name: "KeysInTableModule",
+      root: true,
+      args: [resolveTableId("CAEntityMapping")],
+    },
+    {
+      name: "KeysInTableModule",
+      root: true,
+      args: [resolveTableId("CAEntityReverseMapping")],
+    },
+    {
+      name: "KeysInTableModule",
+      root: true,
+      args: [resolveTableId("CAMind")],
+    },
+    {
+      name: "KeysInTableModule",
+      root: true,
+      args: [resolveTableId("ElectronTunnelSpot")],
+    },
+  ],
+});

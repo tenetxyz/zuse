33166d5acb22b14f7e882141333a25cc62a58ae0
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-09-16 14:33:58 -0400

    add agent check

--- examples/basic-agent-world/packages/world/src/systems/AgentSystem.sol
+++ examples/basic-agent-world/packages/world/src/systems/AgentSystem.sol
@@ -4,11 +4,14 @@ import { IStore } from "@latticexyz/store/src/IStore.sol";
 import { IWorld } from "@tenet-world/src/codegen/world/IWorld.sol";
 import { hasKey } from "@latticexyz/world/src/modules/keysintable/hasKey.sol";
 import { System } from "@latticexyz/world/src/System.sol";
-import { VoxelCoord, VoxelEntity } from "@tenet-utils/src/Types.sol";
-import { OwnedBy, OwnedByTableId } from "@tenet-world/src/codegen/tables/OwnedBy.sol";
+import { VoxelType, OwnedBy, OwnedByTableId } from "@tenet-world/src/codegen/Tables.sol";
+import { VoxelCoord, VoxelEntity, InteractionSelector } from "@tenet-utils/src/Types.sol";
 import { getEntityPositionStrict } from "@tenet-base-world/src/Utils.sol";
+import { REGISTRY_ADDRESS } from "../Constants.sol";
+import { VoxelTypeRegistry, VoxelTypeRegistryData } from "@tenet-registry/src/codegen/tables/VoxelTypeRegistry.sol";
+import { getInteractionSelectors } from "@tenet-registry/src/Utils.sol";
 
-contract ClaimAgentSystem is System {
+contract AgentSystem is System {
   function claimAgent(VoxelEntity memory agentEntity) public {
     // Make sure entity has position
     getEntityPositionStrict(agentEntity);
@@ -20,6 +23,9 @@ contract ClaimAgentSystem is System {
     );
 
     // Make sure entity is an agent
+    bytes32 voxelTypeId = VoxelType.getVoxelTypeId(agentEntity.scale, agentEntity.entityId);
+    InteractionSelector[] memory interactionSelectors = getInteractionSelectors(IStore(REGISTRY_ADDRESS), voxelTypeId);
+    require(interactionSelectors.length > 1, "Not an agent");
 
     // Claim agent
     OwnedBy.set(agentEntity.scale, agentEntity.entityId, _msgSender());

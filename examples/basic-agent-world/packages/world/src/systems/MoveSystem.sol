54de5860868b32a80bde17969bcc01b6a04bf3a4
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-09-17 11:23:34 -0400

    transfer ownership on move

--- examples/basic-agent-world/packages/world/src/systems/MoveSystem.sol
+++ examples/basic-agent-world/packages/world/src/systems/MoveSystem.sol
@@ -3,10 +3,12 @@ pragma solidity >=0.8.0;
 
 import { IStore } from "@latticexyz/store/src/IStore.sol";
 import { MoveEvent } from "@tenet-base-world/src/prototypes/MoveEvent.sol";
+import { hasKey } from "@latticexyz/world/src/modules/keysintable/hasKey.sol";
 import { IWorld } from "@tenet-world/src/codegen/world/IWorld.sol";
 import { VoxelCoord, VoxelEntity } from "@tenet-utils/src/Types.sol";
 import { REGISTRY_ADDRESS } from "@tenet-world/src/Constants.sol";
 import { MoveEventData } from "@tenet-base-world/src/Types.sol";
+import { OwnedBy, OwnedByTableId } from "@tenet-world/src/codegen/Tables.sol";
 import { MoveWorldEventData } from "@tenet-world/src/Types.sol";
 
 contract MoveSystem is MoveEvent {
@@ -23,11 +25,18 @@ contract MoveSystem is MoveEvent {
     bytes4 mindSelector
   ) public returns (VoxelEntity memory, VoxelEntity memory) {
     MoveWorldEventData memory moveWorldEventData = MoveWorldEventData({ agentEntity: agentEntity });
-    return
-      move(
-        voxelTypeId,
-        newCoord,
-        abi.encode(MoveEventData({ oldCoord: oldCoord, worldData: abi.encode(moveWorldEventData) }))
-      );
+    (VoxelEntity memory oldEntity, VoxelEntity memory newEntity) = move(
+      voxelTypeId,
+      newCoord,
+      abi.encode(MoveEventData({ oldCoord: oldCoord, worldData: abi.encode(moveWorldEventData) }))
+    );
+
+    // Transfer ownership of the oldEntity to the newEntity
+    if (hasKey(OwnedByTableId, OwnedBy.encodeKeyTuple(oldEntity.scale, oldEntity.entityId))) {
+      OwnedBy.set(newEntity.scale, newEntity.entityId, OwnedBy.get(oldEntity.scale, oldEntity.entityId));
+      OwnedBy.deleteRecord(oldEntity.scale, oldEntity.entityId);
+    }
+
+    return (oldEntity, newEntity);
   }
 }

aad5d9bafe1c3e5ccc665e41940c473292363f75
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-09-18 14:08:29 -0400

    transfer energy out

--- examples/basic-conserved-world/packages/world/src/systems/BuildSystem.sol
+++ examples/basic-conserved-world/packages/world/src/systems/BuildSystem.sol
@@ -6,9 +6,12 @@ import { BuildEvent } from "@tenet-base-world/src/prototypes/BuildEvent.sol";
 import { BuildEventData } from "@tenet-base-world/src/Types.sol";
 import { OwnedBy, VoxelType, VoxelTypeProperties, BodyPhysics } from "@tenet-world/src/codegen/Tables.sol";
 import { VoxelCoord, VoxelTypeData, VoxelEntity } from "@tenet-utils/src/Types.sol";
+import { min } from "@tenet-utils/src/VoxelCoordUtils.sol";
 import { REGISTRY_ADDRESS } from "@tenet-world/src/Constants.sol";
 import { AirVoxelID } from "@tenet-level1-ca/src/Constants.sol";
 import { BuildWorldEventData } from "@tenet-world/src/Types.sol";
+
+uint256 constant MAXIMUM_ENERGY_OUT = 100;
 
 contract BuildSystem is BuildEvent {
   function getRegistryAddress() internal pure override returns (address) {
@@ -43,25 +46,67 @@ contract BuildSystem is BuildEvent {
   ) internal override {
     // Update the mass of the entity to be the type definition's mass
     uint256 bodyMass = VoxelTypeProperties.get(voxelTypeId);
-    BodyPhysics.setMass(eventVoxelEntity.scale, eventVoxelEntity.entityId, bodyMass);
+    uint32 scale = eventVoxelEntity.scale;
+    BodyPhysics.setMass(scale, eventVoxelEntity.entityId, bodyMass);
     // Calculate how much energy this operation requires
     uint256 energyToDissipate = bodyMass * 100;
-    // Go through all the neighbours and equally take from each one, up till a maximum threshold for this level
+    // Go through all the neighbours and equally take from each one, up till MAXIMUM_ENERGY_IN
     // if it runs out, go to the next set of neighbours, another radius away
     uint8 radius = 1;
+    // dissipate energy
     while (energyToDissipate > 0) {
+      if (radius > 255) {
+        revert("Ran out of neighbours to dissipate energy into");
+      }
       bytes32[] memory useNeighbourEntities = IWorld(_world()).calculateMooreNeighbourEntities(
         eventVoxelEntity,
         radius
       );
-      radius += 1;
-    }
+      uint256 numNeighboursWithValues = 0;
 
-    // level = 1
-    // while energy > 0 {
-    // get neighbours at level 1
-    // need to take from each negihbour, up to maximum threshold for this layer
-    // should take more from ones with higher gravity
-    //
+      for (uint256 i = 0; i < useNeighbourEntities.length; i++) {
+        if (useNeighbourEntities[i] != bytes32(0) && BodyPhysics.getEnergy(scale, useNeighbourEntities[i]) > 0) {
+          numNeighboursWithValues++;
+        }
+      }
+
+      if (numNeighboursWithValues == 0) {
+        radius += 1;
+        continue;
+      }
+
+      // Calculate the average amount of energy to give to each neighbor
+      // TODO: This should be based on gravity, not just a flat amount
+      uint energyPerNeighbor = energyToDissipate / numNeighboursWithValues;
+
+      for (uint i = 0; i < useNeighbourEntities.length; i++) {
+        bytes32 neighborEntity = useNeighbourEntities[i];
+        uint256 neighbourEnergy = BodyPhysics.getEnergy(scale, neighborEntity);
+        if (neighborEntity == bytes32(0) || neighbourEnergy == 0) {
+          continue;
+        }
+
+        uint256 energyToTake = min(energyPerNeighbor, MAXIMUM_ENERGY_OUT);
+        if (neighbourEnergy < energyToTake) {
+          energyToTake = neighbourEnergy;
+        }
+
+        // Transfer the energy
+        BodyPhysics.setEnergy(scale, neighborEntity, neighbourEnergy - energyToTake);
+
+        // Decrease the amount of energy left to dissipate
+        energyToDissipate -= energyToTake;
+
+        // If we have successfully dissipated all energy, exit the loop
+        if (energyToDissipate == 0) {
+          break;
+        }
+      }
+
+      // If we've gone through all neighbors and still have energy to dissipate, increase the radius
+      if (energyToDissipate > 0) {
+        radius += 1;
+      }
+    }
   }
 }
--- packages/utils/src/VoxelCoordUtils.sol
+++ packages/utils/src/VoxelCoordUtils.sol
@@ -24,6 +24,11 @@ function voxelCoordToString(VoxelCoord memory coord) pure returns (string memory
     );
 }
 
+// Helper function to get the minimum of two uints
+function min(uint a, uint b) pure returns (uint) {
+  return a < b ? a : b;
+}
+
 // Helper function to calculate the absolute value of an integer
 function abs(int x) pure returns (int) {
   if (x < 0) {

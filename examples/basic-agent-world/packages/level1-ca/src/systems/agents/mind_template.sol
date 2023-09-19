dae7c36363f95f36bc95cb8a7a842f5e0cea9ba3
Curtis Chong <curtischong5@gmail.com>
2023-09-18 22:53:46 -0400

    finished v1 mind template

--- examples/basic-agent-world/packages/level1-ca/src/systems/agents/mind_template.sol
+++ examples/basic-agent-world/packages/level1-ca/src/systems/agents/mind_template.sol
@@ -4,11 +4,7 @@ pragma solidity ^0.8.0;
 import { IWorld } from "@tenet-level1-ca/src/codegen/world/IWorld.sol";
 import { MindType } from "@tenet-base-ca/src/prototypes/MindType.sol";
 import { VoxelType } from "@tenet-base-world/src/codegen/tables/VoxelType.sol";
-import { VoxelCoord, CreationMetadata, CreationSpawns } from "@tenet-utils/src/Types.sol";
-import { registerMindIntoRegistry } from "@tenet-registry/src/Utils.sol";
-import { DecisionRuleRegistry } from "@tenet-registry/src/codegen/Tables/DecisionRuleRegistry.sol";
-import { REGISTRY_ADDRESS, FighterVoxelID } from "@tenet-level1-ca/src/Constants.sol";
-import { Mind, DecisionRule } from "@tenet-utils/src/Types.sol";
+import { REGISTRY_ADDRESS } from "@tenet-level1-ca/src/Constants.sol";
 import { bytes4ToString } from "@tenet-utils/src/StringUtils.sol";
 
 contract FighterMindSystem is MindType {
@@ -28,16 +24,15 @@ contract FighterMindSystem is MindType {
 
       address worldAddress = _world();
       //   bytes32 targetVoxelTypeId = VoxelType.getVoxelTypeId(worldAddress, 1, targetEntityId);
-      //   bytes decisionRules = DecisionRuleRegistry.get(REGISTRY_ADDRESS, targetVoxelTypeId, worldAddress, address(0));
       // TODO: codegen the decisionRule selectors here
       //   bytes4[] memory decisionRuleSelectors = new bytes4[](1);
       for (uint8 j = 0; j < rules.length; j++) {
-        DecisionRule memory rule = rules[j];
+        bytes4 decisionRuleSelector = rules[j];
 
         bytes memory mindReturnData = safeCall(
           _world(),
-          abi.encodeWithSelector(rule.decisionRuleSelector, voxelTypeId, targetEntityId),
-          string(abi.encodePacked("call decision rule selector=", bytes4ToString(rule.decisionRuleSelector)))
+          abi.encodeWithSelector(decisionRuleSelector, voxelTypeId, targetEntityId),
+          string(abi.encodePacked("call decision rule selector=", bytes4ToString(decisionRuleSelector)))
         );
         bytes4 useInteractionSelector = abi.decode(mindReturnData, (bytes4));
         if (useinteractionSelector != bytes4(0)) {

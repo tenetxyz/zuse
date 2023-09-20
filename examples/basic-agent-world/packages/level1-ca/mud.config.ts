db4109bf7b3edbeb0c7089df736df0d7b4879014
Curtis Chong <curtischong5@gmail.com>
2023-09-20 15:35:24 -0400

    add health component to fighter

--- examples/basic-agent-world/packages/level1-ca/mud.config.ts
+++ examples/basic-agent-world/packages/level1-ca/mud.config.ts
@@ -3,7 +3,16 @@ import { resolveTableId } from "@latticexyz/config";
 
 export default tenetMudConfig({
   namespace: "ca",
-  tables: {},
+  tables: {
+    Health: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: {
+        health: "uint32",
+      },
+    },
+  },
   systems: {
     CASystem: {
       name: "CASystem",

5dd2f6a1e19d977f5568e52af77be9d257dd2da2
Curtis Chong <curtischong5@gmail.com>
2023-09-20 20:53:47 -0400

    End to end fighter (#271)
    * add health component to fighter
    
    * creating the initial decision rule editor is now connected to chain
    
    * update to work with conserved world
    
    * register the default decision rule for a fighter

--- examples/basic-conserved-world/packages/level1-ca/mud.config.ts
+++ examples/basic-conserved-world/packages/level1-ca/mud.config.ts
@@ -3,7 +3,32 @@ import { resolveTableId } from "@latticexyz/config";
 
 export default tenetMudConfig({
   namespace: "ca",
-  tables: {},
+  tables: {
+    Health: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: {
+        health: "uint32",
+      },
+    },
+    Stamina: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: {
+        stamina: "uint32",
+      },
+    },
+    State: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: {
+        state: "string",
+      },
+    },
+  },
   systems: {
     CASystem: {
       name: "CASystem",

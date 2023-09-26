18c15367dfb088ed8f900dbe648ac4e0160d39c3
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-09-25 20:36:57 -0400

    pokemon

--- examples/basic-conserved-world/packages/pokemon-extension/src/Constants.sol
+++ examples/basic-conserved-world/packages/pokemon-extension/src/Constants.sol
@@ -7,3 +7,4 @@ address constant CA_ADDRESS = 0x8464135c8F25Da09e49BC8782676a84730C318bC;
 bytes32 constant EnergySourceVoxelID = bytes32(keccak256("energysource"));
 bytes32 constant SoilVoxelID = bytes32(keccak256("soil"));
 bytes32 constant PlantVoxelID = bytes32(keccak256("plant"));
+bytes32 constant PokemonVoxelID = bytes32(keccak256("pokemon"));
---
+++ examples/basic-conserved-world/packages/pokemon-extension/src/systems/agents/PokemonAgentSystem.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import { IWorld } from "@tenet-pokemon-extension/src/codegen/world/IWorld.sol";
+import { AgentType } from "@tenet-base-ca/src/prototypes/AgentType.sol";
+import { VoxelVariantsRegistryData } from "@tenet-registry/src/codegen/tables/VoxelVariantsRegistry.sol";
+import { NoaBlockType } from "@tenet-registry/src/codegen/Types.sol";
+import { hasKey } from "@latticexyz/world/src/modules/keysintable/hasKey.sol";
+import { registerVoxelVariant, registerVoxelType } from "@tenet-registry/src/Utils.sol";
+import { REGISTRY_ADDRESS, PokemonVoxelID } from "@tenet-pokemon-extension/src/Constants.sol";
+import { VoxelCoord, VoxelSelectors, InteractionSelector, ComponentDef, RangeComponent, StateComponent, ComponentType } from "@tenet-utils/src/Types.sol";
+import { getFirstCaller } from "@tenet-utils/src/Utils.sol";
+import { getCAEntityAtCoord, getCAVoxelType } from "@tenet-base-ca/src/Utils.sol";
+
+bytes32 constant PokemonVoxelVariantID = bytes32(keccak256("pokemon"));
+string constant PokemonTexture = "bafkreihpdljsgdltghxehq4cebngtugfj3pduucijxcrvcla4hoy34f7vq";
+
+contract PokemonAgentSystem is AgentType {
+  function registerBody() public override {
+    address world = _world();
+    VoxelVariantsRegistryData memory pokemonVariant;
+    pokemonVariant.blockType = NoaBlockType.MESH;
+    pokemonVariant.opaque = false;
+    pokemonVariant.solid = false;
+    pokemonVariant.frames = 1;
+    string[] memory pokemonMaterials = new string[](1);
+    pokemonMaterials[0] = PokemonTexture;
+    pokemonVariant.materials = abi.encode(pokemonMaterials);
+    registerVoxelVariant(REGISTRY_ADDRESS, PokemonVoxelVariantID, pokemonVariant);
+
+    bytes32[] memory pokemonChildVoxelTypes = new bytes32[](1);
+    pokemonChildVoxelTypes[0] = PokemonVoxelID;
+    bytes32 baseVoxelTypeId = PokemonVoxelID;
+
+    ComponentDef[] memory componentDefs = new ComponentDef[](3);
+    componentDefs[0] = ComponentDef(
+      ComponentType.RANGE,
+      "Health",
+      abi.encode(RangeComponent({ rangeStart: 0, rangeEnd: 100 }))
+    );
+    componentDefs[1] = ComponentDef(
+      ComponentType.RANGE,
+      "Stamina",
+      abi.encode(RangeComponent({ rangeStart: 0, rangeEnd: 200 }))
+    );
+    string[] memory states = new string[](3);
+    states[0] = "Running";
+    states[1] = "Defending";
+    states[2] = "Attacking";
+    componentDefs[2] = ComponentDef(ComponentType.STATE, "State", abi.encode(StateComponent(states)));
+
+    registerVoxelType(
+      REGISTRY_ADDRESS,
+      "Pokemon",
+      PokemonVoxelID,
+      baseVoxelTypeId,
+      pokemonChildVoxelTypes,
+      pokemonChildVoxelTypes,
+      PokemonVoxelVariantID,
+      VoxelSelectors({
+        enterWorldSelector: IWorld(world).ca_FighterAgentSyst_enterWorld.selector,
+        exitWorldSelector: IWorld(world).ca_FighterAgentSyst_exitWorld.selector,
+        voxelVariantSelector: IWorld(world).ca_FighterAgentSyst_variantSelector.selector,
+        activateSelector: IWorld(world).ca_FighterAgentSyst_activate.selector,
+        onNewNeighbourSelector: IWorld(world).ca_FighterAgentSyst_onNewNeighbour.selector,
+        interactionSelectors: getInteractionSelectors()
+      }),
+      abi.encode(componentDefs)
+    );
+  }
+
+  function enterWorld(VoxelCoord memory coord, bytes32 entity) public override {}
+
+  function exitWorld(VoxelCoord memory coord, bytes32 entity) public override {}
+
+  function variantSelector(
+    bytes32 entity,
+    bytes32[] memory neighbourEntityIds,
+    bytes32[] memory childEntityIds,
+    bytes32 parentEntity
+  ) public view override returns (bytes32) {
+    return PokemonVoxelVariantID;
+  }
+
+  function activate(bytes32 entity) public view override returns (string memory) {}
+
+  function onNewNeighbour(bytes32 interactEntity, bytes32 neighbourEntityId) public override {}
+
+  function getInteractionSelectors() public override returns (InteractionSelector[] memory) {
+    InteractionSelector[] memory voxelInteractionSelectors = new InteractionSelector[](3);
+    voxelInteractionSelectors[0] = InteractionSelector({
+      interactionSelector: IWorld(_world()).ca_FighterAgentSyst_moveForwardEventHandler.selector,
+      interactionName: "Move Forward",
+      interactionDescription: ""
+    });
+    // TODO: change the selectors so they point to legit contracts
+    voxelInteractionSelectors[1] = InteractionSelector({
+      interactionSelector: IWorld(_world()).ca_FighterAgentSyst_moveForwardEventHandler.selector,
+      interactionName: "Attack",
+      interactionDescription: ""
+    });
+    voxelInteractionSelectors[2] = InteractionSelector({
+      interactionSelector: IWorld(_world()).ca_FighterAgentSyst_moveForwardEventHandler.selector,
+      interactionName: "Defend",
+      interactionDescription: ""
+    });
+    return voxelInteractionSelectors;
+  }
+
+  function moveForwardEventHandler(
+    bytes32 centerEntityId,
+    bytes32[] memory neighbourEntityIds,
+    bytes32[] memory childEntityIds,
+    bytes32 parentEntity
+  ) public returns (bytes32, bytes32[] memory) {}
+}

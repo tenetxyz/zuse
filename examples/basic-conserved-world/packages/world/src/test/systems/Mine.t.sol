34f703b97f950e6d1b3e41468d55475d2e955fa6
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-09-25 22:46:35 -0400

    test mine

--- examples/basic-conserved-world/packages/world/src/test/systems/Mine.t.sol
+++ examples/basic-conserved-world/packages/world/src/test/systems/Mine.t.sol
@@ -6,6 +6,10 @@ import { MudTest } from "@latticexyz/store/src/MudTest.sol";
 import { IStore } from "@latticexyz/store/src/IStore.sol";
 import { IWorld } from "@tenet-world/src/codegen/world/IWorld.sol";
 import { VoxelType, OwnedBy } from "@tenet-world/src/codegen/Tables.sol";
+import { VoxelCoord, VoxelTypeData, VoxelEntity } from "@tenet-utils/src/Types.sol";
+import { getEntityAtCoord, getEntityPositionStrict, positionDataToVoxelCoord } from "@tenet-base-world/src/Utils.sol";
+import { FighterVoxelID, GrassVoxelID, AirVoxelID, DirtVoxelID, BedrockVoxelID } from "@tenet-level1-ca/src/Constants.sol";
+import { BodyPhysics, BodyPhysicsData } from "@tenet-world/src/codegen/tables/BodyPhysics.sol";
 
 import { addressToEntityKey } from "@tenet-utils/src/Utils.sol";
 import { console } from "forge-std/console.sol";
@@ -20,5 +24,21 @@ contract MineTest is MudTest {
     super.setUp();
     world = IWorld(worldAddress);
     store = IStore(worldAddress);
+    alice = payable(address(0x1));
+  }
+
+  function testClaimAgent() public {
+    vm.startPrank(alice);
+
+    // Claim agent
+    VoxelEntity memory agentEntity = VoxelEntity({ scale: 1, entityId: getEntityAtCoord(1, VoxelCoord(10, 2, 10)) });
+    world.claimAgent(agentEntity);
+
+    // Mine block with high energy
+    uint256 beforeEnergy = BodyPhysics.getEnergy(agentEntity.scale, agentEntity.entityId);
+    world.mineWithAgent(GrassVoxelID, VoxelCoord(10, 2, 11), agentEntity);
+    uint256 afterEnergy = BodyPhysics.getEnergy(agentEntity.scale, agentEntity.entityId);
+    assertTrue(afterEnergy > beforeEnergy);
+    vm.stopPrank();
   }
 }

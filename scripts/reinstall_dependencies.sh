d09a83f5e9329ce38b877a7286ea86b01f038cb2
Curtis Chong <curtischong5@gmail.com>
2023-09-29 14:32:36 -0400

    fixed installation (#279)
    * fixed installation
    
    * add initialization part to script

--- scripts/reinstall_dependencies.sh
+++ scripts/reinstall_dependencies.sh
@@ -2,5 +2,16 @@
 
 find . -type d -name 'node_modules' -exec rm -rf {} +
 yarn cache clean
-rm yarn.lock # yes. this is needed or else we won't update our pointers (in the yarn.lock) to the latest commit in our noa fork
+# rm yarn.lock # yes. this is needed or else we won't update our pointers (in the yarn.lock) to the latest commit in our noa fork
+
+cd scripts
+yarn install # in scripts
+cd ../ # move to root
 yarn install
+cd examples/basic-conserved-world
+yarn install
+
+
+# Now generate all the files
+# we're in basic-conserved-world
+yarn initialize

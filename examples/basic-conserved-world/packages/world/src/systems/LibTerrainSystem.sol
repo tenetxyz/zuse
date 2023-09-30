a9e8e967f6ff849ed664feb11847ca4bcea31cbb
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-09-30 17:42:40 -0400

    fix compile

--- examples/basic-conserved-world/packages/world/src/systems/LibTerrainSystem.sol
+++ examples/basic-conserved-world/packages/world/src/systems/LibTerrainSystem.sol
@@ -12,8 +12,8 @@ import { BASE_CA_ADDRESS } from "@tenet-world/src/Constants.sol";
 
 uint256 constant TOTAL_MASS_IN_CHUNK = 1000;
 uint256 constant TOTAL_ENERGY_IN_CHUNK = 1000;
-uint256 constant Y_AIR_THRESHOLD = 100;
-uint256 constant Y_GROUND_THRESHOLD = 0;
+int256 constant Y_AIR_THRESHOLD = 100;
+int256 constant Y_GROUND_THRESHOLD = 0;
 
 contract LibTerrainSystem is System {
   function getTerrainBodyPhysicsData(
@@ -57,11 +57,11 @@ contract LibTerrainSystem is System {
     if (isGround || isTerrain) {
       // Step 2: Determine the mass of the ground
       int128 massNoise = IWorld(_world()).noise2d(x, z, denom, precision);
-      mass = uint256((massNoise * int128(TOTAL_MASS_IN_CHUNK)) / (denom * 2));
+      mass = uint256((massNoise * int128(int(TOTAL_MASS_IN_CHUNK))) / (denom * 2));
 
       // Step 3: Determine the energy of the ground
       int128 energyNoise = IWorld(_world()).noise2d(x + denom, z + denom, denom, precision); // Offset coordinates for variation
-      energy = uint256((energyNoise * int128(TOTAL_ENERGY_IN_CHUNK)) / (denom * 2)); // Normalize and scale
+      energy = uint256((energyNoise * int128(int(TOTAL_ENERGY_IN_CHUNK))) / (denom * 2)); // Normalize and scale
     }
 
     return (mass, energy);
@@ -71,7 +71,7 @@ contract LibTerrainSystem is System {
   function onTerrainGen(bytes32 voxelTypeId, VoxelCoord memory coord) public {
     // address caAddress = _msgSender();
     (uint256 terrainMass, uint256 terrainEnergy) = getTerrainProperties(coord);
-    require(terrainMass == VoxelTypeProperties.get(voxelTypeId).mass, "Terrain mass does not match voxel type mass");
+    require(terrainMass == VoxelTypeProperties.get(voxelTypeId), "Terrain mass does not match voxel type mass");
   }
 
   function setTerrainSelector(VoxelCoord memory coord, address contractAddress, bytes4 terrainSelector) public {

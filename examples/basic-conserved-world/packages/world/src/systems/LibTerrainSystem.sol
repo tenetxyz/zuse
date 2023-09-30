489ff1c4415bb26c34384c5b6dcb75f0c4f8f0ac
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-09-30 17:33:47 -0400

    add basic perlin function

--- examples/basic-conserved-world/packages/world/src/systems/LibTerrainSystem.sol
+++ examples/basic-conserved-world/packages/world/src/systems/LibTerrainSystem.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0;
 
+import { IWorld } from "@tenet-world/src/codegen/world/IWorld.sol";
 import { VoxelCoord } from "@tenet-utils/src/Types.sol";
 import { System } from "@latticexyz/world/src/System.sol";
 import { AirVoxelID, GrassVoxelID, DirtVoxelID, BedrockVoxelID } from "@tenet-level1-ca/src/Constants.sol";
@@ -8,6 +9,11 @@ import { BodyPhysics, BodyPhysicsData, VoxelTypeProperties } from "@tenet-world/
 import { getTerrainVoxelId } from "@tenet-base-ca/src/CallUtils.sol";
 import { safeCall } from "@tenet-utils/src/CallUtils.sol";
 import { BASE_CA_ADDRESS } from "@tenet-world/src/Constants.sol";
+
+uint256 constant TOTAL_MASS_IN_CHUNK = 1000;
+uint256 constant TOTAL_ENERGY_IN_CHUNK = 1000;
+uint256 constant Y_AIR_THRESHOLD = 100;
+uint256 constant Y_GROUND_THRESHOLD = 0;
 
 contract LibTerrainSystem is System {
   function getTerrainBodyPhysicsData(
@@ -34,10 +40,47 @@ contract LibTerrainSystem is System {
     return (voxelTypeId, data);
   }
 
+  function getTerrainProperties(VoxelCoord memory coord) public view returns (uint256, uint256) {
+    // Use perlin noise to calculate mass and energy
+
+    // Convert VoxelCoord to int256 for use with the noise functions
+    int256 x = int256(coord.x);
+    int256 y = int256(coord.y);
+    int256 z = int256(coord.z);
+
+    // Define some scaling factors for the noise functions
+    int256 denom = 100;
+    uint8 precision = 8;
+
+    // Step 1: Determine whether we are in air or ground region
+    int128 airGroundNoise = IWorld(_world()).noise(x, y, z, denom, precision);
+    bool isAir = y > Y_AIR_THRESHOLD;
+    bool isGround = y < Y_GROUND_THRESHOLD;
+    bool isTerrain = !isAir && !isGround && airGroundNoise > 0;
+
+    uint256 mass = 0;
+    uint256 energy = 0;
+
+    if (isGround || isTerrain) {
+      // Step 2: Determine the mass of the ground
+      int128 massNoise = IWorld(_world()).noise2d(x, z, denom, precision);
+      mass = uint256((massNoise * int128(TOTAL_MASS_IN_CHUNK)) / (denom * 2));
+
+      // Step 3: Determine the energy of the ground
+      int128 energyNoise = IWorld(_world()).noise2d(x + denom, z + denom, denom, precision); // Offset coordinates for variation
+      energy = uint256((energyNoise * int128(TOTAL_ENERGY_IN_CHUNK)) / (denom * 2)); // Normalize and scale
+    }
+
+    return (mass, energy);
+  }
+
   // Called by CA's on terrain gen
   function onTerrainGen(bytes32 voxelTypeId, VoxelCoord memory coord) public {
     // address caAddress = _msgSender();
     // TODO: Check that the mass at this coord matches the mass of the voxel type
+    (uint256 terrainMass, uint256 terrainEnergy) = getTerrainProperties(coord);
+    require(terrainMass == VoxelTypeProperties.get(voxelTypeId).mass, "Terrain mass does not match voxel type mass");
+    // TODO: set BodyPhysicsData
   }
 
   function setTerrainSelector(VoxelCoord memory coord, address contractAddress, bytes4 terrainSelector) public {

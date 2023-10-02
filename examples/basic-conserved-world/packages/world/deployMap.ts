2f51093ccc33f0d79f3999a2e5a910a512821a80
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-10-02 17:36:40 -0400

    add deploy script for terrain scripts

---
+++ examples/basic-conserved-world/packages/world/deployMap.ts
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const { execSync } = require("child_process");
+
+function runForgeScript(filePath) {
+  const command = `forge script ${filePath} --sig "run(address)" 0x5FbDB2315678afecb367f032d93F642f64180aa3 --broadcast --rpc-url http://127.0.0.1:8545`;
+  try {
+    console.log(`Running command: ${command}`);
+    const stdout = execSync(command).toString();
+    console.log(`Stdout: ${stdout}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    if (error.stderr) {
+      console.error(`Stderr: ${error.stderr.toString()}`);
+    }
+  }
+}
+
+fs.readdir("script/", (err, files) => {
+  if (err) {
+    console.error(`Error reading directory: ${err.message}`);
+    return;
+  }
+
+  files.forEach((file) => {
+    if (file.startsWith("TerrainDeploy") && file.endsWith(".s.sol")) {
+      runForgeScript(`script/${file}`);
+    }
+  });
+});

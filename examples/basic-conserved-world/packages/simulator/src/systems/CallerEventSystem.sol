a090d6967c820c3dd3f9f84b9d2d2e7914f8e811
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-10-07 14:37:06 -0400

    transfer ownership during move

--- examples/basic-conserved-world/packages/simulator/src/systems/CallerEventSystem.sol
+++ examples/basic-conserved-world/packages/simulator/src/systems/CallerEventSystem.sol
@@ -5,8 +5,8 @@ import { IStore } from "@latticexyz/store/src/IStore.sol";
 import { IWorld } from "@tenet-simulator/src/codegen/world/IWorld.sol";
 import { hasKey } from "@latticexyz/world/src/modules/keysintable/hasKey.sol";
 import { System } from "@latticexyz/world/src/System.sol";
-import { Mass, MassTableId, Energy, EnergyTableId, Velocity, VelocityTableId } from "@tenet-simulator/src/codegen/Tables.sol";
-import { VoxelCoord, VoxelTypeData, VoxelEntity } from "@tenet-utils/src/Types.sol";
+import { Health, Stamina, Object, Action, ActionData, Mass, MassTableId, Energy, EnergyTableId, Velocity, VelocityTableId } from "@tenet-simulator/src/codegen/Tables.sol";
+import { VoxelCoord, VoxelTypeData, VoxelEntity, ObjectType } from "@tenet-utils/src/Types.sol";
 import { VoxelTypeRegistry, VoxelTypeRegistryData } from "@tenet-registry/src/codegen/tables/VoxelTypeRegistry.sol";
 import { distanceBetween, voxelCoordsAreEqual, isZeroCoord } from "@tenet-utils/src/VoxelCoordUtils.sol";
 import { console } from "forge-std/console.sol";
@@ -35,7 +35,27 @@ contract CallerEventSystem is System {
     VoxelEntity memory newEntity,
     VoxelCoord memory newCoord
   ) public {
+    address callerAddress = _msgSender();
     IWorld(_world()).velocityChange(oldCoord, newCoord, oldEntity, newEntity);
+
+    // Transfer ownership of other tables
+    uint256 health = Health.get(callerAddress, oldEntity.scale, oldEntity.entityId);
+    uint256 stamina = Stamina.get(callerAddress, oldEntity.scale, oldEntity.entityId);
+    ObjectType objectType = Object.get(callerAddress, oldEntity.scale, oldEntity.entityId);
+    ActionData memory actionData = Action.get(callerAddress, oldEntity.scale, oldEntity.entityId);
+
+    // Set new
+    Health.set(callerAddress, newEntity.scale, newEntity.entityId, health);
+    Stamina.set(callerAddress, newEntity.scale, newEntity.entityId, stamina);
+    Object.set(callerAddress, newEntity.scale, newEntity.entityId, objectType);
+    Action.set(callerAddress, newEntity.scale, newEntity.entityId, actionData);
+
+    // Reset old
+    Health.set(callerAddress, oldEntity.scale, oldEntity.entityId, 0);
+    Stamina.set(callerAddress, oldEntity.scale, oldEntity.entityId, 0);
+    Object.set(callerAddress, oldEntity.scale, oldEntity.entityId, ObjectType.None);
+    ActionData memory emptyActionData;
+    Action.set(callerAddress, oldEntity.scale, oldEntity.entityId, emptyActionData);
   }
 
   function onActivate(VoxelEntity memory entity, VoxelCoord memory coord) public {}

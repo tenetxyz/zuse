cb3b04673be92cd6d9fd7cb3a5bef4a94f2d666d
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-10-08 17:25:55 -0400

    stamina to stamina conversion

--- examples/basic-conserved-world/packages/simulator/src/systems/StaminaSystem.sol
+++ examples/basic-conserved-world/packages/simulator/src/systems/StaminaSystem.sol
@@ -23,6 +23,13 @@ contract StaminaSystem is SimHandler {
       ValueType.Int256,
       ValueType.Int256
     );
+    SimSelectors.set(
+      SimTable.Stamina,
+      SimTable.Stamina,
+      IWorld(_world()).updateStaminaFromStamina.selector,
+      ValueType.Int256,
+      ValueType.Int256
+    );
   }
 
   function updateStaminaFromEnergy(
@@ -57,6 +64,54 @@ contract StaminaSystem is SimHandler {
         receiverEntity.entityId,
         currentReceiverStamina + receiverStamina
       );
+    }
+  }
+
+  function updateStaminaFromStamina(
+    VoxelEntity memory senderEntity,
+    VoxelCoord memory senderCoord,
+    int256 senderStaminaDelta,
+    VoxelEntity memory receiverEntity,
+    VoxelCoord memory receiverCoord,
+    int256 receiverStaminaDelta
+  ) public {
+    address callerAddress = super.getCallerAddress();
+    bool entityExists = hasKey(
+      StaminaTableId,
+      Stamina.encodeKeyTuple(callerAddress, senderEntity.scale, senderEntity.entityId)
+    );
+    require(entityExists, "Sender entity does not exist");
+    if (isEntityEqual(senderEntity, receiverEntity)) {
+      revert("You can't convert your own stamina to stamina");
+    } else {
+      require(receiverStaminaDelta > 0, "Cannot decrease someone's stamina");
+      require(senderStaminaDelta < 0, "Cannot increase your own stamina");
+      uint256 senderStamina = int256ToUint256(receiverStaminaDelta);
+      uint256 receiverStamina = int256ToUint256(receiverStaminaDelta);
+      require(senderStamina == receiverStamina, "Sender stamina must equal receiver stamina");
+
+      uint256 currentSenderStamina = Stamina.get(callerAddress, senderEntity.scale, senderEntity.entityId);
+      require(currentSenderStamina >= senderStamina, "Not enough stamina to transfer");
+      bool receiverEntityExists = hasKey(
+        MassTableId,
+        Mass.encodeKeyTuple(callerAddress, receiverEntity.scale, receiverEntity.entityId)
+      );
+      if (!receiverEntityExists) {
+        receiverEntity = createTerrainEntity(callerAddress, receiverEntity.scale, receiverCoord);
+        receiverEntityExists = hasKey(
+          EnergyTableId,
+          Mass.encodeKeyTuple(callerAddress, receiverEntity.scale, receiverEntity.entityId)
+        );
+      }
+      require(receiverEntityExists, "Receiver entity does not exist");
+      uint256 currentReceiverStamina = Stamina.get(callerAddress, receiverEntity.scale, receiverEntity.entityId);
+      Stamina.set(
+        callerAddress,
+        receiverEntity.scale,
+        receiverEntity.entityId,
+        currentReceiverStamina + receiverStamina
+      );
+      Stamina.set(callerAddress, senderEntity.scale, senderEntity.entityId, currentSenderStamina - senderStamina);
     }
   }
 }

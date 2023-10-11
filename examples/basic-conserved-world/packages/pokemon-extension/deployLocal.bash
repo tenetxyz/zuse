9f1285ac0e15b31d4e5da3dadcb1a284b23759ea
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-10-11 18:40:04 -0400

    fix deploy

--- examples/basic-conserved-world/packages/pokemon-extension/deployLocal.bash
+++ examples/basic-conserved-world/packages/pokemon-extension/deployLocal.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Extract worldAddress using awk
-worldAddress=$(awk -F'"' '/"31337":/{getline; print $4}' worlds.json)
+worldAddress=$(awk -F'"' '/"31337":/{getline; print $4}' ../level1-ca/worlds.json)
 mainWorldAddress=$(awk -F'"' '/"31337":/{getline; print $4}' ../world/worlds.json)
+
+yarn mud deploy --installDefaultModules false --worldAddress ${worldAddress}
 
 # Start constructing the command
 command="forge script script/TerrainDeploy.s.sol --sig 'run(address,address)' '${worldAddress}' '${mainWorldAddress}' --broadcast --rpc-url http://127.0.0.1:8545 -vv"

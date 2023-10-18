fa55d7dcd0cd2650a8a3aeffe0282638f7f01367
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-10-17 20:35:17 -0400

    finish protein soil test

--- examples/basic-conserved-world/packages/world/src/test/systems/ProteinSoil.t.sol
+++ examples/basic-conserved-world/packages/world/src/test/systems/ProteinSoil.t.sol
@@ -11,7 +11,7 @@ import { getEntityAtCoord, getEntityPositionStrict, positionDataToVoxelCoord } f
 import { FaucetVoxelID, GrassVoxelID, AirVoxelID, DirtVoxelID, BedrockVoxelID } from "@tenet-level1-ca/src/Constants.sol";
 import { MindRegistry } from "@tenet-registry/src/codegen/tables/MindRegistry.sol";
 import { REGISTRY_ADDRESS, BASE_CA_ADDRESS, SIMULATOR_ADDRESS } from "@tenet-world/src/Constants.sol";
-import { ProteinSoilVoxelID, PlantVoxelID } from "@tenet-pokemon-extension/src/Constants.sol";
+import { ProteinSoilVoxelID, ElixirSoilVoxelID, PlantVoxelID } from "@tenet-pokemon-extension/src/Constants.sol";
 import { Pokemon, PokemonData } from "@tenet-pokemon-extension/src/codegen/tables/Pokemon.sol";
 import { Plant, PlantData, PlantStage } from "@tenet-pokemon-extension/src/codegen/tables/Plant.sol";
 import { addressToEntityKey } from "@tenet-utils/src/Utils.sol";
@@ -26,6 +26,7 @@ import { Phosphorous } from "@tenet-simulator/src/codegen/tables/Phosphorous.sol
 import { Health } from "@tenet-simulator/src/codegen/tables/Health.sol";
 import { Potassium } from "@tenet-simulator/src/codegen/tables/Potassium.sol";
 import { Protein } from "@tenet-simulator/src/codegen/tables/Protein.sol";
+import { Elixir } from "@tenet-simulator/src/codegen/tables/Elixir.sol";
 
 uint256 constant INITIAL_HIGH_ENERGY = 150;
 
@@ -125,6 +126,7 @@ contract ProteinSoilTest is MudTest {
       plantEntity.entityId
     );
     assertTrue(plantProtein > 0);
+    assertTrue(Elixir.get(IStore(SIMULATOR_ADDRESS), worldAddress, plantEntity.scale, plantEntity.entityId) == 0);
     assertTrue(Nitrogen.get(IStore(SIMULATOR_ADDRESS), worldAddress, plantEntity.scale, plantEntity.entityId) > 0);
     assertTrue(Phosphorous.get(IStore(SIMULATOR_ADDRESS), worldAddress, plantEntity.scale, plantEntity.entityId) > 0);
     assertTrue(Potassium.get(IStore(SIMULATOR_ADDRESS), worldAddress, plantEntity.scale, plantEntity.entityId) > 0);
@@ -151,7 +153,7 @@ contract ProteinSoilTest is MudTest {
 
     // Place down another soil beside it
     VoxelCoord memory soilCoord2 = VoxelCoord({ x: soilCoord.x, y: soilCoord.y, z: soilCoord.z + 1 });
-    VoxelEntity memory soilEntity2 = world.buildWithAgent(ProteinSoilVoxelID, soilCoord2, agentEntity, bytes4(0));
+    VoxelEntity memory soilEntity2 = world.buildWithAgent(ElixirSoilVoxelID, soilCoord2, agentEntity, bytes4(0));
     uint256 soil2Nutrients = Nutrients.get(
       IStore(SIMULATOR_ADDRESS),
       worldAddress,
@@ -159,8 +161,9 @@ contract ProteinSoilTest is MudTest {
       soilEntity2.entityId
     );
     assertTrue(soil2Nutrients == 0);
-    assertTrue(Nitrogen.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity2.scale, soilEntity2.entityId) > 0);
-    assertTrue(Phosphorous.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity2.scale, soilEntity2.entityId) == 0);
+    assertTrue(Nitrogen.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity2.scale, soilEntity2.entityId) == 0);
+    uint256 soil2P = Phosphorous.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity2.scale, soilEntity2.entityId);
+    assertTrue(soil2P > 0);
     assertTrue(Potassium.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity2.scale, soilEntity2.entityId) == 0);
 
     // Place down plant on top of it
@@ -180,70 +183,28 @@ contract ProteinSoilTest is MudTest {
     // Set energy and activate
     Energy.set(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity.scale, soilEntity.entityId, INITIAL_HIGH_ENERGY);
     vm.roll(block.number + 1);
+    Phosphorous.set(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity.scale, soilEntity.entityId, 110);
+    console.log("activate");
     world.activateWithAgent(ProteinSoilVoxelID, soilCoord, agentEntity, bytes4(0));
-    // Soil1 should have nutrients
+    // // Soil1 should have nutrients
     soil1Nutrients = Nutrients.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity.scale, soilEntity.entityId);
-    assertTrue(soil1Nutrients > 0);
+    assertTrue(soil1Nutrients == 0);
+    assertTrue(Phosphorous.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity.scale, soilEntity.entityId) == 0);
     // Soil2 should have nutrients
     soil2Nutrients = Nutrients.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity2.scale, soilEntity2.entityId);
-    assertTrue(soil2Nutrients > 0);
+    assertTrue(soil2Nutrients == 0);
+    assertTrue(
+      Phosphorous.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity2.scale, soilEntity2.entityId) > soil2P
+    );
     // Plant should have nutrients
-    plantNutrients = Nutrients.get(IStore(SIMULATOR_ADDRESS), worldAddress, plantEntity.scale, plantEntity.entityId);
-    assertTrue(plantNutrients > 0);
-
-    vm.stopPrank();
-  }
-
-  function testSoilWithZeroEnergy() public returns (VoxelEntity memory, VoxelEntity memory) {
-    vm.startPrank(alice, alice);
-    VoxelEntity memory agentEntity = setupAgent();
-
-    VoxelCoord memory soilCoord = VoxelCoord({ x: agentCoord.x + 1, y: agentCoord.y, z: agentCoord.z });
-    VoxelEntity memory soilEntity = world.buildWithAgent(ProteinSoilVoxelID, soilCoord, agentEntity, bytes4(0));
-    uint256 soilNutrients = Nutrients.get(
+    uint256 plantProtein = Protein.get(
       IStore(SIMULATOR_ADDRESS),
       worldAddress,
-      soilEntity.scale,
-      soilEntity.entityId
+      plantEntity.scale,
+      plantEntity.entityId
     );
-    assertTrue(soilNutrients == 0);
-    assertTrue(Nitrogen.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity.scale, soilEntity.entityId) > 0);
-    assertTrue(Phosphorous.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity.scale, soilEntity.entityId) > 0);
-    assertTrue(Potassium.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity.scale, soilEntity.entityId) > 0);
-
-    // Place down plant on top of it
-    {
-      VoxelCoord memory plantCoord = VoxelCoord({ x: soilCoord.x, y: soilCoord.y + 1, z: soilCoord.z });
-      VoxelEntity memory plantEntity = world.buildWithAgent(PlantVoxelID, plantCoord, agentEntity, bytes4(0));
-      uint256 plantNutrients = Nutrients.get(
-        IStore(SIMULATOR_ADDRESS),
-        worldAddress,
-        plantEntity.scale,
-        plantEntity.entityId
-      );
-      assertTrue(plantNutrients == 0);
-      assertTrue(Nitrogen.get(IStore(SIMULATOR_ADDRESS), worldAddress, plantEntity.scale, plantEntity.entityId) > 0);
-      assertTrue(Phosphorous.get(IStore(SIMULATOR_ADDRESS), worldAddress, plantEntity.scale, plantEntity.entityId) > 0);
-      assertTrue(Potassium.get(IStore(SIMULATOR_ADDRESS), worldAddress, plantEntity.scale, plantEntity.entityId) > 0);
-    }
-
-    // Move agent to soil
-    {
-      // Place down another soil beside it
-      VoxelCoord memory soilCoord2 = VoxelCoord({ x: soilCoord.x, y: soilCoord.y, z: soilCoord.z + 1 });
-      VoxelEntity memory soilEntity2 = world.buildWithAgent(ProteinSoilVoxelID, soilCoord2, agentEntity, bytes4(0));
-      assertTrue(Energy.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity.scale, soilEntity.entityId) == 0);
-      uint256 soil2Nutrients = Nutrients.get(
-        IStore(SIMULATOR_ADDRESS),
-        worldAddress,
-        soilEntity2.scale,
-        soilEntity2.entityId
-      );
-      assertTrue(soil2Nutrients == 0);
-      assertTrue(Nitrogen.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity2.scale, soilEntity2.entityId) > 0);
-      assertTrue(Phosphorous.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity2.scale, soilEntity2.entityId) > 0);
-      assertTrue(Potassium.get(IStore(SIMULATOR_ADDRESS), worldAddress, soilEntity2.scale, soilEntity2.entityId) > 0);
-    }
+    assertTrue(plantProtein > 0);
+    assertTrue(Elixir.get(IStore(SIMULATOR_ADDRESS), worldAddress, plantEntity.scale, plantEntity.entityId) == 0);
 
     vm.stopPrank();
   }

a9f35fa3221f36f3c672ac695b90b68b54e6e49a
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-10-18 12:27:04 -0400

    add simulator world

--- examples/basic-conserved-world/packages/derived/src/systems/DerivedSystem.sol
+++ examples/basic-conserved-world/packages/derived/src/systems/DerivedSystem.sol
@@ -12,6 +12,7 @@ import { VoxelType, VoxelTypeTableId } from "@tenet-base-world/src/codegen/table
 import { WORLD_ADDRESS, BASE_CA_ADDRESS } from "@tenet-derived/src/Constants.sol";
 import { getEntitySimData } from "@tenet-world/src/CallUtils.sol";
 import { getEntityAtCoord, getVoxelCoordStrict } from "@tenet-base-world/src/Utils.sol";
+import { getNeighbourEntities } from "@tenet-simulator/src/Utils.sol";
 import { console } from "forge-std/console.sol";
 import { Pokemon, PokemonData } from "@tenet-pokemon-extension/src/codegen/tables/Pokemon.sol";
 import { Plant, PlantData, PlantStage } from "@tenet-pokemon-extension/src/codegen/tables/Plant.sol";
@@ -30,8 +31,10 @@ contract DerivedSystem is System {
     bytes32 voxelTypeId = VoxelType.getVoxelTypeId(worldStore, entity.scale, entity.entityId);
     bytes32 entityId = getEntityAtCoord(worldStore, 1, coord);
     VoxelCoord memory entityCoord = getVoxelCoordStrict(worldStore, entity);
+    (bytes32[] memory neighbourEntities, ) = getNeighbourEntities(WORLD_ADDRESS, entity);
     console.logBytes32(voxelTypeId);
     console.logBytes32(entityId);
+    console.logUint(neighbourEntities.length);
 
     // CA data
     bytes32 caEntity = CAEntityMapping.get(caStore, WORLD_ADDRESS, entity.entityId);

391cad56030b3de7e2dbd4f9c7fc81ec5655d11a
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-10-23 18:43:43 -0400

    fix build

--- examples/basic-conserved-world/packages/pretty-extension/deployLocal.bash
+++ examples/basic-conserved-world/packages/pretty-extension/deployLocal.bash
@@ -3,11 +3,14 @@
 # Extract worldAddress using awk
 worldAddress=$(awk -F'"' '/"31337":/{getline; print $4}' ../level1-ca/worlds.json)
 
-# Start constructing the command
-command="forge script script/PostDeploy100.s.sol --sig 'run(address)' '${worldAddress}' --broadcast --rpc-url http://127.0.0.1:8545 -vv"
+# Loop over all files in script/ and forge script each one
+for file in script/*.sol; do
+    # Start constructing the command for each file
+    command="forge script $file --sig 'run(address)' '${worldAddress}' --broadcast --rpc-url http://127.0.0.1:8545 -vv"
 
-# Output the command
-echo "Running command: $command"
+    # Output the command
+    echo "Running command: $command"
 
-# Execute the command and display output as it runs
-eval $command
+    # Execute the command and display output as it runs
+    eval $command
+done

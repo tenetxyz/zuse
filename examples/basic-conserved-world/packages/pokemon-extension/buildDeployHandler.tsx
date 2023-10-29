33f0961384e2b4a5c9776a6a02fc6d1589cefe25
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-10-29 10:50:46 -0400

    change build deploy handlers

---
+++ examples/basic-conserved-world/packages/pokemon-extension/buildDeployHandler.tsx
@@ -0,0 +1,61 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+
+const chainId = process.env.NODE_ENV === "production" ? 1337 : 31337;
+
+function getAddressFromFile(filePath) {
+  try {
+    const content = fs.readFileSync(filePath, "utf8");
+    const json = JSON.parse(content);
+    return json[chainId] && json[chainId].address;
+  } catch (error) {
+    console.error(`Error reading or parsing JSON from ${filePath}:`, error);
+    process.exit(1);
+  }
+}
+
+const worldAddress = getAddressFromFile("../level1-ca/worlds.json");
+const mainWorldAddress = getAddressFromFile("../world/worlds.json");
+
+function executeCommand(command) {
+  try {
+    execSync(command, {
+      stdio: "inherit",
+    });
+  } catch (error) {
+    console.error(`Error executing command: ${command}`);
+    process.exit(1);
+  }
+}
+
+// Get the action (build, deploy, etc.) from command-line arguments
+const action = process.argv[2];
+
+if (!action) {
+  console.error("Please specify an action (e.g., build, deploy).");
+  process.exit(1);
+}
+
+if (action == "build") {
+  // List of dev commands
+  const setRegistryAddressCommand = `node ../../../../scripts/setRegistryAddress.js ${chainId} ../../../../packages/registry/worlds.json src/Constants.sol REGISTRY_ADDRESS`;
+  const setCAAddressCommand = `node ../../../../scripts/setRegistryAddress.js ${chainId} ../level1-ca/worlds.json src/Constants.sol CA_ADDRESS`;
+  const devCommands = [setRegistryAddressCommand, setCAAddressCommand, "yarn run initialize"];
+
+  devCommands.forEach((command) => {
+    executeCommand(command);
+  });
+} else if (action == "deploy") {
+  let deployCommand = `yarn mud deploy --installDefaultModules false --worldAddress ${worldAddress}`;
+  let rpcUrl = "http://127.0.0.1:8545";
+  if (process.env.NODE_ENV === "production") {
+    deployCommand += " --profile=tenet-testgrid";
+    rpcUrl = "https://test-grid.everlon.xyz";
+  }
+  const forgeCommand = `forge script script/TerrainDeploy.s.sol --sig 'run(address,address)' '${worldAddress}' '${mainWorldAddress}' --broadcast --rpc-url ${rpcUrl} -vv`;
+  const devCommands = [deployCommand, forgeCommand];
+
+  devCommands.forEach((command) => {
+    executeCommand(command);
+  });
+}
---
+++ t.py
@@ -0,0 +1,78 @@
+import os
+import subprocess
+import sys
+
+cur_directory = os.path.dirname(os.path.abspath(__file__))
+
+def run_example(path, extra_cmd=None):
+    command = f"cd examples/{path} && yarn run dev"
+    if extra_cmd:
+        command += f"&& {extra_cmd}"
+    print(command)
+    subprocess.run(["yarn", "concurrently", "-n", "example", "-c", "#fb8500", command])
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: ./t run [command]")
+        return
+    # print(sys.argv)
+
+    if sys.argv[1] == "run":
+        if sys.argv[2] == "dev":
+            subcommands = ["./t run anvil", f"./t run framework {' '.join(sys.argv[3:])} && ./t run {sys.argv[3]} {' '.join(sys.argv[4:])}"]
+            command = ["yarn", "concurrently", "-n", "anvil,contracts", "-c", "blue,green,white"] + subcommands
+            # Copy the current environment and modify it
+            env = os.environ.copy()
+            env["NODE_ENV"] = "development"
+            subprocess.run(command, env=env)
+
+        if sys.argv[2] == "prod":
+            subcommands = [f"./t run framework {' '.join(sys.argv[3:])} && ./t run {sys.argv[3]} {' '.join(sys.argv[4:])}"]
+            command = ["yarn", "concurrently", "-n", "anvil,contracts", "-c", "blue,green,white"] + subcommands
+            env = os.environ.copy()
+            env["NODE_ENV"] = "production"
+            subprocess.run(command)
+
+        elif sys.argv[2] == "anvil":
+            os.chdir("scripts")
+            subprocess.run(["yarn", "run", "anvil"])
+
+        elif sys.argv[2] == "framework":
+            if "--skip-build" in sys.argv:
+                subprocess.run(["yarn", "run", "registry"])
+            else:
+                subprocess.run(["yarn", "run", "dev"])
+
+        elif sys.argv[2] == "basic-world":
+            run_example("basic-world")
+
+        elif sys.argv[2] == "basic-conserved-world":
+            extra_cmd = []
+
+            if "--with-pokemon" in sys.argv:
+                extra_cmd.append("yarn run deploy:pokemon")
+            if "--with-extensions" in sys.argv:
+                extra_cmd.append("yarn run deploy:extensions")
+            if "--with-derived" in sys.argv:
+                extra_cmd.append("yarn run deploy:derived")
+            if "--snapshot" in sys.argv:
+                extra_cmd.append(f"sh {cur_directory}/scripts/rollback/create_snapshot.sh")
+
+            # Join the commands with ' && '
+            command_str = " && ".join(extra_cmd)
+
+            run_example("basic-conserved-world", command_str)
+
+        elif sys.argv[2] == "multiple-layers-world":
+            run_example("multiple-layers-world")
+
+        elif sys.argv[2] == "snapshot":
+            subprocess.run([f"sh {cur_directory}/scripts/rollback/create_snapshot.sh"])
+            print("rollbacked snapshot")
+
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()

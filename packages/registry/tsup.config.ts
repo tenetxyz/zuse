b09de9e429e98f85f492de6da821ae1faf54f44f
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-10-31 23:35:33 -0400

    fix js

--- packages/registry/tsup.config.ts
+++ packages/registry/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["mud.config.ts"],
+  entry: ["mud.config.ts", "types/ethers-contracts"],
   target: "esnext",
   format: ["esm", "cjs"],
   dts: false,

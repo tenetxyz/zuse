e9aafe26cfd1beeddd4de40ba7d92b19eb1664e0
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-11-12 13:27:40 -0500

    remove example

--- examples/multiple-layers-world/packages/level1-ca/src/Constants.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.0;
-
-address constant REGISTRY_ADDRESS = 0x057ef64E23666F000b34aE31332854aCBd1c8544;
-
-bytes32 constant AirVoxelID = bytes32(keccak256("air"));
-bytes32 constant AirVoxelVariantID = bytes32(keccak256("air"));
-bytes32 constant ElectronVoxelID = bytes32(keccak256("electron"));

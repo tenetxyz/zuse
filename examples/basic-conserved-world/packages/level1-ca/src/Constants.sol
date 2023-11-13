fdf7f0a39f123864c56b210a5a68904be0376ce3
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-11-14 01:10:09 +0300

    address changes

--- examples/basic-conserved-world/packages/level1-ca/src/Constants.sol
+++ examples/basic-conserved-world/packages/level1-ca/src/Constants.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0;
 
-address constant REGISTRY_ADDRESS = 0xB088741f11DB22A5DB2f2ddE851FD1c9DF10FA71;
+address constant REGISTRY_ADDRESS = 0x057ef64E23666F000b34aE31332854aCBd1c8544;
 
 bytes32 constant AirVoxelID = bytes32(keccak256("air"));
 bytes32 constant AirVoxelVariantID = bytes32(keccak256("air"));
--- examples/basic-conserved-world/packages/simulator/src/Constants.sol
+++ examples/basic-conserved-world/packages/simulator/src/Constants.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0;
 
-address constant REGISTRY_ADDRESS = 0xB088741f11DB22A5DB2f2ddE851FD1c9DF10FA71;
+address constant REGISTRY_ADDRESS = 0x057ef64E23666F000b34aE31332854aCBd1c8544;
 
 // Signatures
 string constant SIM_INIT_ENTITY_SIG = "initEntity((uint32,bytes32),uint256,uint256,(int32,int32,int32))";

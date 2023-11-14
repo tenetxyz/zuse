f524b581a8abdfa70e167f174aebbcd520b73964
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-11-14 17:27:49 +0300

    geth

---
+++ runGeth.bash
@@ -0,0 +1,19 @@
+geth \
+  --datadir data/dev-chain \
+  --dev \
+  --dev.gaslimit 2000000000 \
+  --rpc.gascap 2000000000 \
+  --miner.gaslimit 2000000000 \
+  --http \
+  --http.api eth,web3,net \
+  --http.addr "0.0.0.0" \
+  --http.vhosts "*" \
+  --http.port 8545 \
+  --http.corsdomain "*" \
+  --ws \
+  --ws.addr "0.0.0.0" \
+  --ws.api eth,web3,net \
+  --ws.port 8545 \
+  --ws.origins "*"
+
+geth --exec "loadScript('initAccounts.js')" attach ipc:data/dev-chain/geth.ipc

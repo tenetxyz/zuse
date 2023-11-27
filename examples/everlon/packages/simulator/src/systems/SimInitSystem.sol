28197e911613a800a1445fccfcbeedbb04fe886b
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-11-27 16:06:29 -0500

    init

--- examples/everlon/packages/simulator/src/systems/SimInitSystem.sol
+++ examples/everlon/packages/simulator/src/systems/SimInitSystem.sol
@@ -7,17 +7,40 @@ import { hasKey } from "@latticexyz/world/src/modules/keysintable/hasKey.sol";
 import { SimInitSystem as SimInitProtoSystem } from "@tenet-base-simulator/src/systems/SimInitSystem.sol";
 import { Mass, MassTableId } from "@tenet-simulator/src/codegen/tables/Mass.sol";
 import { Energy, EnergyTableId } from "@tenet-simulator/src/codegen/tables/Energy.sol";
+import { Health, HealthData, HealthTableId } from "@tenet-simulator/src/codegen/tables/Health.sol";
+import { Stamina, StaminaTableId } from "@tenet-simulator/src/codegen/tables/Stamina.sol";
 import { ObjectProperties } from "@tenet-utils/src/Types.sol";
 
 contract SimInitSystem is SimInitProtoSystem {
   function initObject(bytes32 objectEntityId, ObjectProperties memory initialProperties) public override {
     address world = _msgSender();
-    require(!hasKey(MassTableId, Mass.encodeKeyTuple(world, objectEntityId)), "Mass for object already initialized");
+    require(
+      !hasKey(MassTableId, Mass.encodeKeyTuple(world, objectEntityId)),
+      "SimInitSystem: Mass for object already initialized"
+    );
     require(
       !hasKey(EnergyTableId, Energy.encodeKeyTuple(world, objectEntityId)),
-      "Energy for object already initialized"
+      "SimInitSystem: Energy for object already initialized"
     );
     Mass.set(world, objectEntityId, initialProperties.mass);
     Energy.set(world, objectEntityId, initialProperties.energy);
+    if (initialProperties.health > 0) {
+      require(
+        !hasKey(HealthTableId, Health.encodeKeyTuple(world, objectEntityId)),
+        "SimInitSystem: Health for object already initialized"
+      );
+      Health.set(
+        world,
+        objectEntityId,
+        HealthData({ lastUpdateBlock: block.number, health: initialProperties.health })
+      );
+    }
+    if (initialProperties.stamina > 0) {
+      require(
+        !hasKey(StaminaTableId, Stamina.encodeKeyTuple(world, objectEntityId)),
+        "SimInitSystem: Stamina for object already initialized"
+      );
+      Stamina.set(world, objectEntityId, initialProperties.stamina);
+    }
   }
 }

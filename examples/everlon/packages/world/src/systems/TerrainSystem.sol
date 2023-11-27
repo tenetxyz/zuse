7f59fc0173cfa358fdaa40c34ecf923133d4d654
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-11-27 14:59:08 -0500

    terrain system

--- examples/everlon/packages/world/src/systems/TerrainSystem.sol
+++ examples/everlon/packages/world/src/systems/TerrainSystem.sol
@@ -17,24 +17,30 @@ contract TerrainSystem is TerrainProtoSystem {
     return AirObjectID;
   }
 
-  function spawnInitialAgents() public {
-    // TODO: Make this only callable once
-    VoxelCoord memory initialAgentCoord1 = VoxelCoord(50, 10, 50);
-    VoxelCoord memory initialAgentCoord2 = VoxelCoord(50, 10, 55);
-    // Since the world is calling this, the acting entity can be 0
-    IWorld(_world()).build(bytes32(0), BuilderObjectID, initialAgentCoord1);
-    IWorld(_world()).build(bytes32(0), BuilderObjectID, initialAgentCoord2);
+  function decodeToBytes32(bytes memory data) external pure returns (bytes32) {
+    return abi.decode(data, (bytes32));
+  }
+
+  function decodeToObjectProperties(bytes memory data) external pure returns (ObjectProperties memory) {
+    return abi.decode(data, (ObjectProperties));
   }
 
   function getTerrainObjectTypeId(VoxelCoord memory coord) public view override returns (bytes32) {
-    if (coord.y == 0) {
-      return BedrockObjectID;
-    } else if (coord.y > 0 && coord.y <= 9) {
-      if (coord.y == 9) {
-        return GrassObjectID;
-      } else {
-        return DirtObjectID;
-      }
+    VoxelCoord memory shardCoord = coordToShardCoord(coord);
+    require(
+      hasKey(ShardTableId, Shard.encodeKeyTuple(shardCoord.x, shardCoord.y, shardCoord.z)),
+      "TerrainSystem: Shard not claimed"
+    );
+    ShardData memory shardData = Shard.get(shardCoord.x, shardCoord.y, shardCoord.z);
+    (bool terrainSelectorSuccess, bytes memory terrainSelectorReturnData) = safeCall(
+      shardData.contractAddress,
+      abi.encodeWithSelector(shardData.objectTypeIdSelector, coord),
+      "shard terrainSelector"
+    );
+    if (terrainSelectorSuccess) {
+      try this.decodeToBytes32(terrainSelectorReturnData) returns (bytes32 terrainObjectTypeId) {
+        return terrainObjectTypeId;
+      } catch {}
     }
 
     return AirObjectID;
@@ -45,21 +51,46 @@ contract TerrainSystem is TerrainProtoSystem {
     ObjectProperties memory requestedProperties
   ) public override returns (ObjectProperties memory) {
     ObjectProperties memory objectProperties;
-    if (coord.y == 0) {
-      objectProperties.mass = 50;
-      objectProperties.energy = 500;
-    } else if (coord.y > 0 && coord.y <= 9) {
-      if (coord.y == 9) {
-        objectProperties.mass = 5;
-        objectProperties.energy = 50;
-      } else {
-        objectProperties.mass = 5;
-        objectProperties.energy = 100;
-      }
-    } else {
-      objectProperties.mass = 0;
-      objectProperties.energy = 0;
+    // use cache if possible
+    if (hasKey(TerrainPropertiesTableId, TerrainProperties.encodeKeyTuple(coord.x, coord.y, coord.z))) {
+      bytes memory encodedTerrainProperties = TerrainProperties.getProperties(coord.x, coord.y, coord.z);
+      return abi.decode(encodedTerrainProperties, (ObjectProperties));
     }
+
+    VoxelCoord memory shardCoord = coordToShardCoord(coord);
+    require(
+      hasKey(ShardTableId, Shard.encodeKeyTuple(shardCoord.x, shardCoord.y, shardCoord.z)),
+      "TerrainSystem: Shard not claimed"
+    );
+    ShardData memory shardData = Shard.get(shardCoord.x, shardCoord.y, shardCoord.z);
+    (bool propertiesSelectorSuccess, bytes memory propertiesSelectorReturnData) = safeCall(
+      shardData.contractAddress,
+      abi.encodeWithSelector(shardData.propertiesSelector, coord),
+      "shard terrainSelector"
+    );
+    if (propertiesSelectorSuccess) {
+      try this.decodeToObjectProperties(propertiesSelectorReturnData) returns (ObjectProperties memory decodedValue) {
+        objectProperties = decodedValue;
+      } catch {}
+    }
+
+    // Enforce constraints on terrain
+    if (shardData.totalGenMass + objectProperties.mass > MAX_TOTAL_MASS_IN_SHARD) {
+      // Override mass
+      objectProperties.mass = 0;
+    } else {
+      // Update shard data total mass
+      shardData.totalGenMass += objectProperties.mass;
+    }
+    if (shardData.totalGenEnergy + objectProperties.energy > MAX_TOTAL_ENERGY_IN_SHARD) {
+      // Override energy
+      objectProperties.energy = 0;
+    } else {
+      // Update shard data total energy
+      shardData.totalGenEnergy += objectProperties.energy;
+    }
+    Shard.set(shardCoord.x, shardCoord.y, shardCoord.z, shardData);
+    TerrainProperties.setProperties(coord.x, coord.y, coord.z, abi.encode(objectProperties));
 
     return objectProperties;
   }

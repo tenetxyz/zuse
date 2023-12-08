9af4f40c47ff116a4b196fb3863b24e5f2a84dfe
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-12-08 14:54:36 -0700

    start test

---
+++ examples/everlon/packages/world/src/test/Move.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0;
+
+import "forge-std/Test.sol";
+import { MudTest } from "@latticexyz/store/src/MudTest.sol";
+import { IStore } from "@latticexyz/store/src/IStore.sol";
+import { IWorld } from "@tenet-world/src/codegen/world/IWorld.sol";
+import { ObjectType, OwnedBy, ObjectEntity } from "@tenet-world/src/codegen/Tables.sol";
+import { VoxelCoord } from "@tenet-utils/src/Types.sol";
+import { getEntityAtCoord, getEntityPositionStrict, positionDataToVoxelCoord } from "@tenet-base-world/src/Utils.sol";
+import { BuilderObjectID, GrassObjectID, AirObjectID } from "@tenet-world/src/Constants.sol";
+import { REGISTRY_ADDRESS, SIMULATOR_ADDRESS } from "@tenet-world/src/Constants.sol";
+import { console } from "forge-std/console.sol";
+import { Mass } from "@tenet-simulator/src/codegen/tables/Mass.sol";
+import { Energy } from "@tenet-simulator/src/codegen/tables/Energy.sol";
+
+contract MoveTest is MudTest {
+  IWorld private world;
+  IStore private store;
+  address payable internal alice;
+  VoxelCoord faucetAgentCoord = VoxelCoord(50, 10, 50);
+
+  function setUp() public override {
+    super.setUp();
+    world = IWorld(worldAddress);
+    store = IStore(worldAddress);
+    alice = payable(address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266));
+  }
+
+  function testMoveSelf() public {
+    vm.startPrank(alice, alice);
+
+    bytes32 faucetEntityId = getEntityAtCoord(store, faucetAgentCoord);
+    assertTrue(uint256(faucetEntityId) != 0, "Agent not found at coord");
+    bytes32 faucetObjectEntityId = ObjectEntity.get(store, faucetEntityId);
+    VoxelCoord memory buildCoord = VoxelCoord(faucetAgentCoord.x, faucetAgentCoord.y, faucetAgentCoord.z + 1);
+    bytes32 agentEntityId = world.claimAgentFromFaucet(faucetObjectEntityId, BuilderObjectID, buildCoord);
+    assertTrue(uint256(agentEntityId) != 0, "Agent not found at coord");
+
+    vm.stopPrank();
+  }
+}

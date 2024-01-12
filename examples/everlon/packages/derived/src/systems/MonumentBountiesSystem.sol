403654671290e296aa9fd97f77360392dd3919a0
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-01-12 15:17:33 -0700

    add bounty test

--- examples/everlon/packages/derived/src/systems/MonumentBountiesSystem.sol
+++ examples/everlon/packages/derived/src/systems/MonumentBountiesSystem.sol
@@ -24,7 +24,7 @@ import { getObjectProperties } from "@tenet-base-world/src/CallUtils.sol";
 import { positionDataToVoxelCoord, getEntityIdFromObjectEntityId, getVoxelCoord, getEntityAtCoord } from "@tenet-base-world/src/Utils.sol";
 
 import { WORLD_ADDRESS } from "@tenet-derived/src/Constants.sol";
-import { coordToShardCoord } from "@tenet-utils/src/VoxelCoordUtils.sol";
+import { coordToShardCoord, voxelCoordsAreEqual } from "@tenet-utils/src/VoxelCoordUtils.sol";
 import { int32ToUint32, uint32ToInt32 } from "@tenet-utils/src/TypeUtils.sol";
 import { SHARD_DIM } from "@tenet-world/src/Constants.sol";
 import { REGISTRY_ADDRESS } from "@tenet-derived/src/Constants.sol";
@@ -36,7 +36,7 @@ contract MonumentBountiesSystem is System {
     VoxelCoord[] memory relativePositions,
     string memory name,
     string memory description
-  ) public {
+  ) public returns (bytes32) {
     require(bountyAmount > 0, "MonumentBountiesSystem: Bounty amount must be greater than 0");
     require(bytes(name).length > 0, "MonumentBountiesSystem: Name must be non-empty");
 
@@ -56,6 +56,11 @@ contract MonumentBountiesSystem is System {
       );
     }
 
+    require(
+      voxelCoordsAreEqual(relativePositions[0], VoxelCoord({ x: 0, y: 0, z: 0 })),
+      "MonumentBountiesSystem: First relative position must be (0, 0, 0)"
+    );
+
     bytes32 bountyId = getUniqueEntity();
     MonumentBounties.set(
       bountyId,
@@ -69,6 +74,7 @@ contract MonumentBountiesSystem is System {
         description: description
       })
     );
+    return bountyId;
   }
 
   function claimBounty(
@@ -93,21 +99,32 @@ contract MonumentBountiesSystem is System {
     );
 
     // Assert that baseWorldCoord is within the claimed area, ignore Y values
+    VoxelCoord memory monumentClaimedAreaUpperCorner = VoxelCoord(
+      monumentClaimedArea.x +
+        uint32ToInt32(
+          MonumentsLeaderboard.getLength(monumentClaimedArea.x, monumentClaimedArea.y, monumentClaimedArea.z)
+        ),
+      0,
+      monumentClaimedArea.z +
+        uint32ToInt32(
+          MonumentsLeaderboard.getWidth(monumentClaimedArea.x, monumentClaimedArea.y, monumentClaimedArea.z)
+        )
+    );
     require(
       baseWorldCoord.x >= monumentClaimedArea.x &&
-        baseWorldCoord.x < monumentClaimedArea.x &&
+        baseWorldCoord.x < monumentClaimedAreaUpperCorner.x &&
         baseWorldCoord.z >= monumentClaimedArea.z &&
-        baseWorldCoord.z < monumentClaimedArea.z,
+        baseWorldCoord.z < monumentClaimedAreaUpperCorner.z,
       "MonumentBountiesSystem: Base world coord is not within the claimed area"
     );
 
-    VoxelCoord memory relativePositions = abi.decode(bountyData.relativePositions, (VoxelCoord));
+    VoxelCoord[] memory relativePositions = abi.decode(bountyData.relativePositions, (VoxelCoord[]));
     // Go through each relative position, aplpy it to the base world coord, and check if the object type id matches
     for (uint256 i = 0; i < bountyData.objectTypeIds.length; i++) {
       VoxelCoord memory absolutePosition = VoxelCoord({
-        x: baseWorldCoord.x + relativePositions.x,
-        y: baseWorldCoord.y + relativePositions.y,
-        z: baseWorldCoord.z + relativePositions.z
+        x: baseWorldCoord.x + relativePositions[i].x,
+        y: baseWorldCoord.y + relativePositions[i].y,
+        z: baseWorldCoord.z + relativePositions[i].z
       });
       bytes32 entityId = getEntityAtCoord(worldStore, absolutePosition);
       bytes32 objectTypeId = ObjectType.get(worldStore, entityId);
--- examples/everlon/packages/derived/src/test/Monuments.t.sol
+++ examples/everlon/packages/derived/src/test/Monuments.t.sol
@@ -17,7 +17,7 @@ import { ObjectEntity } from "@tenet-world/src/codegen/tables/ObjectEntity.sol";
 import { VoxelCoord } from "@tenet-utils/src/Types.sol";
 import { getEntityAtCoord, getEntityPositionStrict, positionDataToVoxelCoord, getVoxelCoord } from "@tenet-base-world/src/Utils.sol";
 import { int32ToUint32, uint32ToInt32 } from "@tenet-utils/src/TypeUtils.sol";
-import { BuilderObjectID, GrassObjectID, AirObjectID } from "@tenet-world/src/Constants.sol";
+import { BuilderObjectID, GrassObjectID, AirObjectID, DirtObjectID } from "@tenet-world/src/Constants.sol";
 import { REGISTRY_ADDRESS, SIMULATOR_ADDRESS, WORLD_ADDRESS } from "@tenet-derived/src/Constants.sol";
 import { console } from "forge-std/console.sol";
 import { Mass } from "@tenet-simulator/src/codegen/tables/Mass.sol";
@@ -27,6 +27,7 @@ import { Stamina } from "@tenet-simulator/src/codegen/tables/Stamina.sol";
 import { Velocity } from "@tenet-simulator/src/codegen/tables/Velocity.sol";
 
 import { MonumentsLeaderboard, MonumentsLeaderboardData, MonumentsLeaderboardTableId } from "@tenet-derived/src/codegen/Tables.sol";
+import { MonumentBounties, MonumentBountiesData, MonumentBountiesTableId } from "@tenet-derived/src/codegen/Tables.sol";
 
 // TODO: Replace relative imports in IWorld.sol, instead of this hack
 interface IWorld is IBaseWorld, IBuildSystem, IMineSystem, IMoveSystem, IActivateSystem, IFaucetSystem {
@@ -74,7 +75,7 @@ contract MonumentsTest is MudTest {
 
     (, bytes32 agentObjectEntityId) = setupAgent();
 
-    VoxelCoord memory lowerSouthwestCorner = VoxelCoord({ x: agentCoord.x, y: agentCoord.y, z: agentCoord.z });
+    VoxelCoord memory lowerSouthwestCorner = VoxelCoord({ x: agentCoord.x, y: 0, z: agentCoord.z });
     VoxelCoord memory size = VoxelCoord({ x: 10, y: 0, z: 10 });
 
     derivedWorld.claimMonumentsArea(agentObjectEntityId, lowerSouthwestCorner, size);
@@ -107,7 +108,7 @@ contract MonumentsTest is MudTest {
 
     (, bytes32 agentObjectEntityId) = setupAgent();
 
-    VoxelCoord memory lowerSouthwestCorner = VoxelCoord({ x: agentCoord.x + 10, y: agentCoord.y, z: agentCoord.z });
+    VoxelCoord memory lowerSouthwestCorner = VoxelCoord({ x: agentCoord.x + 10, y: 0, z: agentCoord.z });
     VoxelCoord memory size = VoxelCoord({ x: 10, y: 0, z: 10 });
 
     vm.expectRevert();
@@ -121,7 +122,7 @@ contract MonumentsTest is MudTest {
 
     (, bytes32 agentObjectEntityId) = setupAgent();
 
-    VoxelCoord memory lowerSouthwestCorner = VoxelCoord({ x: agentCoord.x, y: agentCoord.y, z: agentCoord.z });
+    VoxelCoord memory lowerSouthwestCorner = VoxelCoord({ x: agentCoord.x, y: 0, z: agentCoord.z });
     VoxelCoord memory size = VoxelCoord({ x: 10, y: 0, z: 10 });
 
     derivedWorld.claimMonumentsArea(agentObjectEntityId, lowerSouthwestCorner, size);
@@ -170,7 +171,7 @@ contract MonumentsTest is MudTest {
 
     (, bytes32 agentObjectEntityId) = setupAgent();
 
-    VoxelCoord memory lowerSouthwestCorner = VoxelCoord({ x: agentCoord.x, y: agentCoord.y, z: agentCoord.z });
+    VoxelCoord memory lowerSouthwestCorner = VoxelCoord({ x: agentCoord.x, y: 0, z: agentCoord.z });
     VoxelCoord memory size = VoxelCoord({ x: 10, y: 0, z: 10 });
 
     derivedWorld.claimMonumentsArea(agentObjectEntityId, lowerSouthwestCorner, size);
@@ -221,7 +222,7 @@ contract MonumentsTest is MudTest {
     (, bytes32 agentObjectEntityId) = setupAgent();
     bytes32 agentObjectTypeId = BuilderObjectID;
 
-    VoxelCoord memory lowerSouthwestCorner1 = VoxelCoord({ x: agentCoord.x, y: agentCoord.y, z: agentCoord.z });
+    VoxelCoord memory lowerSouthwestCorner1 = VoxelCoord({ x: agentCoord.x, y: 0, z: agentCoord.z });
     VoxelCoord memory size = VoxelCoord({ x: 10, y: 0, z: 10 });
 
     derivedWorld.claimMonumentsArea(agentObjectEntityId, lowerSouthwestCorner1, size);
@@ -245,7 +246,7 @@ contract MonumentsTest is MudTest {
       agentCoord = newCoord;
     }
     size = VoxelCoord({ x: 5, y: 0, z: 5 });
-    VoxelCoord memory lowerSouthwestCorner2 = newCoord;
+    VoxelCoord memory lowerSouthwestCorner2 = VoxelCoord(newCoord.x, 0, newCoord.z);
     derivedWorld.claimMonumentsArea(agentObjectEntityId, lowerSouthwestCorner2, size);
     monumentsLBData = MonumentsLeaderboard.get(
       derivedStore,
@@ -295,6 +296,161 @@ contract MonumentsTest is MudTest {
       lowerSouthwestCorner1.z
     );
     assertTrue(monumentsLBData.rank == 2, "Rank not set correctly");
+
+    vm.stopPrank();
+  }
+
+  function testSimpleBounty() public {
+    vm.startPrank(alice, alice);
+
+    (, bytes32 agentObjectEntityId) = setupAgent();
+    bytes32 agentObjectTypeId = BuilderObjectID;
+
+    VoxelCoord memory lowerSouthwestCorner = VoxelCoord({ x: agentCoord.x, y: 0, z: agentCoord.z });
+    VoxelCoord memory size = VoxelCoord({ x: 10, y: 0, z: 10 });
+
+    derivedWorld.claimMonumentsArea(agentObjectEntityId, lowerSouthwestCorner, size);
+    MonumentsLeaderboardData memory monumentsLBData = MonumentsLeaderboard.get(
+      derivedStore,
+      lowerSouthwestCorner.x,
+      lowerSouthwestCorner.y,
+      lowerSouthwestCorner.z
+    );
+    assertTrue(monumentsLBData.owner == alice, "Owner not set correctly");
+
+    uint256 bountyAmount = 100;
+
+    // T grass shape
+    bytes32[] memory objectTypeIds = new bytes32[](5);
+    objectTypeIds[0] = GrassObjectID;
+    objectTypeIds[1] = GrassObjectID;
+    objectTypeIds[2] = GrassObjectID;
+    objectTypeIds[3] = DirtObjectID;
+    objectTypeIds[4] = DirtObjectID;
+
+    VoxelCoord[] memory relativePositions = new VoxelCoord[](5);
+    relativePositions[0] = VoxelCoord({ x: 0, y: 0, z: 0 });
+    relativePositions[1] = VoxelCoord({ x: 1, y: 0, z: 0 });
+    relativePositions[2] = VoxelCoord({ x: 2, y: 0, z: 0 });
+    relativePositions[3] = VoxelCoord({ x: 1, y: 1, z: 0 });
+    relativePositions[4] = VoxelCoord({ x: 1, y: 2, z: 0 });
+
+    bytes32 bountyId = derivedWorld.addBounty(
+      bountyAmount,
+      objectTypeIds,
+      relativePositions,
+      "Test Simple Bounty Name",
+      "Test Simple Bounty Description"
+    );
+    MonumentBountiesData memory bountyData = MonumentBounties.get(derivedStore, bountyId);
+    assertTrue(bountyData.creator == alice, "Creator not set correctly");
+    assertTrue(bountyData.bountyAmount == bountyAmount, "BountyAmount not set correctly");
+    assertTrue(bountyData.claimedBy == address(0), "ClaimedBy not set correctly");
+    assertTrue(bountyData.objectTypeIds.length == objectTypeIds.length, "ObjectTypeIds not set correctly");
+    assertTrue(
+      abi.decode(bountyData.relativePositions, (VoxelCoord[])).length == relativePositions.length,
+      "RelativePositions not set correctly"
+    );
+    assertTrue(bytes(bountyData.name).length > 0, "Name not set correctly");
+    assertTrue(bytes(bountyData.description).length > 0, "Description not set correctly");
+
+    VoxelCoord memory newAgentCoord = VoxelCoord({ x: agentCoord.x + 1, y: agentCoord.y, z: agentCoord.z + 1 });
+    world.move(agentObjectEntityId, agentObjectTypeId, agentCoord, newAgentCoord);
+    agentCoord = newAgentCoord;
+    VoxelCoord memory baseWorldCoord = VoxelCoord({ x: newAgentCoord.x + 1, y: newAgentCoord.y, z: newAgentCoord.z });
+    world.build(agentObjectEntityId, objectTypeIds[0], baseWorldCoord);
+
+    // Should fail because doesn't match bounty
+    vm.expectRevert();
+    derivedWorld.claimBounty(bountyId, lowerSouthwestCorner, baseWorldCoord);
+
+    newAgentCoord = VoxelCoord({ x: agentCoord.x + 1, y: agentCoord.y, z: agentCoord.z + 1 });
+    world.move(agentObjectEntityId, agentObjectTypeId, agentCoord, newAgentCoord);
+    agentCoord = newAgentCoord;
+
+    newAgentCoord = VoxelCoord({ x: agentCoord.x + 1, y: agentCoord.y, z: agentCoord.z });
+    world.move(agentObjectEntityId, agentObjectTypeId, agentCoord, newAgentCoord);
+    agentCoord = newAgentCoord;
+
+    world.build(
+      agentObjectEntityId,
+      objectTypeIds[1],
+      VoxelCoord({
+        x: baseWorldCoord.x + relativePositions[1].x,
+        y: baseWorldCoord.y + relativePositions[1].y,
+        z: baseWorldCoord.z + relativePositions[1].z
+      })
+    );
+    world.build(
+      agentObjectEntityId,
+      objectTypeIds[2],
+      VoxelCoord({
+        x: baseWorldCoord.x + relativePositions[2].x,
+        y: baseWorldCoord.y + relativePositions[2].y,
+        z: baseWorldCoord.z + relativePositions[2].z
+      })
+    );
+    world.build(
+      agentObjectEntityId,
+      objectTypeIds[3],
+      VoxelCoord({
+        x: baseWorldCoord.x + relativePositions[3].x,
+        y: baseWorldCoord.y + relativePositions[3].y,
+        z: baseWorldCoord.z + relativePositions[3].z
+      })
+    );
+
+    newAgentCoord = VoxelCoord({ x: agentCoord.x, y: agentCoord.y + 1, z: agentCoord.z });
+    world.move(agentObjectEntityId, agentObjectTypeId, agentCoord, newAgentCoord);
+    agentCoord = newAgentCoord;
+
+    // Build wrong object type
+    world.build(
+      agentObjectEntityId,
+      GrassObjectID,
+      VoxelCoord({
+        x: baseWorldCoord.x + relativePositions[4].x,
+        y: baseWorldCoord.y + relativePositions[4].y,
+        z: baseWorldCoord.z + relativePositions[4].z
+      })
+    );
+
+    // Should fail because doesn't match bounty
+    vm.expectRevert();
+    derivedWorld.claimBounty(bountyId, lowerSouthwestCorner, baseWorldCoord);
+
+    world.mine(
+      agentObjectEntityId,
+      GrassObjectID,
+      VoxelCoord({
+        x: baseWorldCoord.x + relativePositions[4].x,
+        y: baseWorldCoord.y + relativePositions[4].y,
+        z: baseWorldCoord.z + relativePositions[4].z
+      })
+    );
+
+    world.build(
+      agentObjectEntityId,
+      objectTypeIds[4],
+      VoxelCoord({
+        x: baseWorldCoord.x + relativePositions[4].x,
+        y: baseWorldCoord.y + relativePositions[4].y,
+        z: baseWorldCoord.z + relativePositions[4].z
+      })
+    );
+
+    // Even if somebody else does the claim tx, only the claimed area owner
+    // gets the bounty
+    vm.startPrank(bob, bob);
+    derivedWorld.claimBounty(bountyId, lowerSouthwestCorner, baseWorldCoord);
+    bountyData = MonumentBounties.get(derivedStore, bountyId);
+    assertTrue(bountyData.claimedBy == monumentsLBData.owner, "ClaimedBy not set correctly");
+    vm.stopPrank();
+    vm.startPrank(alice, alice);
+
+    // Should fail because already claimed
+    vm.expectRevert();
+    derivedWorld.claimBounty(bountyId, lowerSouthwestCorner, baseWorldCoord);
 
     vm.stopPrank();
   }

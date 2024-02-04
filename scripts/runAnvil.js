e627a8b73000f57506de13ff321d84dcf3a5db13
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-02-03 17:30:49 -0700

    set one sec block time

--- scripts/runAnvil.js
+++ scripts/runAnvil.js
@@ -1,35 +1,25 @@
-import {
-    anvil,
-    forge,
-    getRpcUrl,
-    getScriptDirectory,
-    getSrcDirectory
-} from "@latticexyz/common/foundry";
-import {
-    homedir
-} from "os";
+import { anvil, forge, getRpcUrl, getScriptDirectory, getSrcDirectory } from "@latticexyz/common/foundry";
+import { homedir } from "os";
 import path from "path";
-import {
-    rmSync
-} from "fs";
+import { rmSync } from "fs";
 
 console.log("Cleaning devnode cache");
 const userHomeDir = homedir();
 rmSync(path.join(userHomeDir, ".foundry", "anvil", "tmp"), {
-    recursive: true,
-    force: true
+  recursive: true,
+  force: true,
 });
 
 const anvilArgs = [
-    // "--block-time",
-    // "1",
-    "--block-base-fee-per-gas",
-    "0",
-    "--host",
-    "0.0.0.0",
-    "--chain-id",
-    "31337",
-    "--gas-limit",
-    "1000000000",
+  "--block-time",
+  "1",
+  "--block-base-fee-per-gas",
+  "0",
+  "--host",
+  "0.0.0.0",
+  "--chain-id",
+  "31337",
+  "--gas-limit",
+  "1000000000",
 ];
 anvil(anvilArgs);
